b2431df95d6caf8c511d48e5b0c2c3596e90bb9a
Daniel Berger <djberg96@gmail.com>
2010-12-22 20:31:00 -0700

    Updated the Array#rindex tests.

--- test/core/Array/instance/tc_rindex.rb
+++ test/core/Array/instance/tc_rindex.rb
@@ -4,33 +4,40 @@
 # Test suite for the Array#rindex instance method.
 ######################################################
 require 'test/helper'
-require "test/unit"
+require 'test/unit'
 
-class TC_Array_RIndex_Instance < Test::Unit::TestCase
-   def setup   
-      @array = [1,"two",nil,false,true,"two",nil]
-   end
+class TC_Array_RIndex_InstanceMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def test_rindex_basic
-      assert_respond_to(@array, :rindex)
-      assert_nothing_raised{ @array.rindex(1) }
-   end
+  def setup
+    @array = [1, "two", nil, false, true, "two", nil]
+  end
 
-   def test_rindex
-      assert_equal(0, @array.rindex(1))
-      assert_equal(5, @array.rindex("two"))
-      assert_equal(6, @array.rindex(nil))
-      assert_equal(3, @array.rindex(false))
-      assert_equal(4, @array.rindex(true))
-      assert_equal(nil, @array.rindex(99))
-   end
+  test "rindex basic functionality" do
+    assert_respond_to(@array, :rindex)
+    assert_nothing_raised{ @array.rindex(1) }
+  end
 
-   def test_rindex_expected_errors
+  test "rindex returns the expected results" do
+    assert_equal(0, @array.rindex(1))
+    assert_equal(5, @array.rindex("two"))
+    assert_equal(6, @array.rindex(nil))
+    assert_equal(3, @array.rindex(false))
+    assert_equal(4, @array.rindex(true))
+  end
+
+  test "rindex returns nil if the value is not found" do
+    assert_equal(nil, @array.rindex(99))
+  end
+
+  if RELEASE < 7
+    test "rindex requires one argument only" do
       assert_raises(ArgumentError){ @array.rindex }
-      assert_raises(ArgumentError){ @array.rindex(0,1) }
-   end
+      assert_raises(ArgumentError){ @array.rindex(0, 1) }
+    end
+  end
 
-   def teardown
-      @array = nil
-   end
+  def teardown
+    @array = nil
+  end
 end
