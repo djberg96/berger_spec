d07213ca93a2ed2f7dab81788bd7fc1b6ecf3055
Daniel Berger <djberg96@gmail.com>
2010-12-22 19:17:42 -0700

    Replaced old boolean test with updated code.

--- test/core/Array/instance/tc_equality.rb
+++ test/core/Array/instance/tc_equality.rb
@@ -8,56 +8,56 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Array_Equality_Instance < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   class AEquality
-      def to_ary
-         [1,2,3]
-      end
-   end
+  class AEquality
+    def to_ary
+       [1,2,3]
+    end
+  end
 
-   def setup
-      @array_int1    = [1, 2, 3]
-      @array_int2    = [1, 2, 3]
-      @array_int3    = [3, 1, 2]
-      @array_chr_int = ['1', '2', '3']
-      @custom        = AEquality.new
-   end
+  def setup
+    @array_int1    = [1, 2, 3]
+    @array_int2    = [1, 2, 3]
+    @array_int3    = [3, 1, 2]
+    @array_chr_int = ['1', '2', '3']
+    @custom        = AEquality.new
+  end
 
-   def test_basic
-      assert_respond_to(@array_int1, :==)
-      assert_nothing_raised{ @array_int1 == @array_int2 }
-      assert_kind_of(Boolean, @array_int1 == @array_int2)
-   end
+  def test_basic
+    assert_respond_to(@array_int1, :==)
+    assert_nothing_raised{ @array_int1 == @array_int2 }
+    assert_boolean(@array_int1 == @array_int2)
+  end
 
-   def test_equality_success
-      msg = "=> See RubyForge bug #11585"
-      assert_equal(true, @array_int1 == @array_int2)
-      assert_equal(true, @array_int1 == @custom, msg)
-      assert_equal(true, [1.1, 2.1] == [1.1, 2.1])
-   end
+  def test_equality_success
+    msg = "=> See RubyForge bug #11585"
+    assert_equal(true, @array_int1 == @array_int2)
+    assert_equal(true, @array_int1 == @custom, msg)
+    assert_equal(true, [1.1, 2.1] == [1.1, 2.1])
+  end
 
-   def test_equality_failure
-      assert_equal(false, @array_int1 == @array_int3)
-      assert_equal(false, @array_int1 == @array_chr_int)
-      assert_equal(false, @array_int1 == [])
-      assert_equal(false, @array_int1 == nil)
-      assert_equal(false, @array_int1 == 0)
-      assert_equal(false, [1.11, 2.1] == [1.1, 2.1])
-   end
+  def test_equality_failure
+    assert_equal(false, @array_int1 == @array_int3)
+    assert_equal(false, @array_int1 == @array_chr_int)
+    assert_equal(false, @array_int1 == [])
+    assert_equal(false, @array_int1 == nil)
+    assert_equal(false, @array_int1 == 0)
+    assert_equal(false, [1.11, 2.1] == [1.1, 2.1])
+  end
 
-   def test_edge_cases
-      assert_equal(false, [0] == 0)
-      assert_equal(false, [nil] == nil)
-      assert_equal(false, [true] == true)
-      assert_equal(false, [false] == false)
-   end
+  def test_edge_cases
+    assert_equal(false, [0] == 0)
+    assert_equal(false, [nil] == nil)
+    assert_equal(false, [true] == true)
+    assert_equal(false, [false] == false)
+  end
 
-   def teardown
-      @array_int1 = nil
-      @array_int2 = nil
-      @array_int3 = nil
-      @array_chr_int = nil
-      @custom = nil
-   end
+  def teardown
+    @array_int1 = nil
+    @array_int2 = nil
+    @array_int3 = nil
+    @array_chr_int = nil
+    @custom = nil
+  end
 end
