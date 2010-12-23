c8c381888f9a92c9324e57f48e3d38f3ca468b46
Daniel Berger <dberger@globe.gov>
2010-12-23 13:09:41 -0700

    Updated String#strip tests.

--- test/core/String/instance/test_strip.rb
+++ test/core/String/instance/test_strip.rb
@@ -7,49 +7,58 @@
 require 'test/helper'
 require 'test/unit'
 
-class TC_String_Strip_Instance < Test::Unit::TestCase
-   def setup
-      @string1 = " hello\r\n"
-      @string2 = "hello\t               "
-      @string3 = "hello  \000"
-      @string4 = "hello  \000world\000  "
-   end
+class TC_String_Strip_InstanceMethod < Test::Unit::TestCase
+  def setup
+    @string1 = " hello\r\n"
+    @string2 = "hello\t               "
+    @string3 = "hello  \000"
+    @string4 = "hello  \000world\000  "
+  end
 
-   def test_strip_basic
-      assert_respond_to(@string1, :strip)
-      assert_nothing_raised{ @string1.strip }
-      assert_kind_of(String, @string1.strip)
-   end
+  test "strip basic functionality" do
+    assert_respond_to(@string1, :strip)
+    assert_nothing_raised{ @string1.strip }
+    assert_kind_of(String, @string1.strip)
+  end
 
-   def test_strip
-      assert_equal('hello', @string1.strip)
-      assert_equal('hello', @string2.strip)
-      assert_equal('hello', @string3.strip)
-      assert_equal("hello  \000world\000", @string4.strip)
-   end
+  test "strip returns expected results" do
+    assert_equal('hello', @string1.strip)
+    assert_equal('hello', @string2.strip)
+  end
 
-   def test_strip_leaves_original_string_unmodified
-      assert_nothing_raised{ @string3.strip }
-      assert_equal("hello  \000", @string3)
-   end
+  test "strip removes trailing octal zero if it's the last character" do
+    assert_equal('hello', @string3.strip)
+  end
 
-   def test_strip_frozen_string
-      assert_equal('hello', @string1.freeze.strip)
-   end
+  test "strip does not remove a trailing octal zero if it's not the last character" do
+    assert_equal("hello  \000world\000", @string4.strip)
+  end
 
-   def test_strip_edge_cases
-      assert_equal('', ''.strip)
-      assert_equal('', "\000\000".strip)
-   end
+  test "strip does not modify the receiver" do
+    assert_nothing_raised{ @string3.strip }
+    assert_equal("hello  \000", @string3)
+  end
 
-   def test_strip_expected_errors
-      assert_raises(ArgumentError){ @string1.strip('x') }
-   end
+  test "strip works on a frozen string" do
+    assert_equal('hello', @string1.freeze.strip)
+  end
 
-   def teardown
-      @string1 = nil
-      @string2 = nil
-      @string3 = nil
-      @string4 = nil
-   end
+  test "strip called on an empty string returns an empty string" do
+    assert_equal('', ''.strip)
+  end
+
+  test "strip called on a string of octal zeroes returns an empty string" do
+    assert_equal('', "\000\000".strip)
+  end
+
+  test "strip does not accept any arguments" do
+    assert_raises(ArgumentError){ @string1.strip('x') }
+  end
+
+  def teardown
+    @string1 = nil
+    @string2 = nil
+    @string3 = nil
+    @string4 = nil
+  end
 end
