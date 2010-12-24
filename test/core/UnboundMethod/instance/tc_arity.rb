23dc3cb73339888c3ce338cfb80c515401c716c6
Daniel Berger <djberg96@gmail.com>
2010-12-24 08:00:44 -0700

    Modified UnboundMethod tests to ensure test-unit 2 is loaded.

--- test/core/UnboundMethod/instance/tc_arity.rb
+++ test/core/UnboundMethod/instance/tc_arity.rb
@@ -3,6 +3,7 @@
 #
 # Test case for the UnboundMethod#arity instance method.
 ########################################################################
+require 'test/helper'
 require 'test/unit'
 
 class UnboundTest
