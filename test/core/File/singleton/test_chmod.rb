da1d2e36250d5b495879cba23bd4008a2f0e9809
Daniel Berger <djberg96@gmail.com>
2010-12-30 08:12:07 -0700

    Refactored the File.lchmod tests.

--- test/core/File/singleton/test_chmod.rb
+++ test/core/File/singleton/test_chmod.rb
@@ -1,5 +1,5 @@
 #####################################################################
-# tc_chmod.rb
+# test_chmod.rb
 #
 # Test case for the File.chmod class method.
 #####################################################################
@@ -7,55 +7,59 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_File_Chmod_ClassMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @file1 = File.expand_path(__FILE__)
-      @file2 = base_file(__FILE__, 'tc_atime.rb')
-      @file1_mode = File.stat(@file1).mode
-      @file2_mode = File.stat(@file2).mode
-   end
+  def setup
+    @file1 = File.expand_path(__FILE__)
+    @file2 = base_file(__FILE__, 'test_atime.rb')
+    @file1_mode = File.stat(@file1).mode
+    @file2_mode = File.stat(@file2).mode
+  end
 
-   def test_chmod_basic
-      assert_respond_to(File, :chmod)
-      assert_nothing_raised{ File.chmod(0644, @file1) }
-      assert_nothing_raised{ File.chmod(0644, @file1, @file2) }
-      assert_kind_of(Fixnum, File.chmod(0644, @file1))
-   end
+  test "chmod basic functionality" do
+    assert_respond_to(File, :chmod)
+    assert_nothing_raised{ File.chmod(0644, @file1) }
+    assert_kind_of(Fixnum, File.chmod(0644, @file1))
+  end
 
-   def test_chmod
-      assert_nothing_raised{ File.chmod(0644, @file1) }
-      assert_equal(1, File.chmod(0644, @file1))
-      assert_equal('100644', File.stat(@file1).mode.to_s(8))
+  test "chmod can be called on multiple files" do
+    assert_nothing_raised{ File.chmod(0644, @file1, @file2) }
+    assert_equal('100644', File.stat(@file1).mode.to_s(8))
+    assert_equal('100644', File.stat(@file2).mode.to_s(8))
+  end
 
-      assert_nothing_raised{ File.chmod(0444, @file2) }
-      assert_equal(1, File.chmod(0444, @file2))
-      assert_equal('100444', File.stat(@file2).mode.to_s(8))
-   end
+  test "chmod returns the number of files modified" do
+    assert_equal(1, File.chmod(0644, @file1))
+    assert_equal(2, File.chmod(0644, @file1, @file2))
+  end
 
-   def test_chmod_multiple_files
-      assert_nothing_raised{ File.chmod(0444, @file1, @file2) }
-      assert_equal(2, File.chmod(0444, @file1, @file2))
-      assert_equal('100444', File.stat(@file1).mode.to_s(8))
-      assert_equal('100444', File.stat(@file2).mode.to_s(8))
-   end
+  test "chmod sets the permissions as expected" do
+    assert_nothing_raised{ File.chmod(0644, @file1) }
+    assert_equal('100644', File.stat(@file1).mode.to_s(8))
+    assert_nothing_raised{ File.chmod(0444, @file2) }
+    assert_equal('100444', File.stat(@file2).mode.to_s(8))
+  end
 
-   def test_chmod_edge_cases
-      assert_nothing_raised{ File.chmod(0444) } # Debatable
-      assert_equal(0, File.chmod(0444))
-   end
+  test "chmod does not actually require any file arguments" do
+    notify("File.chmod with no files should probably be an ArgumentError")
+    assert_nothing_raised{ File.chmod(0444) }
+    assert_equal(0, File.chmod(0444))
+  end
 
-   def test_chmod_expected_errors
-      assert_raises(ArgumentError){ File.chmod }
-      assert_raises(TypeError){ File.chmod('0644') }
-   end
+  test "chmod requires at least one argument" do
+    assert_raises(ArgumentError){ File.chmod }
+  end
 
-   def teardown
-      File.chmod(@file1_mode, @file1)
-      File.chmod(@file2_mode, @file2)
-      @file1 = nil
-      @file2 = nil
-      @file1_mode = nil
-      @file2_mode = nil
-   end
+  test "the first argument to chmod must be numeric" do
+    assert_raises(TypeError){ File.chmod('0644') }
+  end
+
+  def teardown
+    File.chmod(@file1_mode, @file1)
+    File.chmod(@file2_mode, @file2)
+    @file1 = nil
+    @file2 = nil
+    @file1_mode = nil
+    @file2_mode = nil
+  end
 end
--- test/core/File/singleton/test_lchmod.rb
+++ test/core/File/singleton/test_lchmod.rb
@@ -1,69 +1,86 @@
 #####################################################################
-# tc_lchmod.rb
+# test_lchmod.rb
 #
-# Test case for the File.lchmod class method. Note that almost all
-# of these tests are skipped on MS Windows, OS X, and SOLARIS.
+# Test case for the File.lchmod singleton method.
 #####################################################################
+require 'mkmf/lite'
 require 'test/helper'
 require 'test/unit'
 
-class TC_File_Lchmod_ClassMethod < Test::Unit::TestCase
-   include Test::Helper
+class TC_File_Lchmod_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
+  extend Mkmf::Lite
 
-   # TODO: Setup actual symbolic links
-   def setup
-      @file1 = File.expand_path(__FILE__)
-      @file2 = base_file(__FILE__, 'tc_atime.rb')
-      @file1_mode = File.stat(@file1).mode
-      @file2_mode = File.stat(@file2).mode
-   end
+  def self.startup
+    @@lchmod_support = have_func('lchmod')
+    @@lchmod_message = "lchmod not supported on this platform"
+  end
 
-   def test_lchmod_basic
-      assert_respond_to(File, :lchmod)
-      unless WINDOWS || OSX || SOLARIS
-         assert_nothing_raised{ File.lchmod(0644, @file1) }
-         assert_nothing_raised{ File.lchmod(0644, @file1, @file2) }
-         assert_kind_of(Fixnum, File.lchmod(0644, @file1))
-      end
-   end
+  def setup
+    @file1 = File.expand_path(__FILE__)
+    @file2 = base_file(__FILE__, 'test_atime.rb')
+    @file1_mode = File.stat(@file1).mode
+    @file2_mode = File.stat(@file2).mode
+  end
 
-   unless WINDOWS || OSX || SOLARIS
-      def test_lchmod
-         assert_nothing_raised{ File.lchmod(0644, @file1) }
-         assert_equal(1, File.lchmod(0644, @file1))
-         assert_equal('100644', File.stat(@file1).mode.to_s(8))
+  test "lchmod basic functionality" do
+    omit_unless(@@lchmod_support, @@lchmod_message)
+    assert_respond_to(File, :lchmod)
+    assert_nothing_raised{ File.lchmod(0644, @file1) }
+    assert_kind_of(Fixnum, File.lchmod(0644, @file1))
+  end
 
-         assert_nothing_raised{ File.lchmod(0444, @file2) }
-         assert_equal(1, File.lchmod(0444, @file2))
-         assert_equal('100444', File.stat(@file2).mode.to_s(8))
-      end
+  test "lchmod can be called on multiple files" do
+    omit_unless(@@lchmod_support, @@lchmod_message)
+    assert_nothing_raised{ File.lchmod(0644, @file1, @file2) }
+    assert_equal('100644', File.stat(@file1).mode.to_s(8))
+    assert_equal('100644', File.stat(@file2).mode.to_s(8))
+  end
 
-      def test_lchmod_multiple_files
-         assert_nothing_raised{ File.lchmod(0444, @file1, @file2) }
-         assert_equal(2, File.lchmod(0444, @file1, @file2))
-         assert_equal('100444', File.stat(@file1).mode.to_s(8))
-         assert_equal('100444', File.stat(@file2).mode.to_s(8))
-      end
+  test "lchmod returns the number of files modified" do
+    omit_unless(@@lchmod_support, @@lchmod_message)
+    assert_equal(1, File.lchmod(0644, @file1))
+    assert_equal(2, File.lchmod(0644, @file1, @file2))
+  end
 
-      def test_lchmod_edge_cases
-         assert_nothing_raised{ File.lchmod(0444) } # Debatable
-         assert_equal(0, File.lchmod(0444))
-      end
+  test "lchmod sets the permissions as expected" do
+    omit_unless(@@lchmod_support, @@lchmod_message)
+    assert_nothing_raised{ File.lchmod(0644, @file1) }
+    assert_equal('100644', File.stat(@file1).mode.to_s(8))
+    assert_nothing_raised{ File.lchmod(0444, @file2) }
+    assert_equal('100444', File.stat(@file2).mode.to_s(8))
+  end
 
-      def test_lchmod_expected_errors
-         assert_raises(ArgumentError){ File.lchmod }
-         assert_raises(TypeError){ File.lchmod('0644') }
-      end
-   end
+  test "lchmod does not actually require any file arguments" do
+    omit_unless(@@lchmod_support, @@lchmod_message)
+    notify("File.lchmod with no files should probably be an ArgumentError")
+    assert_nothing_raised{ File.lchmod(0444) }
+    assert_equal(0, File.lchmod(0444))
+  end
 
-   def teardown
-      unless WINDOWS || OSX || SOLARIS
-         File.lchmod(@file1_mode, @file1)
-         File.lchmod(@file2_mode, @file2)
-      end
-      @file1 = nil
-      @file2 = nil
-      @file1_mode = nil
-      @file2_mode = nil
-   end
+  test "lchmod requires at least one argument" do
+    omit_unless(@@lchmod_support, @@lchmod_message)
+    assert_raises(ArgumentError){ File.lchmod }
+  end
+
+  test "the first argument to lchmod must be numeric" do
+    omit_unless(@@lchmod_support, @@lchmod_message)
+    assert_raises(TypeError){ File.lchmod('0644') }
+  end
+
+  def teardown
+    if @@lchmod_support
+      File.lchmod(@file1_mode, @file1)
+      File.lchmod(@file2_mode, @file2)
+    end
+    @file1 = nil
+    @file2 = nil
+    @file1_mode = nil
+    @file2_mode = nil
+  end
+
+  def self.shutdown
+    @@lchmod_support = nil
+    @@lchmod_message = nil
+  end
 end
