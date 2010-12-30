8a4ed90d7160ab9fab7f3d094b45c382b3692f48
Daniel Berger <dberger@globe.gov>
2010-12-29 23:16:49 -0700

    Updated File#mtime tests.

--- test/core/File/instance/tc_mtime.rb
+++ test/core/File/instance/tc_mtime.rb
@@ -7,29 +7,29 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_File_Mtime_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @name = File.expand_path(__FILE__)
-      @file = File.open(@name)
-   end
+  def setup
+    @name = File.expand_path(__FILE__)
+    @file = File.open(@name)
+  end
 
-   def test_mtime
-      assert_respond_to(@file, :mtime)
-      assert_nothing_raised{ @file.mtime }
-      assert_kind_of(Time, @file.mtime)
-   end
+  test "mtime basic functionality" do
+    assert_respond_to(@file, :mtime)
+    assert_nothing_raised{ @file.mtime }
+    assert_kind_of(Time, @file.mtime)
+  end
 
-   def test_mtime_fails_on_closed_handle
-      assert_nothing_raised{ @file.close }
-      assert_raise(IOError){ @file.mtime }
-   end
+  test "mtime fails on a closed handle" do
+    @file.close
+    assert_raise(IOError){ @file.mtime }
+  end
 
-   def test_mtime_expected_errors
-      assert_raises(ArgumentError){ @file.mtime(@name) }
-   end
+  test "mtime does not accept any arguments" do
+    assert_raises(ArgumentError){ @file.mtime(@name) }
+  end
 
-   def teardown
-      @file.close unless @file.closed?
-      @name = nil
-      @file = nil
-   end
+  def teardown
+    @file.close if @file && !@file.closed?
+    @name = nil
+    @file = nil
+  end
 end
