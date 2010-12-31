cce094b779ca5a1742963ac6fcd2541723901367
Daniel Berger <djberg96@gmail.com>
2010-12-31 15:50:11 -0700

    Updated the Time#zone tests.

--- test/core/Time/instance/test_zone.rb
+++ test/core/Time/instance/test_zone.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_zone.rb
+# test_zone.rb
 #
 # Test case for the Time#zone instance method.
 ########################################################################
@@ -7,32 +7,38 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Time_Zone_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @gmt   = Time.gm(2000, 6, 1, 20, 15, 1)
-      @local = Time.local(2000, 6, 1, 20, 15, 1)
-      @tz    = get_tz_name
-   end
+  def setup
+    @gmt   = Time.gm(2000, 6, 1, 20, 15, 1)
+    @local = Time.local(2000, 6, 1, 20, 15, 1)
+    @tz    = get_tz_name
+  end
 
-   def test_zone_basic
-      assert_respond_to(@local, :zone)
-      assert_nothing_raised{ @local.zone }
-   end
+  test "zone basic functionality" do
+    assert_respond_to(@local, :zone)
+    assert_nothing_raised{ @local.zone }
+  end
 
-   def test_zone
-      assert_equal('UTC', @gmt.zone)
-      assert_equal(@tz, @local.zone)
-   end
+  test "gmt time returns UTC for the zone" do
+    assert_equal('UTC', @gmt.zone)
+  end
 
-   def test_zone_expected_errors
-      assert_raise(ArgumentError){ @local.zone(1) }
-      assert_raise(NoMethodError){ @local.zone = 'UTC' }
-   end
+  test "local time returns the correct zone name" do
+    assert_equal(@tz, @local.zone)
+  end
 
-   def teardown
-      @gmt   = nil
-      @local = nil
-      @tz    = nil
-   end
+  test "zone does not accept any arguments" do
+    assert_raise(ArgumentError){ @local.zone(1) }
+  end
+
+  test "users cannot assign a zone" do
+    assert_raise(NoMethodError){ @local.zone = 'UTC' }
+  end
+
+  def teardown
+    @gmt   = nil
+    @local = nil
+    @tz    = nil
+  end
 end
