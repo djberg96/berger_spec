3d7d7f58bcf7264c90b561f129f1723695af5cf7
Daniel Berger <djberg96@gmail.com>
2011-01-02 20:45:55 -0700

    Added tests for Array#select.

---
+++ test/core/Array/instance/test_select.rb
@@ -0,0 +1,67 @@
+########################################################################
+# test_select.rb
+#
+# Tests for the Array#select instance method. Although Array#select
+# is mixed in from Enumerable, these tests remain in place because
+# the array.c source file has its own implementation.
+########################################################################
+require 'test/helper'
+require 'test/unit'
+
+class TC_Array_Select_InstanceMethod < Test::Unit::TestCase
+  include Test::Helper
+
+  def setup
+    @array = ['alpha', 1, 'beta', 2.1, ['a', 'b', 'c']]
+  end
+
+  test "select basic functionality" do
+    assert_respond_to(@array, :select)
+    assert_nothing_raised{ @array.select{} }
+  end
+
+  test "select returns an array when a match is found" do
+    assert_equal(['alpha', 'beta'], @array.select{ |e| e =~ /\w/i })
+    assert_equal([1], @array.select{ |e| e.kind_of?(Fixnum) })
+  end
+
+  test "select returns an empty array when no match is found" do
+    assert_equal([], @array.select{ |e| e == 'bogus' })
+  end
+
+  test "find_all handles explicit nil and false as expected" do
+    assert_equal([nil, nil], [nil, nil].select{ |e| e.nil? })
+    assert_equal([false, false], [false, false].select{ |e| e == false })
+  end
+
+  test "calling select on an empty array returns expected result" do
+    if PRE187
+      assert_equal([], [].select)
+      assert_equal([], [].select{})
+    else
+      assert_equal(0, [].select.count)
+      assert_equal(0, [].select{}.count)
+    end
+  end
+
+  test "select returns the original object if non-false object is yielded" do
+    assert_equal(@array, @array.select{ true })
+    #assert_equal(@array.object_id, @array.select{ true }.object_id)
+  end
+
+  test "select with no block exhibits the expected behavior" do
+    if PRE187
+      assert_raise(LocalJumpError){ @array.select }
+    else
+      assert_kind_of(Enumerable::Enumerator, @array.select)
+    end
+  end
+
+  test "select does not take any arguments" do
+    assert_raise(ArgumentError){ @array.select('alpha') }
+  end
+
+  def teardown
+    @array = nil
+  end
+end
