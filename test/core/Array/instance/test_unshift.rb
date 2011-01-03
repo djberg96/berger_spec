3ec24f63c30c01bfb59477889153aff100c3cd66
Daniel Berger <djberg96@gmail.com>
2011-01-02 21:11:49 -0700

    Updated the tests for Array#unshift

--- test/core/Array/instance/test_unshift.rb
+++ test/core/Array/instance/test_unshift.rb
@@ -1,41 +1,48 @@
 ###########################################################
-# tc_unshift.rb
+# test_unshift.rb
 #
 # Test suite for the Array#unshift instance method.
 ###########################################################
 require 'test/helper'
 require 'test/unit'
 
-class TC_Array_Unshift_Instance < Test::Unit::TestCase
-   def setup
-      @array = ['a','b','c']
-   end
+class Test_Array_Unshift_InstanceMethod < Test::Unit::TestCase
+  def setup
+    @array = ['a','b','c']
+  end
 
-   def test_basic
-      assert_respond_to(@array, :unshift)
-      assert_nothing_raised{ @array.unshift }
-   end
+  test "unshift basic functionality" do
+    assert_respond_to(@array, :unshift)
+    assert_nothing_raised{ @array.unshift }
+  end
 
-   def test_unshift
-      assert_equal([1, 'a', 'b', 'c'], @array.unshift(1))
-      assert_equal(['d', 2, 1, 'a', 'b', 'c'], @array.unshift('d', 2))
-      assert_equal([nil, [1,2], 'd', 2, 1, 'a', 'b', 'c'], @array.unshift(nil, [1,2]))
-      assert_equal([nil, [1,2], 'd', 2, 1, 'a', 'b', 'c'], @array)
-   end
-   
-   def test_unshift_advanced
-      assert_equal([1, 2, 'a', 'b', 'c'], @array.unshift(*[1,2]))
-      assert_equal([[1, 2], 'a', 'b', 'c'], ['a','b','c'].unshift(*{1,2}))
-   end
-   
-   def test_unshift_edge_cases
-      assert_equal(['a', 'b', 'c'], @array.unshift)
-      assert_equal([nil, 'a', 'b', 'c'], @array.unshift(nil))
-      assert_equal([nil, nil], [nil].unshift(nil))
-      assert_equal([nil, false], [false].unshift(nil))
-   end
+  test "unshift expected results" do
+    assert_equal([1, 'a', 'b', 'c'], @array.unshift(1))
+    assert_equal(['d', 2, 1, 'a', 'b', 'c'], @array.unshift('d', 2))
+    assert_equal([nil, [1,2], 'd', 2, 1, 'a', 'b', 'c'], @array.unshift(nil, [1,2]))
+  end
 
-   def teardown
-      @array = nil
-   end
+  test "unshift modifies its receiver" do
+    assert_nothing_raised{ @array.unshift(1) }
+    assert_equal([1, 'a', 'b', 'c'], @array)
+  end
+  
+  test "unshift with splat works as expected" do
+    assert_equal([1, 2, 'a', 'b', 'c'], @array.unshift(*[1,2]))
+    assert_equal([[1, 2], 'a', 'b', 'c'], ['a','b','c'].unshift(*{1,2}))
+  end
+
+  test "unshift without arguments returns the original array" do
+    assert_equal(['a', 'b', 'c'], @array.unshift)
+  end
+
+  test "unshifting an explicit nil works as expected" do
+    assert_equal([nil, 'a', 'b', 'c'], @array.unshift(nil))
+    assert_equal([nil, nil], [nil].unshift(nil))
+    assert_equal([nil, false], [false].unshift(nil))
+  end
+
+  def teardown
+    @array = nil
+  end
 end
