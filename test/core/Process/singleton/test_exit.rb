b0da273b3d178cd1f96e7c8cd021b218a6a311f8
Daniel Berger <djberg96@gmail.com>
2011-01-03 22:29:33 -0700

    Refactored the Process.exit tests.

--- test/core/Process/singleton/test_exit.rb
+++ test/core/Process/singleton/test_exit.rb
@@ -1,5 +1,5 @@
 #####################################################################
-# tc_exit.rb
+# test_exit.rb
 #
 # Test case for the Process.exit module method. I have no idea how
 # to properly test this on MS Windows without a fork() method.
@@ -8,45 +8,58 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Process_Exit_ModuleMethod < Test::Unit::TestCase
-   include Test::Helper
-   
-   def test_exit_basic
-      assert_respond_to(Process, :exit)
-   end
+  include Test::Helper
 
-   unless WINDOWS
-      # The default exit status is 0, not 1. The documentation in Programming
-      # Ruby, 2nd ed, is incorrect.
-      #
-      def test_exit
-         fork{ Process.exit }
-         pid, status = Process.wait2
-         assert_equal(0, status.exitstatus)
-      end
+  def skip_check
+    omit_if(WINDOWS, "exit test skipped on MS Windows")
+    omit_if(JRUBY, "exit test skipped on JRuby")
+  end
 
-      def test_exit_with_true_status
-         fork{ Process.exit(true) }
-         pid, status = Process.wait2
+  test "exit method is defined" do
+    assert_respond_to(Process, :exit)
+  end
 
-         assert_equal(0, status.exitstatus)
-      end
+  # The default exit status is 0, not 1. The documentation in Programming
+  # Ruby, 2nd ed, is incorrect.
+  test "exit returns a default exit status of zero" do
+    skip_check
+    fork{ Process.exit }
+    pid, status = Process.wait2
+    assert_equal(0, status.exitstatus)
+  end
 
-      def test_exit_with_false_status
-         fork{ Process.exit(false) }
-         pid, status = Process.wait2
+  test "exit with an argument of true returns zero (success)" do
+    skip_check
+    fork{ Process.exit(true) }
+    pid, status = Process.wait2
+    assert_equal(0, status.exitstatus)
+  end
 
-         assert_equal(1, status.exitstatus)
-      end
+  test "exit with an argument of false returns one (failure)" do
+    skip_check
+    fork{ Process.exit(false) }
+    pid, status = Process.wait2
+    assert_equal(1, status.exitstatus)
+  end
 
-      def test_exit_with_numeric
-         fork{ Process.exit(99) }
-         pid, status = Process.wait2
+  test "exit accepts and returns the same numeric argument" do
+    skip_check
+    fork{ Process.exit(99) }
+    pid, status = Process.wait2
+    assert_equal(99, status.exitstatus)
+  end
 
-         assert_equal(99, status.exitstatus)
-      end
+  test "exit requires a true, false or numeric argument" do
+    omit_if(WINDOWS, "exit test skipped on MS Windows")
+    assert_raise(TypeError){ Process.exit('test') }
+  end
 
-      def teardown
-         Process.waitall
-      end
-   end
+  test "exit accepts a maximum of one argument" do
+    omit_if(WINDOWS, "exit test skipped on MS Windows")
+    assert_raise(ArgumentError){ Process.exit(1, 2) }
+  end
+
+  def teardown
+    Process.waitall unless WINDOWS || JRUBY
+  end
 end
