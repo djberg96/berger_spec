4e19a5b4f28d1317d59b2c9c1db3397eab3bcc75
Daniel Berger <djberg96@gmail.com>
2011-01-04 18:47:38 -0700

    Updated tests for Array#uniq.

--- test/core/Array/instance/test_uniq.rb
+++ test/core/Array/instance/test_uniq.rb
@@ -1,45 +1,51 @@
-####################################################################
-# tc_uniq.rb
+########################################################################
+# test_uniq.rb
 #
-# Test suite for the Array#uniq and Array#unique instance methods.
-####################################################################
+# Test suite for the Array#uniq instance method. Tests for the
+# Array#uniq! method can be found in test_array_uniq_bang.rb file.
+########################################################################
 require 'test/helper'
-require "test/unit"
+require 'test/unit'
 
-class TC_Array_Uniq_Instance < Test::Unit::TestCase
-   def setup   
-      @array = ["a","b","b","c","c","c",nil,nil,false,false,true,true]
-   end
+class Test_Array_Uniq_InstanceMethod < Test::Unit::TestCase
+  def setup   
+    @array = ["a","b","b","c","c","c",nil,nil,false,false,true,true]
+  end
 
-   def test_basic
-      assert_respond_to(@array, :uniq)
-      assert_respond_to(@array, :uniq!)
-      assert_nothing_raised{ @array.uniq }
-      assert_nothing_raised{ @array.uniq! }
-   end
+  test "uniq basic functionality" do
+    assert_respond_to(@array, :uniq)
+    assert_nothing_raised{ @array.uniq }
+    assert_kind_of(Array, @array.uniq)
+  end
 
-   def test_uniq
-      assert_equal(["a","b","c",nil,false,true], @array.uniq)
-      assert_equal([1,2,3], [1,2,3].uniq)
-      assert_equal([nil], [nil].uniq)
-      assert_equal(
-         ["a","b","b","c","c","c",nil,nil,false,false,true,true], @array
-      )
-   end
+  test "uniq expected results" do
+    assert_equal(["a","b","c",nil,false,true], @array.uniq)
+    assert_equal([1,2,3], [1,2,3].uniq)
+    assert_equal(
+      ["a","b","b","c","c","c",nil,nil,false,false,true,true], @array
+    )
+  end
 
-   def test_uniq_bang
-      assert_equal(["a","b","c",nil,false,true], @array.uniq!)
-      assert_equal(["a","b","c",nil,false,true], @array)
-      assert_equal(nil, [1,2,3].uniq!)
-      assert_equal(nil, [nil].uniq!)
-   end
+  test "uniq does not modify the receiver" do
+    @array = [1, 1, 2, 2]
+    assert_nothing_raised{ @array.uniq }
+    assert_equal([1, 1, 2, 2], @array)
+  end
 
-   def test_expected_errors
-      assert_raises(ArgumentError){ @array.uniq(1) }
-      assert_raises(ArgumentError){ @array.uniq!(1) }
-   end
+  test "uniq handles explict nil and false values as expected" do 
+    assert_equal([nil], [nil].uniq)
+    assert_equal([false], [false].uniq)
+  end
 
-   def teardown
-      @array = nil
-   end
+  test "calling uniq on an empty array returns an empty array" do
+    assert_equal([], [].uniq)
+  end
+
+  test "passing the wrong number of arguments raises an error" do
+    assert_raise(ArgumentError){ @array.uniq(1) }
+  end
+
+  def teardown
+    @array = nil
+  end
 end
