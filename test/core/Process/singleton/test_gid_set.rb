00e91334f79496a223d92797ca963991fe5830ba
Daniel Berger <dberger@globe.gov>
2011-01-27 17:49:14 -0700

    Updated Process.gid tests, and split out Process.gid= tests into their own suite.

---
+++ test/core/Process/singleton/test_gid_set.rb
@@ -0,0 +1,45 @@
+######################################################################
+# test_gid_set.rb
+#
+# Test case for the Process.gid= method. Note that these tests are
+# skipped unless run as root on a non-Windows platform.
+######################################################################
+require 'test/helper'
+require 'test/unit'
+
+class TC_Process_GidSet_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
+
+  def setup
+    unless WINDOWS
+      @local_gid = Etc.getgrnam('staff').gid
+      @login_gid = Etc.getpwnam(Etc.getlogin).gid
+    end
+  end
+
+  test "gid= basic functionality" do
+    assert_respond_to(Process, :gid=)
+  end
+
+  test "gid= sets the gid as expected" do
+    omit_unless(ROOT, "Process.gid= tests skipped except when root")
+    assert_nothing_raised{ Process.gid = @local_gid }
+    assert_equal(@local_gid, Process.gid)
+    assert_nothing_raised{ Process.gid = @login_gid }
+    assert_equal(@login_gid, Process.gid)
+  end
+
+  test "gid= returns the value that was assigned" do
+    omit_unless(ROOT, "Process.gid= tests skipped except when root")
+    assert_equal(@local_gid, Process.gid = @local_gid)
+  end
+
+  test "gid= requires a numeric argument" do
+    assert_raises(TypeError){ Process.gid = "bogus" }
+  end
+
+  def teardown
+    @local_gid = nil
+    @login_gid = nil
+  end
+end
