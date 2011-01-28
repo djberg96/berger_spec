282a284321a2ff7bc6149b5bc9838e866ecbf3fe
Daniel Berger <djberg96@gmail.com>
2011-01-27 21:11:52 -0700

    Refactored the Process.euid and Process.euid= tests.

--- test/core/Process/singleton/test_euid.rb
+++ test/core/Process/singleton/test_euid.rb
@@ -1,46 +1,36 @@
 ######################################################################
-# tc_euid.rb
+# test_euid.rb
 #
-# Test case for the Process.euid and Process.euid= module methods.
-# For now these tests are for UNIX platforms only.
+# Test case for the Process.euid method.
 ######################################################################
 require 'test/helper'
 require 'test/unit'
-require 'etc' rescue nil
 
-class TC_Process_Euid_ModuleMethod < Test::Unit::TestCase
-   include Test::Helper
+class TC_Process_Euid_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def setup
-      @euid = nil
-      @user = Etc.getpwnam('nobody')
-   end
+  def setup
+    @euid = nil
+    @user = Etc.getpwnam('nobody')
+  end
 
-   def test_euid_basic
-      assert_respond_to(Process, :euid)
-      assert_respond_to(Process, :euid=)
-   end
+  test "euid basic functionality" do
+    assert_respond_to(Process, :euid)
+  end
 
-   unless WINDOWS
-      def test_euid
-         assert_nothing_raised{ Process.euid }
-         assert_kind_of(Fixnum, Process.euid)
-      end
+  test "euid returns the expected results" do
+    omit_if(WINDOWS, "Process.euid test skipped on MS Windows")
+    assert_nothing_raised{ Process.euid }
+    assert_kind_of(Fixnum, Process.euid)
+    assert_true(Process.euid < 100000)
+  end
 
-      # This test will only run if run as root
-      if Process.euid == 0
-         def test_euid_set
-            assert_nothing_raised{ @euid = Process.euid }
-            assert_nothing_raised{ Process.euid = @user.gid }
-            assert_equal(@user.gid, Process.euid)
-            assert_nothing_raised{ Process.euid = @euid }
-            assert_equal(@euid, Process.euid)
-         end
-      end
-   end
+  test "euid does not accept any arguments" do
+    assert_raise(ArgumentError){ Process.euid(1) }
+  end
 
-   def teardown
-      @euid = nil
-      @user = nil
-   end
+  def teardown
+    @euid = nil
+    @user = nil
+  end
 end
---
+++ test/core/Process/singleton/test_euid_set.rb
@@ -0,0 +1,43 @@
+######################################################################
+# test_euid_set.rb
+#
+# Test case for the Process.euid= method.
+######################################################################
+require 'test/helper'
+require 'test/unit'
+
+class TC_Process_Euid_Set_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
+
+  def setup
+    @euid = nil
+    @user = Etc.getpwnam('nobody')
+  end
+
+  test "euid= basic functionality" do
+    assert_respond_to(Process, :euid=)
+  end
+
+  test "euid= works as expected" do
+    omit_unless(ROOT, "Process.euid= tests skipped unless run as root")
+    assert_nothing_raised{ @euid = Process.euid }
+    assert_nothing_raised{ Process.euid = @user.gid }
+    assert_equal(@user.gid, Process.euid)
+    assert_nothing_raised{ Process.euid = @euid }
+    assert_equal(@euid, Process.euid)
+  end
+
+  test "euid= returns the assigned value" do
+    @euid = Process.euid
+    assert_equal(@euid, Process.euid = @euid)
+  end
+
+  test "euid= requires a numeric argument" do
+    assert_raise(TypeError){ Process.euid = "test" }
+  end
+
+  def teardown
+    @euid = nil
+    @user = nil
+  end
+end
