c97dc61b79d7f2231547c2810d171d1c6a76c80e
Daniel Berger <dberger@globe.gov>
2011-02-08 12:39:44 -0700

    Added tests for the Process.groups method.

---
+++ test/core/Process/singleton/test_groups.rb
@@ -0,0 +1,29 @@
+######################################################################
+# test_groups.rb
+#
+# Test case for the Process.groups module method.
+######################################################################
+require 'test/helper'
+require 'test/unit'
+
+class TC_Process_Groups_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
+
+  test "groups basic functionality" do
+    assert_respond_to(Process, :groups)
+  end
+
+  test "groups returns the expected results" do
+    omit_if(WINDOWS, "Process.groups test skipped on Windows")
+    assert_kind_of(Array, Process.groups)
+    assert_true(Process.groups.size > 0)
+  end
+
+  test "the array returned by the groups method contains only integers" do
+    Process.groups.each{ |group| assert_kind_of(Numeric, group) }
+  end
+
+  test "groups does not accept any arguments" do
+    assert_raises(ArgumentError){ Process.groups(0) }
+  end
+end
