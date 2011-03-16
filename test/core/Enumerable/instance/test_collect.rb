c889997487452fee669465e4b4ed77d9382bdf8a
Daniel Berger <dberger@globe.gov>
2011-03-16 09:21:26 -0600

    Refactored the Enumerable#collect tests.

--- test/core/Enumerable/instance/test_collect.rb
+++ test/core/Enumerable/instance/test_collect.rb
@@ -1,5 +1,5 @@
 #########################################################################
-# tc_collect.rb
+# test_collect.rb
 #
 # Test suite for the Enumerable#collect instance method and the
 # Enumerable#map alias.
@@ -11,55 +11,61 @@ require 'test/helper'
 require 'test/unit'
 
 class MyEnumCollect
-   include Enumerable
+  include Enumerable
 
-   attr_accessor :arg1, :arg2, :arg3
+  attr_accessor :arg1, :arg2, :arg3
 
-   def initialize(arg1=1, arg2=2, arg3=3)
-      @arg1 = arg1
-      @arg2 = arg2
-      @arg3 = arg3
-   end
+  def initialize(arg1=1, arg2=2, arg3=3)
+    @arg1 = arg1
+    @arg2 = arg2
+    @arg3 = arg3
+  end
 
-   def each
-      yield @arg1
-      yield @arg2
-      yield @arg3
-   end
+  def each
+    yield @arg1
+    yield @arg2
+    yield @arg3
+  end
 end
 
-class TC_Enumerable_Collect_Instance < Test::Unit::TestCase
-   def setup
-      @enum = MyEnumCollect.new
-   end
+class TC_Enumerable_Collect_InstanceMethod < Test::Unit::TestCase
+  def setup
+    @enum = MyEnumCollect.new(1, 2, 3)
+  end
 
-   def test_collect_basic
-      assert_respond_to(@enum, :collect)
-      assert_nothing_raised{ @enum.collect }
-   end
+  test "collect basic functionality" do
+    assert_respond_to(@enum, :collect)
+    assert_nothing_raised{ @enum.collect }
+  end
 
-   def test_collect
-      assert_equal([2,3,4], @enum.collect{ |e| e += 1 })
-      assert_equal([7,7,7], @enum.collect{ 7 })
-   end
+  test "collect returns the expected results" do
+    assert_equal([2,3,4], @enum.collect{ |e| e += 1 })
+    assert_equal([7,7,7], @enum.collect{ 7 })
+  end
 
-   def test_collect_edge_cases
-      assert_equal(['a','a','a'], @enum.collect{ 'a' })
-      assert_equal([nil, nil, nil], @enum.collect{})
-      assert_equal([1, 2, 3], @enum.collect)
-   end
+  test "collect honors custom each method definition" do
+    assert_equal(['a','a','a'], @enum.collect{ 'a' })
+  end
 
-   def test_map_alias
-      msg = '=> Known issue in MRI'
-      assert_respond_to(@enum, :map)
-      assert_equal(true, @enum.method(:collect) == @enum.method(:map), msg) 
-   end
+  test "collect yields an array of nils when given an empty block" do
+    assert_equal([nil, nil, nil], @enum.collect{})
+  end
 
-   def test_collect_expected_errors
-      assert_raise(ArgumentError){ @enum.collect(5) }
-   end
+  test "collect with no block returns the original object" do
+    assert_equal([1, 2, 3], @enum.collect)
+  end
 
-   def teardown
-      @enum = nil
-   end
+  test "map is an alias for collect" do
+    msg = '=> Known issue in MRI'
+    assert_respond_to(@enum, :map)
+    assert_alias_method(@enum, :collect, :map, msg)
+  end
+
+  test "collect does not accept any arguments" do
+    assert_raise(ArgumentError){ @enum.collect(5) }
+  end
+
+  def teardown
+    @enum = nil
+  end
 end
