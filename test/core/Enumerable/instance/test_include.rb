e5c2c2d9623960752a66120077f64be7b803d565
Daniel Berger <dberger@globe.gov>
2011-03-16 16:03:34 -0600

    Refactored tests for Enumerable#include?

--- test/core/Enumerable/instance/test_include.rb
+++ test/core/Enumerable/instance/test_include.rb
@@ -1,5 +1,5 @@
 #########################################################################
-# tc_include.rb
+# test_include.rb
 #
 # Test suite for the Enumerable#include instance method and the
 # Enumerable#member? alias.
@@ -8,60 +8,60 @@ require 'test/helper'
 require 'test/unit'
 
 class MyEnumInclude
-   include Enumerable
+  include Enumerable
 
-   attr_accessor :arg1, :arg2, :arg3
+  attr_accessor :arg1, :arg2, :arg3
 
-   def initialize(arg1, arg2, arg3)
-      @arg1 = arg1
-      @arg2 = arg2
-      @arg3 = arg3
-   end
+  def initialize(arg1, arg2, arg3)
+    @arg1 = arg1
+    @arg2 = arg2
+    @arg3 = arg3
+  end
 
-   def each
-      yield @arg1
-      yield @arg2
-      yield @arg3
-   end
+  def each
+    yield @arg1
+    yield @arg2
+    yield @arg3
+  end
 end
 
 class TC_Enumerable_Include_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @enum = MyEnumInclude.new('alpha', 7, 'beta')
-   end
+  def setup
+    @enum = MyEnumInclude.new('alpha', 7, 'beta')
+  end
 
-   def test_include_basic
-      assert_respond_to(@enum, :include?)
-      assert_nothing_raised{ @enum.include?(1) }
-   end
+  test "include? basic functionality" do
+    assert_respond_to(@enum, :include?)
+    assert_nothing_raised{ @enum.include?(1) }
+  end
 
-   def test_include
-      assert_equal(true, @enum.include?('alpha'))
-      assert_equal(true, @enum.include?(7))
-      assert_equal(true, @enum.include?('beta'))
-      assert_equal(false, @enum.include?('7'))
-      assert_equal(false, @enum.include?(8))
-   end
+  test "include? returns the expected results" do
+    assert_true(@enum.include?('alpha'))
+    assert_true(@enum.include?(7))
+    assert_true(@enum.include?('beta'))
+    assert_false(@enum.include?('7'))
+    assert_false(@enum.include?(8))
+  end
 
-   def test_include_explicit_false_and_nil
-      @enum = MyEnumInclude.new(true, false, nil)
-      assert_equal(true, @enum.include?(true))
-      assert_equal(true, @enum.include?(false))
-      assert_equal(true, @enum.include?(nil))
-   end
+  test "include? works as expected with explicit nil and false members" do
+    @enum = MyEnumInclude.new(true, false, nil)
+    assert_true(@enum.include?(true))
+    assert_true(@enum.include?(false))
+    assert_true(@enum.include?(nil))
+  end
 
-   def test_member_alias
-      msg = '=> Known issue in MRI'
-      assert_respond_to(@enum, :member?)
-      assert_equal(true, @enum.method(:member?) == @enum.method(:include?),msg)
-   end
+  test "member? is an alias for include?" do
+    msg = '=> Known issue in MRI'
+    assert_respond_to(@enum, :member?)
+    assert_alias_method(@enum, :member?, :include?)
+  end
 
-   def test_include_expected_errors
-      assert_raise(ArgumentError){ @enum.include? }
-      assert_raise(ArgumentError){ @enum.include?(5, 7) }
-   end
+  test "include? requires one and only one argument" do
+    assert_raise(ArgumentError){ @enum.include? }
+    assert_raise(ArgumentError){ @enum.include?(5, 7) }
+  end
 
-   def teardown
-      @enum   = nil
-   end
+  def teardown
+    @enum   = nil
+  end
 end
