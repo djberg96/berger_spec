5901956d9e8aa29a16070a91de4d7a2217971d06
Daniel Berger <dberger@globe.gov>
2011-03-18 09:39:31 -0600

    Refactored the Enumerable#all? tests.

--- test/core/Enumerable/instance/test_all.rb
+++ test/core/Enumerable/instance/test_all.rb
@@ -1,5 +1,5 @@
 ######################################################################
-# tc_all.rb
+# test_all.rb
 #
 # Test case for the Enumerable#all? instance method.
 ######################################################################
@@ -7,43 +7,49 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Enumerable_All_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @enum = ['a', 'b', 'c']
-   end
+  def setup
+    @enum = ['a', 'b', 'c']
+  end
 
-   def test_all_basic
-      assert_respond_to(@enum, :all?)
-      assert_nothing_raised{ @enum.all? }
-      assert_nothing_raised{ @enum.all?{ } }
-   end
+  test "all? basic functionality" do
+    assert_respond_to(@enum, :all?)
+    assert_nothing_raised{ @enum.all? }
+    assert_nothing_raised{ @enum.all?{ } }
+  end
 
-   def test_all_no_block
-      assert_equal(true, [1, 2, 3].all?)
-      assert_equal(false, [nil, false, true].all?)
-      assert_equal(false, [nil, false].all?)
-   end
+  test "all? without a block returns the expected result" do
+    assert_true([1, 2, 3].all?)
+    assert_false([nil, false, true].all?)
+    assert_false([nil, false].all?)
+  end
 
-   def test_all_with_block
-      assert_equal(false, [1, 2, 3].all?{ |e| e > 1 })
-      assert_equal(true, [1, 2, 3].all?{ |e| e > 0 })
-   end
+  test "all? with a block returns the expected result" do
+    assert_false([1, 2, 3].all?{ |e| e > 1 })
+    assert_true([1, 2, 3].all?{ |e| e > 0 })
+  end
 
-   def test_all_with_explicit_false_and_nil
-      assert_equal(true, [nil].all?{ |e| e.nil? })
-      assert_equal(true, [false].all?{ |e| e == false })
-   end
+  test "all? with an explicit nil or false works as expected" do
+    assert_true([nil].all?{ |e| e.nil? })
+    assert_true([false].all?{ |e| e == false })
+  end
 
-   def test_all_edge_cases
-      assert_equal(true, [].all?)
-      assert_equal(true, [0].all?)
-      assert_equal(true, [true].all?)
-   end
+  test "calling all? on an empty array returns true" do
+    assert_equal(true, [].all?)
+  end
 
-   def test_all_expected_errors
-      assert_raise(ArgumentError){ [1, 2, 3].all?(1) }
-   end
+  test "calling all? on a zero element returns true" do
+    assert_equal(true, [0].all?)
+  end
 
-   def teardown
-      @enum = nil
-   end
+  test "calling all? on an explicitly true element returns true" do
+    assert_equal(true, [true].all?)
+  end
+
+  test "all? does not accept any arguments" do
+    assert_raise(ArgumentError){ [1, 2, 3].all?(1) }
+  end
+
+  def teardown
+    @enum = nil
+  end
 end
