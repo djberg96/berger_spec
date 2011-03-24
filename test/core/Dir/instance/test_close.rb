89931b8899cf06c22e2b2704f095d8d6fd8dac82
Daniel Berger <dberger@globe.gov>
2011-03-24 09:40:12 -0600

    Refactored tests for Dir#close and Dir#path, minor update for Dir#each.

--- test/core/Dir/instance/test_close.rb
+++ test/core/Dir/instance/test_close.rb
@@ -1,5 +1,5 @@
 ######################################################################
-# tc_close.rb
+# test_close.rb
 #
 # Test case for the Dir#close instance method
 ######################################################################
@@ -7,32 +7,34 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Dir_Close_Instance < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @dirname = "bogus"
-      system("mkdir #{@dirname}")
-      @dir = Dir.new(@dirname)
-   end
+  def setup
+    @dirname = "bogus"
+    system("mkdir #{@dirname}")
+    @dir = Dir.new(@dirname)
+  end
 
-   def test_close_basic
-      assert_respond_to(@dir, :close)
-      assert_nothing_raised{ @dir.close }
-   end
+  test "close basic functionality" do
+    assert_respond_to(@dir, :close)
+    assert_nothing_raised{ @dir.close }
+  end
 
-   def test_close
-      assert_equal(nil, @dir.close)
-   end
+  test "close returns nil" do
+    assert_equal(nil, @dir.close)
+  end
 
-   def test_close_expected_errors
-      assert_raises(ArgumentError){ @dir.close(1) }
+  test "calling close on a closed dir object raises an IO error" do
+    assert_nothing_raised{ @dir.close }
+    assert_raises(IOError){ @dir.close }
+  end
 
-      @dir.close
-      assert_raises(IOError){ @dir.close }
-   end
+  test "close does not accept any parameters" do
+    assert_raises(ArgumentError){ @dir.close(1) }
+  end
 
-   def teardown
-      @dir = nil
-      remove_dir(@dirname)
-   end
+  def teardown
+    @dir = nil
+    remove_dir(@dirname)
+  end
 end
--- test/core/Dir/instance/test_path.rb
+++ test/core/Dir/instance/test_path.rb
@@ -1,5 +1,5 @@
 #########################################################
-# tc_path.rb
+# test_path.rb
 #
 # Test suite for the Dir#path instance method.
 #########################################################
@@ -7,40 +7,39 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Dir_Path_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @dir_dot = Dir.new('.')
-      @dir_pwd = Dir.new(Dir.pwd)
-   end
+  def setup
+    @dir_dot = Dir.new('.')
+    @dir_pwd = Dir.new(Dir.pwd)
+  end
 
-   def test_path_basic
-      assert_respond_to(@dir_dot, :path)
-      assert_nothing_raised{ @dir_dot.path }
-      assert_kind_of(String, @dir_dot.path)
-   end
-   
-   def test_path
-      assert_equal('.', @dir_dot.path)
-      assert_equal(Dir.pwd, @dir_pwd.path)
-   end
-   
-   def test_expected_errors
-      assert_raises(ArgumentError){ @dir_dot.path('foo') }
-   end
-   
-   if WINDOWS
-      def test_windows_paths
-         std_dir = "C:\\"
-         dir = Dir.new(std_dir)
-         assert_equal("C:\\", dir.path)
-      end
-   end
-   
-   def teardown
-      @dir_dot.close
-      @dir_pwd.close
-      @dir_dot = nil
-      @dir_pwd = nil
-   end
+  test "path basic functionality" do
+    assert_respond_to(@dir_dot, :path)
+    assert_nothing_raised{ @dir_dot.path }
+    assert_kind_of(String, @dir_dot.path)
+  end
+
+  test "path returns expected results" do
+    assert_equal('.', @dir_dot.path)
+    assert_equal(Dir.pwd, @dir_pwd.path)
+  end
+
+  test "path does not accept any parameters" do
+    assert_raises(ArgumentError){ @dir_dot.path('foo') }
+  end
+
+  test "path handles windows paths as expected" do
+    omit_unless(WINDOWS, "path test skipped except on MS Windows")
+    std_dir = "C:\\"
+    dir = Dir.new(std_dir)
+    assert_equal("C:\\", dir.path)
+  end
+
+  def teardown
+    @dir_dot.close
+    @dir_pwd.close
+    @dir_dot = nil
+    @dir_pwd = nil
+  end
 end
