8c6faf26545b9523d8acf34625f685c182c99b77
Daniel Berger <dberger@globe.gov>
2011-04-04 15:59:53 -0600

    Refactored Enumerable#each_with_index tests.

--- test/core/Enumerable/instance/test_each_with_index.rb
+++ test/core/Enumerable/instance/test_each_with_index.rb
@@ -1,5 +1,5 @@
 #########################################################################
-# tc_each_with_index.rb
+# test_each_with_index.rb
 #
 # Test suite for the Enumerable#each_with_index instance method.
 #
@@ -10,46 +10,60 @@ require 'test/helper'
 require 'test/unit'
 
 class MyEnumEachWithIndex
-   include Enumerable
+  include Enumerable
 
-   attr_accessor :arg1, :arg2, :arg3
+  attr_accessor :arg1, :arg2, :arg3
 
-   def initialize(arg1='a', arg2='b', arg3='c')
-      @arg1 = arg1
-      @arg2 = arg2
-      @arg3 = arg3
-   end
+  def initialize(arg1='a', arg2='b', arg3='c')
+    @arg1 = arg1
+    @arg2 = arg2
+    @arg3 = arg3
+  end
 
-   def each
-      yield @arg1
-      yield @arg2
-      yield @arg3
-   end
+  def each
+    yield @arg1
+    yield @arg2
+    yield @arg3
+  end
 end
 
 class TC_Enumerable_EachWithIndex_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @enum  = MyEnumEachWithIndex.new
-      @array = []
-   end
+  include Test::Helper
 
-   def test_each_with_index_basic
-      assert_respond_to(@enum, :each_with_index)
-      assert_nothing_raised{ @enum.each_with_index{} }
-   end
+  def setup
+    @enum  = MyEnumEachWithIndex.new
+    @array = []
+  end
 
-   def test_each_with_index
-      assert_nothing_raised{ @enum.each_with_index{ |e, i| @array[i] = e } }
-      assert_equal(['a', 'b', 'c'], @array)
-   end
+  test "each_with_index basic functionality" do
+    assert_respond_to(@enum, :each_with_index)
+    assert_nothing_raised{ @enum.each_with_index{} }
+  end
 
-   def test_expected_errors
+  test "each_with_index returns the expected results" do
+    assert_nothing_raised{ @enum.each_with_index{ |e, i| @array[i] = e } }
+    assert_equal(['a', 'b', 'c'], @array)
+    assert_equal('a', @array[0])
+  end
+
+  test "each_with_index returns the original object if the block is empty" do
+    assert_equal(@enum, @enum.each_with_index{})
+  end
+
+  test "each_with_index does not accept any arguments" do
+    assert_raise(ArgumentError){ @enum.each_with_index(true) }
+  end
+
+  test "each_with_index behaves as expected when no block is provided" do
+    if PRE187
       assert_raise(LocalJumpError){ @enum.each_with_index }
-      assert_raise(ArgumentError){ @enum.each_with_index(true) }
-   end
+    else
+      assert_kind_of(Enumerable::Enumerator, @enum.each_with_index)
+    end
+  end
 
-   def teardown
-      @enum  = nil
-      @array = nil
-   end
+  def teardown
+    @enum  = nil
+    @array = nil
+  end
 end
