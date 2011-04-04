a137eae5a89ed9b053a83f82fee04e30c1fdfff7
Daniel Berger <dberger@globe.gov>
2011-04-04 16:09:21 -0600

    Refactored Dir#read tests.

--- test/core/Dir/instance/test_read.rb
+++ test/core/Dir/instance/test_read.rb
@@ -1,33 +1,45 @@
 ######################################################################
-# tc_read.rb
+# test_read.rb
 #
 # Test case for the Dir#read instance method.
 ######################################################################
 require 'test/helper'
 require "test/unit"
 
-class TC_Dir_Read_Instance < Test::Unit::TestCase
-   def setup
-      @dir = Dir.new(Dir.pwd)
-   end
+class TC_Dir_Read_InstanceMethod < Test::Unit::TestCase
+  def setup
+    @dir = Dir.new(Dir.pwd)
+    @arr = []
+  end
 
-   def test_read_basic
-      assert_respond_to(@dir, :read)
-      assert_nothing_raised{ @dir.read }
-   end
+  test "read basic functionality" do
+    assert_respond_to(@dir, :read)
+    assert_nothing_raised{ @dir.read }
+  end
 
-   def test_read
-      assert_equal(".", @dir.read)
-      assert_equal("..", @dir.read)
-      assert_nothing_raised{ Dir.entries(Dir.pwd).length.times{ @dir.read } }
-      assert_equal(nil, @dir.read)
-   end
+  test "read returns the expected results" do
+    assert_true(@dir.read.is_a?(String))
+    assert_true(@dir.read.is_a?(String))
+  end
 
-   def test_read_expected_errors
-      assert_raises(ArgumentError){ @dir.read(2) }
-   end
+  test "read returns non nil values up to the end of the stream" do
+    Dir.entries(Dir.pwd).length.times{ @arr = @dir.read }
+    assert_true(@arr.all?{ |e| e.is_a?(String) })
+  end
 
-   def teardown
-      @dir = nil
-   end
+  test "reading beyond the end of the handle stream returns nil" do
+    assert_nothing_raised{ 1000.times{ @dir.read } }
+    assert_equal(nil, @dir.read)
+    assert_equal(nil, @dir.read)
+  end
+
+  test "read does not accept any arguments" do
+    assert_raises(ArgumentError){ @dir.read(2) }
+  end
+
+  def teardown
+    @dir.close
+    @dir = nil
+    @arr = nil
+  end
 end
