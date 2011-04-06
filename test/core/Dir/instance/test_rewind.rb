9d5d87c738335a80c7d61a5ae4490f276b3bdac3
Daniel Berger <dberger@globe.gov>
2011-04-06 14:26:38 -0600

    Refactored the Dir#rewind tests.

--- test/core/Dir/instance/test_rewind.rb
+++ test/core/Dir/instance/test_rewind.rb
@@ -1,5 +1,5 @@
 ######################################################################
-# tc_rewind.rb
+# test_rewind.rb
 #
 # Test case for the Dir#rewind instance method.
 ######################################################################
@@ -7,32 +7,39 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Dir_Rewind_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @pwd = pwd_n
-      @dir = Dir.new(@pwd)
-   end
+  def setup
+    @pwd = pwd_n
+    @dir = Dir.new(@pwd)
+  end
 
-   def test_rewind_basic
-      assert_respond_to(@dir, :rewind)
-      assert_nothing_raised{ @dir.rewind }
-   end
+  test "rewind basic functionality" do
+    assert_respond_to(@dir, :rewind)
+    assert_nothing_raised{ @dir.rewind }
+  end
 
-   def test_rewind
-      assert_equal(".", @dir.read)
-      assert_nothing_raised{ @dir.rewind }
-      assert_equal(".", @dir.read)
-      assert_nothing_raised{ 5.times{ @dir.rewind } }
-      assert_kind_of(Dir, @dir.rewind)
-   end
+  test "rewind behaves as expected" do
+    first = @dir.read
+    assert_nothing_raised{ @dir.rewind }
+    assert_equal(0, @dir.tell)
+    assert_equal(first, @dir.read)
+  end
 
-   def test_rewind_expected_failures
-      assert_raises(ArgumentError){ @dir.rewind(2) }
-   end
+  test "rewind returns itself" do
+    assert_kind_of(Dir, @dir.rewind)
+  end
 
-   def teardown
-      @pwd = nil
-      @dir = nil
-   end
+  test "calling rewind multiple times is effectively a no-op" do
+    assert_nothing_raised{ 5.times{ @dir.rewind } }
+  end
+
+  test "rewind does not accept any arguments" do
+    assert_raises(ArgumentError){ @dir.rewind(2) }
+  end
+
+  def teardown
+    @pwd = nil
+    @dir = nil
+  end
 end
