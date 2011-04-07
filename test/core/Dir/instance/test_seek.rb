6dcb9a348ab74f771e8a16838e4294f94b3f5bf4
Daniel Berger <dberger@globe.gov>
2011-04-07 11:07:22 -0600

    Refactored the Dir#seek tests.

--- test/core/Dir/instance/test_seek.rb
+++ test/core/Dir/instance/test_seek.rb
@@ -1,5 +1,5 @@
 ######################################################################
-# tc_seek.rb
+# test_seek.rb
 #
 # Test case for the Dir#seek instance method.
 ######################################################################
@@ -7,31 +7,46 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Dir_Seek_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @pwd = pwd_n
-      @dir = Dir.new(@pwd)
-   end
+  def setup
+    @pwd = pwd_n
+    @dir = Dir.new(@pwd)
+  end
 
-   def test_seek_basic
-      assert_respond_to(@dir, :seek)
-      assert_nothing_raised{ @dir.seek(0) }
-   end
+  test "seek basic functionality" do
+    assert_respond_to(@dir, :seek)
+    assert_nothing_raised{ @dir.seek(0) }
+  end
 
-   # This caused a segfault on Windows for some versions of Ruby.
-   def test_seek
-      assert_equal(".", @dir.read)
-      assert_kind_of(Dir, @dir.seek(0))
-   end
+  # This caused a segfault on Windows on older versions of Ruby.
+  test "seek behaves as expected" do
+    index1 = @dir.tell
+    first  = @dir.read
+    index2 = @dir.tell
+    second = @dir.read
 
-   def test_seek_expected_errors
-      assert_raises(TypeError){ @dir.seek("bogus") }
-      assert_raises(ArgumentError){ @dir.seek(0,0) }
-   end
+    assert_nothing_raised{ @dir.seek(index1) }
+    assert_equal(first, @dir.read)
+    assert_nothing_raised{ @dir.seek(index2) }
+    assert_equal(second, @dir.read)
+  end
 
-   def teardown
-      @pwd = nil
-      @dir = nil
-   end
+  test "seek returns the receiver" do
+    assert_kind_of(Dir, @dir.seek(0))
+  end
+
+  test "seek requires a numeric argument" do
+    assert_raises(TypeError){ @dir.seek("bogus") }
+  end
+
+  test "seek only accepts one argument" do
+    assert_raises(ArgumentError){ @dir.seek(0,0) }
+  end
+
+  def teardown
+    @dir.close if @dir
+    @pwd = nil
+    @dir = nil
+  end
 end
