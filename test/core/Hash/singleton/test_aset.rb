e4f3c0f797ffeef97aefee9618022c8369c5aeee
Daniel Berger <dberger@globe.gov>
2011-04-28 15:54:27 -0600

    Refactored singleton tests.

--- test/core/Hash/singleton/test_aset.rb
+++ test/core/Hash/singleton/test_aset.rb
@@ -1,29 +1,38 @@
 ###########################################################
-# tc_aset.rb
+# test_aset.rb
 #
 # Test suite for the Hash[] class method.
 ##########################################################
 require 'test/helper'
-require "test/unit"
+require 'test/unit'
 
-class TC_Hash_Aset_ClassMethod < Test::Unit::TestCase
-   def test_empty
-      assert_equal({}, Hash[])
-   end
+class TC_Hash_Aset_SingletonMethod < Test::Unit::TestCase
+  test "aset with no arguments returns an empty hash" do
+    assert_equal({}, Hash[])
+  end
 
-   def test_aset_strings
-      assert_equal({"foo"=>1}, Hash["foo"=>1])
-      assert_equal({"foo"=>1}, Hash["foo",1])
-      assert_equal({"foo"=>1, "bar"=>2}, Hash["foo"=>1, "bar"=>2])
-   end
+  test "aset with string keys works as expected" do
+    assert_equal({'foo'=>1}, Hash['foo'=>1])
+    assert_equal({'foo'=>1}, Hash['foo',1])
+    assert_equal({'foo'=>1, 'bar'=>2}, Hash['foo'=>1, 'bar'=>2])
+  end
 
-   def test_aset_symbols
-      assert_equal({:foo=>1}, Hash[:foo=>1])
-      assert_equal({:foo=>1}, Hash[:foo,1])
-      assert_equal({:foo=>1, :bar=>2}, Hash[:foo=>1, :bar=>2])
-   end
+  test "aset with symbol keys works as expected" do
+    assert_equal({:foo=>1}, Hash[:foo=>1])
+    assert_equal({:foo=>1}, Hash[:foo,1])
+    assert_equal({:foo=>1, :bar=>2}, Hash[:foo=>1, :bar=>2])
+  end
 
-   def test_aset_expected_errors
-      assert_raises(ArgumentError){ Hash["foo"] } # Odd number of arguments
-   end
+  test "aset with explicit nil or false keys works as expected" do
+    assert_equal({nil => 1}, Hash[nil => 1])
+    assert_equal({false => 1}, Hash[false => 1])
+  end
+
+  test "aset with nested hash works as expected" do
+    assert_equal({:foo => {:bar => 1}}, Hash[:foo, {:bar => 1}])
+  end
+
+  test "aset requires an even number of arguments" do
+    assert_raises(ArgumentError){ Hash['foo'] }
+  end
 end
--- test/core/Hash/singleton/test_new.rb
+++ test/core/Hash/singleton/test_new.rb
@@ -1,55 +1,55 @@
 ################################################################
-# tc_new.rb
+# test_new.rb
 #
 # Test suite for the Hash.new class method.
 ################################################################
 require 'test/helper'
-require "test/unit"
+require 'test/unit'
 
-class TC_Hash_New_ClassMethod < Test::Unit::TestCase
-   def setup
-      @hash = nil
-   end
+class TC_Hash_New_SingletonMethod < Test::Unit::TestCase
+  def setup
+    @hash = nil
+  end
 
-   def test_basic
-      assert_respond_to(Hash, :new)
-   end
+  test "new basic functionality" do
+    assert_respond_to(Hash, :new)
+    assert_nothing_raised{ Hash.new }
+    assert_kind_of(Hash, Hash.new)
+  end
 
-   def test_new
-      assert_nothing_raised{ Hash.new }
-      assert_kind_of(Hash, Hash.new)
-   end
+  test "new accepts a default value" do
+    assert_nothing_raised{ Hash.new("test") }
+    assert_nothing_raised{ Hash.new(0) }
+    assert_nothing_raised{ Hash.new(nil) }
+    assert_nothing_raised{ Hash.new(false) }
+  end
 
-   def test_new_default_value
-      assert_nothing_raised{ Hash.new("test") }
-      assert_nothing_raised{ Hash.new(0) }
-      assert_nothing_raised{ Hash.new(nil) }
-      assert_nothing_raised{ Hash.new(false) }
-   end
+  test "the default value passed to new is returned if the key is not found" do
+    @hash = Hash.new('test')
+    assert_equal('test', @hash['foo'])
+    assert_equal('test', @hash['bar'])
+  end
 
-   def test_new_default_value_behavior
-      assert_nothing_raised{ @hash = Hash.new("test") }
-      assert_equal("test", @hash["foo"])
-      assert_equal("test", @hash["bar"])
-   end
+  test "new accepts a block" do
+    assert_nothing_raised{ Hash.new{ } }
+    assert_nothing_raised{ Hash.new{ |hash, key| } }
+  end
 
-   def test_new_with_block
-      assert_nothing_raised{ Hash.new{ } }
-      assert_nothing_raised{ Hash.new{ |hash, key| } }
-   end
+  test "the value of the block passed to new is returned if the key is not found" do
+    @hash = Hash.new{ |h,k| h[k] = 'test' }
+    assert_equal('test', @hash['foo'])
+    assert_equal('test', @hash['bar'])
+  end
 
-   def test_new_with_block_behavior
-      assert_nothing_raised{ @hash = Hash.new{ |h,k| h[k] = "test" } }
-      assert_equal("test", @hash["foo"])
-      assert_equal("test", @hash["bar"])
-   end
+  test "new accepts a maximum of one argument" do
+    assert_raise(ArgumentError){ Hash.new(1,2) }
+  end
 
-   def test_expected_errors
-      assert_raise(ArgumentError){ Hash.new(1,2) }  # too many arguments
-      assert_raise(ArgumentError){ Hash.new(0){ } } # arg + block illegal
-   end
+  test "new does not accept both an argument and a block" do
+    assert_raise(ArgumentError){ Hash.new(0){ } }
+  end
 
-   def teardown
-      @hash = nil
-   end
+  def teardown
+    @hash = nil
+  end
 end
