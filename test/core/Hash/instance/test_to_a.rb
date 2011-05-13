963cdee6dbd96868ff877d2c2bdf10bb71e9a33d
Daniel Berger <dberger@globe.gov>
2011-05-13 08:44:11 -0600

    Updated Hash#to_a tests.

--- test/core/Hash/instance/test_to_a.rb
+++ test/core/Hash/instance/test_to_a.rb
@@ -1,38 +1,42 @@
 ############################################################
-# tc_to_a.rb
+# test_to_a.rb
 #
 # Test suite for the Hash#to_a instance method.
 ############################################################
 require 'test/helper'
-require "test/unit"
+require 'test/unit'
 
 class TC_Hash_ToA_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @hash = {"a",1,"b",2,"c",3}
-   end
+  def setup
+    @hash = {"a",1,"b",2,"c",3}
+  end
 
-   def test_to_a_basic
-      assert_respond_to(@hash, :to_a)
-      assert_nothing_raised{ @hash.to_a }
-      assert_kind_of(Array, @hash.to_a)
-   end
+  test "to_a basic functionality" do
+    assert_respond_to(@hash, :to_a)
+    assert_nothing_raised{ @hash.to_a }
+    assert_kind_of(Array, @hash.to_a)
+  end
 
-   def test_to_a
-      assert_equal([["a",1],["b",2],["c",3]], @hash.to_a)
-      assert_equal([], {}.to_a)
-   end
-   
-   def test_to_a_edge_cases
-      assert_equal([['a', 3]], {'a', 1, 'a', 2, 'a', 3}.to_a)
-      assert_equal([[nil, nil]], {nil, nil, nil, nil}.to_a)
-      assert_equal([['a', [1,2]], ['b', [3,4]]], {'a', [1,2], 'b', [3,4]}.to_a)
-   end
+  test "to_a returns expected results" do
+    assert_equal([["a",1],["b",2],["c",3]], @hash.to_a)
+    assert_equal([['a', [1,2]], ['b', [3,4]]], {'a', [1,2], 'b', [3,4]}.to_a)
+    assert_equal([], {}.to_a)
+  end
 
-   def test_to_a_expected_errors
-      assert_raises(ArgumentError){ @hash.to_a(1) }
-   end
+  test "a hash with identical keys returns the expected result" do
+    assert_equal([['a', 3]], {'a', 1, 'a', 2, 'a', 3}.to_a)
+  end
 
-   def teardown
-      @hash = nil
-   end
+  test "a hash of nil or false keys and values returns the expected result" do
+    assert_equal([[nil, nil]], {nil, nil, nil, nil}.to_a)
+    assert_equal([[false, false]], {false, false, false, false}.to_a)
+  end
+
+  test "to_a does not accept any arguments" do
+    assert_raise(ArgumentError){ @hash.to_a(1) }
+  end
+
+  def teardown
+    @hash = nil
+  end
 end
