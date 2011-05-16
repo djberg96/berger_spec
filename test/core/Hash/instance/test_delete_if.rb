d5ab1a0decbdb8a0b8016febf86baf5019b30e4f
Daniel Berger <dberger@globe.gov>
2011-05-16 16:36:29 -0600

    Refactored the Hash#delete_if tests.

--- test/core/Hash/instance/test_delete_if.rb
+++ test/core/Hash/instance/test_delete_if.rb
@@ -1,33 +1,45 @@
 ###########################################################
-# tc_delete_if.rb
+# test_delete_if.rb
 #
 # Test suite for the Hash#delete_if instance method.
 ###########################################################
 require 'test/helper'
-require "test/unit"
+require 'test/unit'
 
-class TC_Hash_DeleteIf_Instance < Test::Unit::TestCase
-   def setup
-      @hash = {:foo, 1, "bar", 2, nil, 3, false, 4}
-   end
+class TC_Hash_DeleteIf_InstanceMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def test_delete_if_basic
-      assert_respond_to(@hash, :delete_if)
-      assert_nothing_raised{ @hash.delete_if{} }
-   end
+  def setup
+    @hash = {:foo, 1, "bar", 2, nil, 3, false, 4}
+  end
 
-   def test_delete_if
-      assert_equal({:foo, 1, "bar", 2}, @hash.delete_if{ |k,v| v > 2 })
-      assert_equal({:foo, 1, "bar", 2}, @hash.delete_if{ |k,v| v > 5 })
-      assert_equal({}, @hash.delete_if{ |k,v| v >= 0 })
-   end
+  test "delete_if basic functionality" do
+    assert_respond_to(@hash, :delete_if)
+    assert_nothing_raised{ @hash.delete_if{} }
+  end
 
-   def test_delete_if_expected_errors
-      assert_raises(LocalJumpError){ @hash.delete_if }
-      assert_raises(ArgumentError){ @hash.delete_if(1){} }
-   end
+  test "delete_if returns the expected results" do
+    assert_equal({:foo, 1, "bar", 2}, @hash.delete_if{ |k,v| v > 2 })
+    assert_equal({:foo, 1, "bar", 2}, @hash.delete_if{ |k,v| v > 5 })
+  end
 
-   def teardown
-      @hash = nil
-   end
+  test "delete_if returns an empty hash if the proc conditions are not met" do
+    assert_equal({}, @hash.delete_if{ |k,v| v >= 0 })
+  end
+
+  test "delete_if with no block behaves as expected" do
+    if PRE187
+      assert_raise(LocalJumpError){ @hash.delete_if }
+    else
+      assert_kind_of(Enumerable::Enumerator, @hash.delete_if)
+    end
+  end
+
+  test "delete_if does not accept any arguments" do
+    assert_raise(ArgumentError){ @hash.delete_if(1){} }
+  end
+
+  def teardown
+    @hash = nil
+  end
 end
