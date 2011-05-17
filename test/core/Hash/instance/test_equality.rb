ce642034ccde35a0b91e45fcb2252df261ae0bb7
Daniel Berger <dberger@globe.gov>
2011-05-17 14:46:26 -0600

    Updated tests for the Hash#== method.

--- test/core/Hash/instance/test_equality.rb
+++ test/core/Hash/instance/test_equality.rb
@@ -1,54 +1,65 @@
 ###########################################################
-# tc_equality.rb
+# test_equality.rb
 #
-# Test suite for the Hash#== instance method.
+# Tests for the Hash#== instance method.
 ###########################################################
 require 'test/helper'
 require 'test/unit'
 
 class TC_Hash_Equality_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  class HEquality
+    def to_hash
+      {'foo' => 1, 'bar' => 2}
+    end
+  end
 
-   class HEquality
-      def to_hash
-         {'foo' => 1, 'bar' => 2}
-      end
-   end
+  def setup
+    @hash1  = {"foo"=>1, "bar"=>2}
+    @hash2  = {"bar"=>2, "foo"=>1}
+    @hash3  = {:foo=>1, :bar=>2}
+    @custom = HEquality.new
+  end
 
-   def setup
-      @hash1  = {"foo"=>1, "bar"=>2}
-      @hash2  = {"bar"=>2, "foo"=>1}
-      @hash3  = {:foo=>1, :bar=>2}
-      @custom = HEquality.new
-   end
+  test "equality basic functionality" do
+    assert_respond_to(@hash1, :==)
+    assert_nothing_raised{ @hash1 == @hash2 }
+    assert_boolean(@hash1 == @hash2)
+  end
 
-   def test_equality_basic
-      assert_respond_to(@hash1, :==)
-      assert_nothing_raised{ @hash1 == @hash2 }
-   end
+  test "a hash is always equal to itself" do
+    assert_true(@hash1 == @hash1)
+  end
 
-   def test_equality_success
-      assert_equal(true, @hash1 == @hash1)
-      assert_equal(true, @hash1 == @hash2)
-      assert_equal(true, {} == {})
-   end
+  test "a hash is equal to another hash if they have same keys and values" do
+    assert_true(@hash1 == @hash2)
+  end
 
-   def test_equality_failure
-      assert_equal(false, @hash1 == @hash3)
-      assert_equal(false, {nil=>1} == {false=>1})
-      assert_equal(false, {true=>1} == {false=>1})
-      assert_equal(false, {nil=>1} == {0=>1})
-   end
+  test "an empty hash is equal to another empty hash" do
+    assert_true({} == {})
+  end
 
-   def test_equality_using_custom_to_hash_method
-      assert_equal(true, @hash1 == @custom)
-      assert_equal(false, {1,2} == @custom)
-   end
+  test "symbols and strings are not considered equal" do
+    assert_false(@hash1 == @hash3)
+  end
 
-   def teardown   
-      @hash1  = nil
-      @hash2  = nil
-      @hash3  = nil
-      @custom = nil
-   end
+  test "false and nil and true are not equal" do
+    assert_false({nil=>1} == {false=>1})
+    assert_false({true=>1} == {false=>1})
+  end
+
+  test "nil and zero are not equal" do
+    assert_false({nil=>1} == {0=>1})
+  end
+
+  test "custom to_hash method is honored when determining equality" do
+    assert_true(@hash1 == @custom)
+    assert_false({1,2} == @custom)
+  end
+
+  def teardown
+    @hash1  = nil
+    @hash2  = nil
+    @hash3  = nil
+    @custom = nil
+  end
 end
