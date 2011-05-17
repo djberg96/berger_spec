dfcd2a361fefe2ca6bd675daf563ac4652da3403
Daniel Berger <dberger@globe.gov>
2011-05-17 14:48:40 -0600

    Updated Hash#has_key tests.

--- test/core/Hash/instance/test_has_key.rb
+++ test/core/Hash/instance/test_has_key.rb
@@ -1,120 +1,46 @@
 #####################################################################
-# tc_has_key.rb
+# test_has_key.rb
 #
-# Test suite for the Hash#has_key? instance method as well as the
-# Hash#include?, Hash#key? and Hash#member? aliases.
+# Tests for the Hash#has_key? instance method.
 #####################################################################
 require 'test/helper'
 require 'test/unit'
 
-class TC_Hash_HasKey_Instance < Test::Unit::TestCase
-   include Test::Helper
-   
-   def setup
-      @hash = {:foo, 1, "bar", 2, nil, 3, false, 4}
-   end
+class TC_Hash_HasKey_InstanceMethod < Test::Unit::TestCase
+  def setup
+    @hash = {:foo, 1, "bar", 2, nil, 3, false, 4}
+  end
 
-   def test_has_key_basic
-      assert_respond_to(@hash, :has_key?)
-      assert_nothing_raised{ @hash.has_key?(:foo) }
-      assert_boolean(@hash.has_key?(1))
-   end
-   
-   def test_key_basic
-      assert_respond_to(@hash, :key?)
-      assert_nothing_raised{ @hash.key?(:foo) }
-      assert_boolean(@hash.key?(1))
-   end
-   
-   def test_include_basic
-      assert_respond_to(@hash, :has_key?)
-      assert_nothing_raised{ @hash.include?(:foo) }
-      assert_boolean(@hash.include?(1))
-   end
-     
-   def test_member_basic
-      assert_respond_to(@hash, :member?)
-      assert_nothing_raised{ @hash.member?(:foo) }
-      assert_boolean(@hash.member?(1))
-   end
+  test "has_key? basic functionality" do
+    assert_respond_to(@hash, :has_key?)
+    assert_nothing_raised{ @hash.has_key?(:foo) }
+    assert_boolean(@hash.has_key?(1))
+  end
 
-   def test_has_key_expected_true
-      assert_equal(true, @hash.has_key?(:foo))
-      assert_equal(true, @hash.has_key?("bar"))
-      assert_equal(true, @hash.has_key?(nil))
-      assert_equal(true, @hash.has_key?(false))
-   end
-   
-   def test_key_expected_true
-      assert_equal(true, @hash.key?(:foo))
-      assert_equal(true, @hash.key?("bar"))
-      assert_equal(true, @hash.key?(nil))
-      assert_equal(true, @hash.key?(false))
-   end
-   
-   def test_include_expected_true
-      assert_equal(true, @hash.has_key?(:foo))
-      assert_equal(true, @hash.has_key?("bar"))
-      assert_equal(true, @hash.has_key?(nil))
-      assert_equal(true, @hash.has_key?(false))
-   end
-      
-   def test_member_expected_true
-      assert_equal(true, @hash.member?(:foo))
-      assert_equal(true, @hash.member?("bar"))
-      assert_equal(true, @hash.member?(nil))
-      assert_equal(true, @hash.member?(false))
-   end
-   
-   def test_has_key_expected_false
-      assert_equal(false, @hash.has_key?(99))
-      assert_equal(false, @hash.has_key?(true))
-      assert_equal(false, @hash.has_key?(1.0))
-      assert_equal(false, @hash.has_key?(:bar))
-   end
-   
-   def test_key_expected_false
-      assert_equal(false, @hash.key?(99))
-      assert_equal(false, @hash.key?(true))
-      assert_equal(false, @hash.key?(1.0))
-      assert_equal(false, @hash.key?(:bar))
-   end
-   
-   def test_include_expected_false
-      assert_equal(false, @hash.include?(99))
-      assert_equal(false, @hash.include?(true))
-      assert_equal(false, @hash.include?(1.0))
-      assert_equal(false, @hash.include?(:bar))
-   end
-      
-   def test_member_expected_false
-      assert_equal(false, @hash.member?(99))
-      assert_equal(false, @hash.member?(true))
-      assert_equal(false, @hash.member?(1.0))
-      assert_equal(false, @hash.member?(:bar))
-   end
+  test "key? is an alias for has_key?" do
+    assert_alias_method(@hash, :key?, :has_key?)
+  end
 
-   def test_has_key_expected_errors
-      assert_raises(ArgumentError){ @hash.has_key? }
-      assert_raises(ArgumentError){ @hash.has_key?(1,2) }
-   end
-   
-   def test_key_expected_errors
-      assert_raises(ArgumentError){ @hash.key? }
-      assert_raises(ArgumentError){ @hash.key?(1,2) }
-   end
-   
-   def test_include_expected_errors
-      assert_raises(ArgumentError){ @hash.include? }
-      assert_raises(ArgumentError){ @hash.include?(1,2) }
-   end
-      
-   def test_member_expected_errors
-      assert_raises(ArgumentError){ @hash.member? }
-      assert_raises(ArgumentError){ @hash.member?(1,2) }
-   end
+  test "has_key? expected true values" do
+    assert_true(@hash.has_key?(:foo))
+    assert_true(@hash.has_key?("bar"))
+    assert_true(@hash.has_key?(nil))
+    assert_true(@hash.has_key?(false))
+  end
 
-   def teardown
-      @hash = nil
-   end
+  test "has_key? expected false values" do
+    assert_false(@hash.has_key?(99))
+    assert_false(@hash.has_key?(true))
+    assert_false(@hash.has_key?(1.0))
+    assert_false(@hash.has_key?(:bar))
+  end
+
+  test "has_key requires one argument only" do
+    assert_raise(ArgumentError){ @hash.has_key? }
+    assert_raise(ArgumentError){ @hash.has_key?(1,2) }
+  end
+
+  def teardown
+    @hash = nil
+  end
 end
