eb9471ff8c8e87ec4b9bffa4ff1a58d841183147
Daniel Berger <dberger@globe.gov>
2011-05-17 15:53:38 -0600

    Updated Hash#reject tests.

--- test/core/Hash/instance/test_reject.rb
+++ test/core/Hash/instance/test_reject.rb
@@ -1,43 +1,48 @@
 ####################################################################
-# tc_reject.rb
+# test_reject.rb
 #
-# Test suite for the Hash#reject and Hash#reject! instance methods.
+# Test suite for the Hash#reject instance method.
 ####################################################################
 require 'test/helper'
-require "test/unit"
+require 'test/unit'
 
-class TC_Hash_Reject_Instance < Test::Unit::TestCase
-   def setup
-      @hash = {:foo, 1, "bar", 2, nil, 3, false, 4}
-   end
+class TC_Hash_Reject_InstanceMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def test_reject_basic
-      assert_respond_to(@hash, :reject)
-      assert_nothing_raised{ @hash.reject{} }
-      assert_nothing_raised{ @hash.reject!{} }
-   end
+  def setup
+    @hash = {:foo, 1, "bar", 2, nil, 3, false, 4}
+  end
 
-   def test_reject
-      assert_equal({:foo, 1, "bar", 2}, @hash.reject{ |k,v| v > 2 })
-      assert_equal(
-         {:foo, 1, "bar", 2, nil, 3, false, 4},
-         @hash.reject{ |k,v| v > 5 }
-      )
-      assert_equal({}, @hash.reject{ |k,v| v >= 0 })
-   end
+  test "reject basic functionality" do
+    assert_respond_to(@hash, :reject)
+    assert_nothing_raised{ @hash.reject{} }
+    assert_nothing_raised{ @hash.reject!{} }
+  end
 
-   def test_reject_bang
-      assert_equal({:foo, 1, "bar", 2}, @hash.reject!{ |k,v| v > 2 })
-      assert_equal({:foo, 1, "bar", 2}, @hash)
-      assert_nil(@hash.reject!{ |k,v| v > 99 })
-   end
+  test "reject returns the expected results" do
+    assert_equal({:foo, 1, "bar", 2}, @hash.reject{ |k,v| v > 2 })
+    assert_equal(@hash, @hash.reject{ |k,v| v > 5 })
+    assert_equal({}, @hash.reject{ |k,v| v >= 0 })
+  end
 
-   def test_reject_expected_errors
-      assert_raises(LocalJumpError){ @hash.reject }
-      assert_raises(ArgumentError){ @hash.reject(1){} }
-   end
+  test "reject handles explicit nil and false properly" do
+    assert_equal({}, {1 => nil, 2 => nil}.reject{ |k,v| v.nil? })
+    assert_equal({}, {1 => false, 2 => false}.reject{ |k,v| v == false })
+  end
 
-   def teardown
-      @hash = nil
-   end
+  test "reject without a block behaves as expected" do
+    if PRE187
+      assert_raise(LocalJumpError){ @hash.reject }
+    else
+      assert_kind_of(Enumerable::Enumerator, @hash.reject)
+    end
+  end
+
+  test "reject does not accept any arguments" do
+    assert_raise(ArgumentError){ @hash.reject(1){} }
+  end
+
+  def teardown
+    @hash = nil
+  end
 end
