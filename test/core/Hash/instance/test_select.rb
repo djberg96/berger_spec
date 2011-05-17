77174855c9eb4ca6351ea20cbd3755804a7d24e7
Daniel Berger <dberger@globe.gov>
2011-05-17 15:59:14 -0600

    Updated Hash#select tests.

--- test/core/Hash/instance/test_select.rb
+++ test/core/Hash/instance/test_select.rb
@@ -1,34 +1,44 @@
 ###########################################################
-# tc_select.rb
+# test_select.rb
 #
 # Test suite for the Hash#select instance method.
 ###########################################################
 require 'test/helper'
-require "test/unit"
+require 'test/unit'
 
-class TC_Hash_Select_Instance < Test::Unit::TestCase
-   def setup
-      @hash = {"a",1,"b",2,"c",3}
-   end
+class TC_Hash_Select_InstanceMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def test_select_basic
-      assert_respond_to(@hash, :select)
-      assert_nothing_raised{ @hash.select{} }
-   end
+  def setup
+    @hash = {"a", 1, "b", 2, "c", 3}
+  end
 
-   def test_select
-      assert_equal([["a",1],["b",2]], @hash.select{ |k,v| v <=2 })
-      assert_equal([["a",1],["b",2],["c",3]], @hash.select{ true })
-      assert_equal([], @hash.select{ false })
-      assert_equal([], @hash.select{ |k,v| v > 99 })
-   end
+  test "select basic functionality" do
+    assert_respond_to(@hash, :select)
+    assert_nothing_raised{ @hash.select{} }
+    assert_kind_of(Array, @hash.select{})
+  end
 
-   def test_select_expected_errors
-      assert_raises(LocalJumpError){ @hash.select }
-      assert_raises(ArgumentError){ @hash.select(1) }
-   end
+  test "select returns expected results" do
+    assert_equal([["a",1],["b",2]], @hash.select{ |k,v| v <=2 })
+    assert_equal([["a",1],["b",2],["c",3]], @hash.select{ true })
+    assert_equal([], @hash.select{ false })
+    assert_equal([], @hash.select{ |k,v| v > 99 })
+  end
 
-   def teardown
-      @hash = nil
-   end
+  test "select behaves as expected if no block is provided" do
+    if PRE187
+      assert_raise(LocalJumpError){ @hash.select }
+    else
+      assert_kind_of(Enumerable::Enumerator, @hash.select)
+    end
+  end
+
+  test "select does not accept any arguments" do
+    assert_raise(ArgumentError){ @hash.select(1) }
+  end
+
+  def teardown
+    @hash = nil
+  end
 end
