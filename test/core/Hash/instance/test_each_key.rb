148a6eb4e3920837d884faa0c00b985596f9ca4d
Daniel Berger <dberger@globe.gov>
2011-05-17 14:11:52 -0600

    Refactored Hash#each_key tests.

--- test/core/Hash/instance/test_each_key.rb
+++ test/core/Hash/instance/test_each_key.rb
@@ -1,43 +1,52 @@
 ###########################################################
-# tc_each_key.rb
+# test_each_key.rb
 #
 # Test suite for the Hash#each_key instance method.
 ###########################################################
 require 'test/helper'
-require "test/unit"
+require 'test/unit'
 
-class TC_Hash_EachKey_Instance < Test::Unit::TestCase
-   def setup
-      @hash = {"ant", 1, "bat", 2, "cat", 3, "dog", 4}
-   end
+class TC_Hash_EachKey_InstanceMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def test_each_basic
-      assert_respond_to(@hash, :each_key)
-      assert_nothing_raised{ @hash.each_key{} }
-   end
+  def setup
+    @hash = {'ant', 1, 'bat', 2, 'cat', 3, 'dog', 4}
+  end
 
-   def test_each_iterate
-      i = 0
-      @hash.each_key{ |key|
-         assert_equal(true, ["ant","bat","cat","dog"].include?(key))
-         i += 1
-      }
-      assert_equal(4, i)
-   end
+  test 'each_key basic functionality' do
+    assert_respond_to(@hash, :each_key)
+    assert_nothing_raised{ @hash.each_key{} }
+  end
 
-   def test_each_noop_on_empty
-      i = 0
-      {}.each_key{ i += 1 }
-      assert_equal(0, i)
-      assert_equal(@hash, @hash.each_key{})
-   end
+  test 'each_key iterates as expected' do
+    i = 0
+    @hash.each_key{ |key|
+      assert_true(['ant','bat','cat','dog'].include?(key))
+      i += 1
+    }
+    assert_equal(4, i)
+  end
 
-   def test_each_expected_errors
-      assert_raises(ArgumentError){ @hash.each_key(1){} }
-      assert_raises(LocalJumpError){ @hash.each_key }
-   end
+  test "calling each_key on an empty hash is a no-op" do
+    i = 0
+    {}.each_key{ i += 1 }
+    assert_equal(0, i)
+    assert_equal(@hash, @hash.each_key{})
+  end
 
-   def teardown
-      @hash = nil
-   end
+  test "each_key does not accept any arguments" do
+    assert_raise(ArgumentError){ @hash.each_key(1){} }
+  end
+
+  test "each_key without a block behaves as expected" do
+    if PRE187
+      assert_raise(LocalJumpError){ @hash.each_key }
+    else
+      assert_kind_of(Enumerable::Enumerator, @hash.each_key)
+    end
+  end
+
+  def teardown
+    @hash = nil
+  end
 end
