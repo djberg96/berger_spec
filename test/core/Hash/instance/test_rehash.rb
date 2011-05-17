f1f33099a3252af61a0ce0dc9e61c982d95a59a3
Daniel Berger <dberger@globe.gov>
2011-05-17 15:39:31 -0600

    Updated Hash#rehash tests.

--- test/core/Hash/instance/test_rehash.rb
+++ test/core/Hash/instance/test_rehash.rb
@@ -1,50 +1,50 @@
 ############################################################
-# tc_rehash.rb
+# test_rehash.rb
 #
 # Test suite for the Hash#rehash instance method.
 ############################################################
 require 'test/helper'
-require "test/unit"
+require 'test/unit'
 
-class TC_Hash_Rehash_Instance < Test::Unit::TestCase
-   def setup
-      @array = ['a', 'b']
-      @hash  = {@array, 100}
-   end
+class Test_Hash_Rehash_InstanceMethod < Test::Unit::TestCase
+  def setup
+    @array = ['a', 'b']
+    @hash  = {@array, 100}
+  end
 
-   def test_rehash_basic
-      assert_respond_to(@hash, :rehash)
-      assert_nothing_raised{ @hash.rehash }
-   end
+  test "rehash basic functionality" do
+    assert_respond_to(@hash, :rehash)
+    assert_nothing_raised{ @hash.rehash }
+    assert_kind_of(Hash, @hash.rehash)
+  end
 
-   def test_rehash
-      assert_equal(100, @hash[@array])          # Check index
-      assert_equal({['a', 'b'] => 100}, @hash)  # Check hash itself
+  test "rehash returns the expected results" do
+    assert_equal(100, @hash[@array])          # Check index
+    assert_equal({['a', 'b'] => 100}, @hash)  # Check hash itself
 
-      assert_nothing_raised{ @array[0] = 'c' }  # Change the object
-      assert_equal(nil, @hash[@array])          # Nil until rehashed
+    assert_nothing_raised{ @array[0] = 'c' }  # Change the object
+    assert_equal(nil, @hash[@array])          # Nil until rehashed
 
-      assert_nothing_raised{ @hash.rehash }     # Rehash the hash
-      assert_equal({['c','b'] => 100}, @hash)   # Check hash again
-   end
+    assert_nothing_raised{ @hash.rehash }     # Rehash the hash
+    assert_equal({['c','b'] => 100}, @hash)   # Check hash again
+  end
 
-   # Try to break it using nils within objects
-   def test_rehash_edge_cases
-      obj = [nil, nil]
+  test "rehashing a hash with explicit nils works as expected" do
+    obj = [nil, nil]
 
-      assert_nothing_raised{ @hash = {obj => 100} }
-      assert_equal(100, @hash[obj])
-      assert_equal({[nil,nil] => 100}, @hash)
+    assert_nothing_raised{ @hash = {obj => 100} }
+    assert_equal(100, @hash[obj])
+    assert_equal({[nil,nil] => 100}, @hash)
 
-      obj[0] = 0
+    obj[0] = 0
 
-      assert_equal(nil, @hash[obj])
-      assert_nothing_raised{ @hash.rehash }
-      assert_equal({[0, nil] => 100}, @hash)
-   end
+    assert_equal(nil, @hash[obj])
+    assert_nothing_raised{ @hash.rehash }
+    assert_equal({[0, nil] => 100}, @hash)
+  end
 
-   def teardown
-      @array = nil
-      @hash  = nil
-   end
+  def teardown
+    @array = nil
+    @hash  = nil
+  end
 end
