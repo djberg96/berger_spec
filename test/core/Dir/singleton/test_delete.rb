ccd432977448222e4ed55a0822e2da5d97514701
Daniel Berger <dberger@globe.gov>
2011-05-24 08:58:07 -0600

    Updated the Dir.delete and Dir.entries tests.

--- test/core/Dir/singleton/test_delete.rb
+++ test/core/Dir/singleton/test_delete.rb
@@ -1,5 +1,5 @@
 ######################################################################
-# tc_delete.rb
+# test_delete.rb
 #
 # Test case for the Dir.delete class method.  This also covers the
 # Dir.rmdir and Dir.unlink aliases.
@@ -7,63 +7,50 @@
 require 'test/helper'
 require 'test/unit'
 
-class TC_Dir_Delete_Class < Test::Unit::TestCase
-   include Test::Helper
+class TC_Dir_Delete_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def setup
-      @cur_dir = base_dir(__FILE__)
-      @new_dir = File.join(@cur_dir, 'bogus')
-      Dir.mkdir(@new_dir)
-   end
+  def setup
+    @cur_dir = base_dir(__FILE__)
+    @new_dir = File.join(@cur_dir, 'bogus')
+    Dir.mkdir(@new_dir)
+  end
 
-   def test_delete_basic
-      assert_respond_to(Dir, :delete)
-      assert_nothing_raised{ Dir.delete(@new_dir) }
-   end
+  test "delete basic functionality" do
+    assert_respond_to(Dir, :delete)
+    assert_nothing_raised{ Dir.delete(@new_dir) }
+  end
 
-   def test_rmdir_basic
-      assert_respond_to(Dir, :rmdir)
-      assert_nothing_raised{ Dir.rmdir(@new_dir) }
-   end
+  test "rmdir is an alias for delete" do
+    assert_respond_to(Dir, :rmdir)
+    assert_alias_method(Dir, :rmdir, :delete)
+  end
 
-   def test_unlink_basic
-      assert_respond_to(Dir, :unlink)
-      assert_nothing_raised{ Dir.unlink(@new_dir) }
-   end
+  test "unlink is an alias for delete" do
+    assert_respond_to(Dir, :unlink)
+    assert_alias_method(Dir, :rmdir, :delete)
+  end
 
-   def test_delete
-      assert_equal(0, Dir.delete(@new_dir))
-   end
+  test "delete returns zero upon success" do
+    assert_equal(0, Dir.delete(@new_dir))
+  end
 
-   def test_rmdir
-      assert_equal(0, Dir.rmdir(@new_dir))
-   end
+  test "delete requires a string argument" do
+    assert_raise(TypeError){ Dir.delete(1) }
+  end
 
-   def test_unlink
-      assert_equal(0, Dir.rmdir(@new_dir))
-   end
+  test "delete requires one argument only" do
+    assert_raise(ArgumentError){ Dir.delete }
+    assert_raise(ArgumentError){ Dir.delete(@new_dir, @new_dir) }
+  end
 
-   def test_delete_expected_errors
-      assert_raise(TypeError){ Dir.delete(1) }
-      assert_raise(ArgumentError){ Dir.delete(@new_dir, @new_dir) }
-      assert_raise_kind_of(SystemCallError){ Dir.delete(@cur_dir) }
-   end
+  test "delete cannot delete the current process directory" do
+    assert_raise_kind_of(SystemCallError){ Dir.delete(@cur_dir) }
+  end
 
-   def test_rmdir_expected_errors
-      assert_raise(TypeError){ Dir.rmdir(1) }
-      assert_raise(ArgumentError){ Dir.rmdir(@new_dir, @new_dir) }
-      assert_raise_kind_of(SystemCallError){ Dir.rmdir(@cur_dir) }
-   end
-
-   def test_unlink_expected_errors
-      assert_raise(TypeError){ Dir.unlink(1) }
-      assert_raise(ArgumentError){ Dir.unlink(@new_dir, @new_dir) }
-      assert_raise_kind_of(SystemCallError){ Dir.unlink(@cur_dir) }
-   end
-
-   def teardown
-      remove_dir(@new_dir) if File.exists?(@new_dir)
-      @cur_dir = nil
-      @new_dir = nil
-   end
+  def teardown
+    remove_dir(@new_dir) if File.exists?(@new_dir)
+    @cur_dir = nil
+    @new_dir = nil
+  end
 end
