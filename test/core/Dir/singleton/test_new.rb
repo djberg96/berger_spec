2ed6e93d540d71cace11cf27816a276971f408cd
Daniel Berger <djberg96@gmail.com>
2011-12-21 08:07:14 -0700

    Updated Dir.new tests.

--- test/core/Dir/singleton/test_new.rb
+++ test/core/Dir/singleton/test_new.rb
@@ -1,40 +1,43 @@
 ##############################################
-# tc_new.rb
+# test_new.rb
 #
 # Test suite for the Dir.new class method.
 ##############################################
-require "test/helper"
-require "test/unit"
+require 'test/helper'
+require 'test/unit'
 
-class TC_Dir_New_Class < Test::Unit::TestCase
-   include Test::Helper
+class TC_Dir_New_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def setup
-      @dir = base_file(__FILE__, "test")
-      Dir.mkdir(@dir)
-   end
-   
-   def test_new_basic
-      assert_respond_to(Dir, :new)
-      assert_nothing_raised{ Dir.new(@dir) }
-      assert_kind_of(Dir, Dir.new(@dir))
-   end
+  def setup
+    @dir = base_file(__FILE__, "test")
+    Dir.mkdir(@dir)
+  end
 
-   # Dir.open with no block is a synonym for Dir.new
-   def test_new_alias
-      assert_respond_to(Dir, :open)
-      assert_nothing_raised{ Dir.open(@dir) }
-      assert_kind_of(Dir, Dir.open(@dir))
-   end
-   
-   def test_new_expected_errors
-      assert_raise(ArgumentError){ Dir.new }
-      assert_raise(TypeError){ Dir.new(nil) }
-      assert_raise(TypeError){ Dir.new(true) }
-      assert_raise_kind_of(SystemCallError){ Dir.new("bogus") }
-   end
-   
-   def teardown
-      Dir.rmdir(@dir) if File.exists?(@dir)
-   end
+  test "new basic functionality" do
+    assert_respond_to(Dir, :new)
+    assert_nothing_raised{ Dir.new(@dir) }
+  end
+
+  test "new returns a Dir object" do
+    assert_kind_of(Dir, Dir.new(@dir))
+  end
+
+  test "new requires one argument only" do
+    assert_raise(ArgumentError){ Dir.new }
+    assert_raise(ArgumentError){ Dir.new(@dir, @dir) }
+  end
+
+  test "the argument to new must actually exist" do
+    assert_raise_kind_of(SystemCallError){ Dir.new("bogus") }
+  end
+
+  test "the argument to new must be a string" do
+    assert_raise(TypeError){ Dir.new(nil) }
+    assert_raise(TypeError){ Dir.new(true) }
+  end
+
+  def teardown
+    Dir.rmdir(@dir) if File.exists?(@dir)
+  end
 end
