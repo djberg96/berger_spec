1609ae6fdc638e2628cf6108265013880e7d1bcf
Daniel Berger <djberg96@gmail.com>
2011-12-21 06:45:38 -0700

    Updated the Dir.foreach tests.

--- test/core/Dir/singleton/test_foreach.rb
+++ test/core/Dir/singleton/test_foreach.rb
@@ -1,44 +1,52 @@
 ######################################################################
-# tc_foreach.rb
+# test_foreach.rb
 #
 # Test case for the Dir.foreach class method.
 ######################################################################
-require "test/helper"
-require "test/unit"
+require 'test/helper'
+require 'test/unit'
 
-class TC_Dir_Foreach_Class < Test::Unit::TestCase
-   include Test::Helper
+class TC_Dir_Foreach_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def setup
-      if WINDOWS
-         @entries = `dir /A /B`.split("\n").push('.', '..')
-      else
-         @entries = `ls -a1`.split("\n")
-         @entries.push('.') unless @entries.include?('.')
-         @entries.push('..') unless @entries.include?('..')
-      end
-      @pwd = Dir.pwd
-   end
+  def setup
+    if WINDOWS
+      @entries = `dir /A /B`.split("\n").push('.', '..')
+    else
+      @entries = `ls -a1`.split("\n")
+      @entries.push('.') unless @entries.include?('.')
+      @entries.push('..') unless @entries.include?('..')
+    end
+    @pwd = Dir.pwd
+  end
 
-   def test_foreach_basic
-      assert_respond_to(Dir, :foreach)
-      assert_nothing_raised{ Dir.foreach(@pwd){ } }
-   end
+  test "foreach basic functionality" do
+    assert_respond_to(Dir, :foreach)
+    assert_nothing_raised{ Dir.foreach(@pwd){ } }
+  end
 
-   def test_foreach
-      entries = []
-      assert_nothing_raised{ Dir.foreach(@pwd){ |e| entries.push(e) } }
-      assert_equal(entries.sort, @entries.sort)
-   end
+  test "foreach yields the expected results" do
+    entries = []
+    assert_nothing_raised{ Dir.foreach(@pwd){ |e| entries.push(e) } }
+    assert_equal(entries.sort, @entries.sort)
+  end
 
-   def test_foreach_expected_errors
-      assert_raises(ArgumentError){ Dir.foreach }
-      assert_raises(ArgumentError){ Dir.foreach(@pwd, @pwd) }
-      assert_raises(TypeError){ Dir.foreach(1){} }
-   end
+  test "foreach requires a block" do
+    assert_raises(LocalJumpError){ Dir.foreach(@pwd) }
+  end
 
-   def teardown
-      @pwd     = nil
-      @entries = nil
-   end
+  test "foreach requires one argument only" do
+    assert_raises(ArgumentError){ Dir.foreach }
+    assert_raises(ArgumentError){ Dir.foreach(@pwd, @pwd) }
+  end
+
+  test "foreach requires a string argument" do
+    assert_raises(TypeError){ Dir.foreach(1){} }
+    assert_raises(TypeError){ Dir.foreach([]){} }
+  end
+
+  def teardown
+    @pwd     = nil
+    @entries = nil
+  end
 end
