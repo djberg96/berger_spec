e30df965052d7e3e3bf653e7bbb4776f5d97d07e
Daniel Berger <djberg96@gmail.com>
2011-12-21 06:51:49 -0700

    Updated tests for Dir.getwd.

--- test/core/Dir/singleton/test_getwd.rb
+++ test/core/Dir/singleton/test_getwd.rb
@@ -1,5 +1,5 @@
 ###############################################################
-# tc_getwd.rb
+# test_getwd.rb
 #
 # Test suite for the Dir.getwd class method.  This also tests
 # the Dir.pwd alias.
@@ -7,40 +7,37 @@
 require 'test/helper'
 require 'test/unit'
 
-class TC_Dir_Getwd_Class < Test::Unit::TestCase
-   include Test::Helper
+class TC_Dir_Getwd_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def setup
-      @pwd = pwd_n
-      @pwd.tr!('\\','/') if WINDOWS
-   end
+  def setup
+    @pwd = pwd_n
+    @pwd.tr!('\\','/') if WINDOWS
+  end
 
-   def test_getwd_basic
-      assert_respond_to(Dir, :getwd)
-      assert_nothing_raised{ Dir.getwd }
-      assert_kind_of(String, Dir.getwd)
-   end
+  test "getwd basic functionality" do
+    assert_respond_to(Dir, :getwd)
+    assert_nothing_raised{ Dir.getwd }
+  end
 
-   def test_getwd
-      assert_equal(@pwd, Dir.getwd)
-   end
-   
-   # Alias
-   def test_pwd_basic
-      assert_respond_to(Dir, :pwd)
-      assert_nothing_raised{ Dir.pwd }
-      assert_kind_of(String, Dir.pwd)
-   end
+  test "getwd returns a string" do
+    assert_kind_of(String, Dir.getwd)
+  end
 
-   def test_pwd
-      assert_equal(@pwd, Dir.pwd)
-   end
-   
-   def test_getwd_expected_errors
-      assert_raises(ArgumentError){ Dir.getwd("foo") }
-   end
+  test "getwd returns the expected value" do
+    assert_equal(@pwd, Dir.getwd)
+  end
 
-   def teardown
-      @pwd = nil
-   end
+  test "pwd is an alias for getwd" do
+    assert_respond_to(Dir, :pwd)
+    assert_alias_method(Dir, :pwd, :getwd)
+  end
+
+  test "getwd does not accept any arguments" do
+    assert_raises(ArgumentError){ Dir.getwd("foo") }
+  end
+
+  def teardown
+    @pwd = nil
+  end
 end
