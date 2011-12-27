8a3e67ade60dcee1dc22254e46bd71e52200360a
Daniel Berger <djberg96@gmail.com>
2011-12-27 07:25:12 -0700

    Tests for Process.getpgrp skipped on MS Windows.

--- test/core/Process/singleton/test_getpgrp.rb
+++ test/core/Process/singleton/test_getpgrp.rb
@@ -10,17 +10,20 @@ class TC_Process_Getpgrp_SingletonMethod < Test::Unit::TestCase
   include Test::Helper
 
   test "getpgrp basic functionality" do
+    omit_if(WINDOWS, "Process.getpgrp tests skipped on MS Windows")
     assert_respond_to(Process, :getpgrp)
     assert_nothing_raised{ Process.getpgrp }
     assert_kind_of(Fixnum, Process.getpgrp)
   end
 
   test "getpgrp returns expected results" do
+    omit_if(WINDOWS, "Process.getpgrp tests skipped on MS Windows")
     assert_true(Process.getpgrp > 0)
     assert_equal(Process.getpgrp, Process.getpgid(0))
   end
 
   test "getpgrp does not accept any arguments" do
+    omit_if(WINDOWS, "Process.getpgrp tests skipped on MS Windows")
     assert_raise(ArgumentError){ Process.getpgrp(1) }
   end
 end
