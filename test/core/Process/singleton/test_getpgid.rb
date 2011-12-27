a2576dd3b4149ea6088da6baaa7b148721a7ecd0
Daniel Berger <djberg96@gmail.com>
2011-12-27 07:22:18 -0700

    Most tests for Process.getpgid now skipped on MS Windows.

--- test/core/Process/singleton/test_getpgid.rb
+++ test/core/Process/singleton/test_getpgid.rb
@@ -14,12 +14,14 @@ class TC_Process_Getpgid_SingletonMethod < Test::Unit::TestCase
   end
 
   test "getpgid basic functionality" do
+    omit_if(WINDOWS, "Process.getpgid tests skipped on MS Windows")
     assert_respond_to(Process, :getpgid)
     assert_nothing_raised{ Process.getpgid(@pid) }
     assert_kind_of(Fixnum, Process.getpgid(@pid))
   end
 
   test "getpgid returns expected results" do
+    omit_if(WINDOWS, "Process.getpgid tests skipped on MS Windows")
     assert_true(Process.getpgid(@pid) > 0)
     assert_true(Process.getpgid(@pid) < 100000)
   end
@@ -30,6 +32,7 @@ class TC_Process_Getpgid_SingletonMethod < Test::Unit::TestCase
   end
 
   test "getpgid requires a numeric argument" do
+    omit_if(WINDOWS, "Process.getpgid tests skipped on MS Windows")
     assert_raise(TypeError){ Process.getpgid("test") }
   end
 
