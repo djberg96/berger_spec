3650e365ab700eabc01badeb3ad422389051736a
Daniel Berger <djberg96@gmail.com>
2012-01-19 08:39:58 -0700

    Updated the Errno tests.

--- test/core/Errno/singleton/test_errno.rb
+++ test/core/Errno/singleton/test_errno.rb
@@ -1,25 +1,30 @@
 ###############################################################################
-# tc_errno.rb
+# test_errno.rb
 #
 # Test case for the Errno module. The tests here are rather generic because
 # it is impossible to predict which Errno classes will exist on any given
-# system.
+# operating system.
 ###############################################################################
 require 'test/helper'
 require 'test/unit'
 
 class TC_Errno_Module < Test::Unit::TestCase
-   def test_errno_constants_basic
-      assert_respond_to(Errno, :constants)
-      assert_kind_of(Array, Errno.constants)
-   end
+  test "Errno is a module" do
+    assert_kind_of(Module, Errno.class)
+  end
 
-   def test_errno_subclass_of_systemcallerror
-      assert_kind_of(SystemCallError, Errno::EINVAL.new)
-      assert_kind_of(SystemCallError, Errno::EACCES.new)
-   end
+  test "Errno contains a list of subclasses" do
+    assert_respond_to(Errno, :constants)
+    assert_kind_of(Array, Errno.constants)
+  end
 
-   def test_errno_constant
-      assert_kind_of(Fixnum, Errno::EINVAL::Errno)
-   end
+  test "Errno classes are a subclass of SystemCallError" do
+    assert_kind_of(SystemCallError, Errno::EINVAL.new)
+    assert_kind_of(SystemCallError, Errno::EACCES.new)
+  end
+
+  test "Errno class constants are operating system error numbers" do
+    assert_kind_of(Fixnum, Errno::EINVAL::Errno)
+    assert_kind_of(Fixnum, Errno::EACCES::Errno)
+  end
 end
