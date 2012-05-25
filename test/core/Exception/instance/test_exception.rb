8418840b4acf85d3128dcdaa5c33754124bbc745
Daniel Berger <djberg96@gmail.com>
2012-05-25 10:46:27 -0600

    Updated the Exception#exception tests.

--- test/core/Exception/instance/test_exception.rb
+++ test/core/Exception/instance/test_exception.rb
@@ -1,47 +1,55 @@
 ###############################################################################
-# tc_exception.rb
+# test_exception.rb
 #
 # Test case for the Exception#exception instance method.
 ###############################################################################
 require 'test/helper'
-require 'test/unit'
-
-class TestException < Exception; end
 
 class TC_Exception_Exception_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @exception = TestException.new
-      @object    = nil
-   end
+  class TestException < Exception; end
 
-   def test_exception_basic
-      assert_respond_to(@exception, :exception)
-      assert_nothing_raised{ @exception.exception }
-   end
+  def setup
+    @exception = TestException.new
+    @object    = nil
+  end
 
-   def test_exception_no_message
-      assert_kind_of(TestException, @exception.exception)
-      assert_equal(@exception.object_id, @exception.exception.object_id)
-   end
+  test "exception method basic functionality" do
+    assert_respond_to(@exception, :exception)
+    assert_nothing_raised{ @exception.exception }
+  end
 
-   def test_exception_with_message
-      assert_nothing_raised{ @object = @exception.exception('this is a test') }
-      assert_kind_of(TestException, @object)
-      assert_not_equal(@object.object_id, @exception.object_id)
-      assert_not_equal(@object.message, @exception.message)
-   end
+  test "exception method accepts an optional string argument" do
+    assert_nothing_raised{ @object = @exception.exception('test') }
+    assert_equal('test', @object.message)
+  end
 
-   def test_exception_with_self
-      assert_nothing_raised{ @exception.exception(@exception) }
-      assert_nothing_raised{ @exception.exception(TestException) }
-   end
+  test "exception method without an argument returns itself" do
+    assert_kind_of(TestException, @exception.exception)
+    assert_equal(@exception.object_id, @exception.exception.object_id)
+  end
 
-   def test_exception_expected_errors
-      assert_raise(ArgumentError){ @exception.message('hello', 'world') }
-   end
+  test "exception method with itself as the argument returns itself" do
+    assert_kind_of(TestException, @exception.exception(@exception))
+    assert_equal(@exception.object_id, @exception.exception.object_id)
+  end
 
-   def teardown
-      @exception = nil
-      @object    = nil
-   end
+  test "adding a messge to the exception method creates a new exception object" do
+    @object = @exception.exception('test')
+    assert_kind_of(TestException, @object)
+    assert_not_equal(@object.object_id, @exception.object_id)
+    assert_not_equal(@object.message, @exception.message)
+  end
+
+  test "exception method accepts only argument" do
+    assert_raise(ArgumentError){ @exception.message('hello', 'world') }
+  end
+
+  test "exception method raises an ArgumentError if the argument is not a string or itself" do
+    assert_raise(ArgumentError){ @exception.message(1) }
+  end
+
+  def teardown
+    @exception = nil
+    @object    = nil
+  end
 end
