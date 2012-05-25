67e337909e0b019184a014f9966c17cfac9c13da
Daniel Berger <djberg96@gmail.com>
2012-05-25 06:45:19 -0600

    Updated the Exception#backtrace tests.

--- test/core/Exception/instance/test_backtrace.rb
+++ test/core/Exception/instance/test_backtrace.rb
@@ -1,38 +1,37 @@
 ###############################################################################
-# tc_backtrace.rb
+# test_backtrace.rb
 #
 # Test case for the Exception#backtrace instance method.
 ###############################################################################
 require 'test/helper'
-require 'test/unit'
 
 class TC_Exception_Backtrace_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @exception = RuntimeError.new
-   end
+  def setup
+    @exception = RuntimeError.new
+  end
 
-   def test_backtrace_basic
-      assert_respond_to(@exception, :backtrace)
-      assert_nothing_raised{ @exception.backtrace }
-      assert_nil(@exception.backtrace)
-   end
+  test "backtrace basic functionality" do
+    assert_respond_to(@exception, :backtrace)
+    assert_nothing_raised{ @exception.backtrace }
+  end
 
-   def test_backtrace
-      begin; 1/0; rescue Exception => @exception; end 
-      assert_not_nil(@exception.backtrace)
-      assert_kind_of(Array, @exception.backtrace)
-   end
+  test "backtrace returns an array" do
+    begin; 1/0; rescue Exception => @exception; end
+    assert_not_nil(@exception.backtrace)
+    assert_kind_of(Array, @exception.backtrace)
+  end
 
-   def test_backtrace_format
-      begin; 1/0; rescue Exception => @exception; end 
-      assert_not_nil(@exception.backtrace.first =~ /.*?tc_backtrace\.rb:\d+:in.+/)
-   end
+  test "backtrace format includes the file name, line number, and 'in' followed by method name" do
+    begin; 1/0; rescue Exception => @exception; end
+    regex = /.*?test_backtrace\.rb:\d+:in.+/i
+    assert_not_nil(regex.match(@exception.backtrace.first))
+  end
 
-   def test_backtrace_expected_errors
-      assert_raise(ArgumentError){ @exception.backtrace(true) }
-   end
+  test "backtrace method does not accept any arguments" do
+    assert_raise(ArgumentError){ @exception.backtrace(true) }
+  end
 
-   def teardown
-      @exception = nil
-   end
+  def teardown
+    @exception = nil
+  end
 end
