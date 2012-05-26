bec5efe4104c75aedc82931469e4e5718bf4829e
Daniel Berger <djberg96@gmail.com>
2012-05-25 21:42:28 -0600

    Updated Exception#messages

--- test/core/Exception/instance/test_message.rb
+++ test/core/Exception/instance/test_message.rb
@@ -1,36 +1,35 @@
 ###############################################################################
-# tc_message.rb
+# test_message.rb
 #
 # Test case for the Exception#message instance method.
 ###############################################################################
 require 'test/helper'
-require 'test/unit'
 
 class TC_Exception_Message_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @message    = 'Message Test'
-      @exception1 = Exception.new(@message)
-      @exception2 = Exception.new
-   end
+  def setup
+    @message    = 'Message Test'
+    @exception1 = Exception.new(@message)
+    @exception2 = Exception.new
+  end
 
-   def test_message_basic
-      assert_respond_to(@exception1, :message)
-      assert_nothing_raised{ @exception1.message }
-      assert_kind_of(String, @exception1.message)
-   end
+  test "message basic functionality" do
+    assert_respond_to(@exception1, :message)
+    assert_nothing_raised{ @exception1.message }
+    assert_kind_of(String, @exception1.message)
+  end
 
-   def test_message
-      assert_equal('Message Test', @exception1.message)
-      assert_equal('Exception', @exception2.message)
-   end
+  test "message method returns the expected result" do
+    assert_equal('Message Test', @exception1.message)
+    assert_equal('Exception', @exception2.message)
+  end
 
-   def test_message_expected_errors
-      assert_raise(ArgumentError){ @exception1.message(1) }
-   end
+  test "message method does not accept any arguments" do
+    assert_raise(ArgumentError){ @exception1.message(1) }
+  end
 
-   def teardown
-      @message    = nil
-      @exception1 = nil
-      @exception2 = nil
-   end
+  def teardown
+    @message    = nil
+    @exception1 = nil
+    @exception2 = nil
+  end
 end
