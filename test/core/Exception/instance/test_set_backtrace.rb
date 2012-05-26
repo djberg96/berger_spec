f37796c5f5a859e76484900be8c00af3b705c82f
Daniel Berger <djberg96@gmail.com>
2012-05-26 10:09:29 -0600

    Refactored the Exception#set_backtrace tests.

--- test/core/Exception/instance/test_set_backtrace.rb
+++ test/core/Exception/instance/test_set_backtrace.rb
@@ -1,5 +1,5 @@
 ###############################################################################
-# tc_set_backtrace.rb
+# test_set_backtrace.rb
 #
 # Test case for the Exception#set_backtrace instance method. Note that
 # contrary to what the Pickaxe, 2nd edition says, there is no enforcement
@@ -7,41 +7,55 @@
 # it will accept a plain string, in addition to an array of strings.
 ###############################################################################
 require 'test/helper'
-require 'test/unit'
 
 class TC_Exception_SetBacktrace_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @exception = Exception.new
-      @backtrace = 'testing'
-   end
+  def setup
+    @exception = Exception.new
+    @backtrace = 'testing'
+  end
 
-   def test_set_backtrace_basic
-      assert_respond_to(@exception, :set_backtrace)
-      assert_nothing_raised{ @exception.set_backtrace(@backtrace) }
-      assert_kind_of(Array, @exception.set_backtrace(@backtrace))
-   end
+  test "set_backtrace basic functionality" do
+    assert_respond_to(@exception, :set_backtrace)
+  end
 
-   def test_set_backtrace
-      assert_equal(['testing'], @exception.set_backtrace(@backtrace))
-      assert_equal(['testing'], @exception.backtrace)
-      assert_equal(['hello', 'world'], @exception.set_backtrace(['hello', 'world']))
-      assert_equal(['hello', 'world'], @exception.backtrace)
-   end
+  test "set_backtrace allows either a string or an array of strings as an argument" do
+    assert_nothing_raised{ @exception.set_backtrace('test') }
+    assert_nothing_raised{ @exception.set_backtrace(['foo', 'bar']) }
+  end
 
-   def test_set_backtrace_edge_cases
-      assert_equal(nil, @exception.set_backtrace(nil))
-   end
+  test "set_backtrace returns an array if the argument is not nil" do
+    assert_kind_of(Array, @exception.set_backtrace('test'))
+    assert_kind_of(Array, @exception.set_backtrace(['foo', 'bar']))
+  end
 
-   def test_set_backtrace_expected_errors
-      assert_raise(ArgumentError){ @exception.set_backtrace }
-      assert_raise(ArgumentError){ @exception.set_backtrace(1,2) }
-      assert_raise(TypeError){ @exception.set_backtrace(true) }
-      assert_raise(TypeError){ @exception.set_backtrace(0) }
-      assert_raise(TypeError){ @exception.set_backtrace(['hello', 0]) }
-   end
+  test "set_backtrace returns nil if its argument is nil" do
+    assert_nil(@exception.set_backtrace(nil))
+  end
 
-   def teardown
-      @exception = nil
-      @backtrace = nil
-   end
+  test "set_backtrace returns the expected result if a string is passed as an argument" do
+    assert_equal(['testing'], @exception.set_backtrace(@backtrace))
+    assert_equal(['testing'], @exception.backtrace)
+  end
+
+  test "set_backtrace returns the expected result if an array is passed as an argument" do
+    assert_equal(['hello', 'world'], @exception.set_backtrace(['hello', 'world']))
+    assert_equal(['hello', 'world'], @exception.backtrace)
+  end
+
+  test "set_backtrace requires one argument only" do
+    assert_raise(ArgumentError){ @exception.set_backtrace }
+    assert_raise(ArgumentError){ @exception.set_backtrace('foo', 'bar') }
+  end
+
+  test "set_backtrace only accepts a string or an array of strings" do
+    assert_raise(TypeError){ @exception.set_backtrace(true) }
+    assert_raise(TypeError){ @exception.set_backtrace(0) }
+    assert_raise(TypeError){ @exception.set_backtrace(['hello', 0]) }
+    assert_raise(TypeError){ @exception.set_backtrace(@exception) }
+  end
+
+  def teardown
+    @exception = nil
+    @backtrace = nil
+  end
 end
