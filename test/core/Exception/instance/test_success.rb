add5bd0373ebc1b73f4bd673e42b14af0a548600
Daniel Berger <djberg96@gmail.com>
2012-05-25 22:27:00 -0600

    Updated the Exception#success? tests.

--- test/core/Exception/instance/test_success.rb
+++ test/core/Exception/instance/test_success.rb
@@ -1,43 +1,40 @@
 ###############################################################################
-# tc_success.rb
+# test_success.rb
 #
 # Test case for the Exception#success? instance method. Actually, this method
 # only exists on the SystemExit subclass.
 ###############################################################################
 require 'test/helper'
-require 'test/unit'
 
 class TC_Exception_Success_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @err = nil
-      begin; exit(99); rescue SystemExit => @err; end
-   end
+  def setup
+    @err = nil
+    begin; exit(99); rescue SystemExit => @err; end
+  end
 
-   def test_success_basic
-      assert_respond_to(@err, :success?)
-      assert_nothing_raised{ @err.success? }
-      assert_boolean(@err.success?)
-   end
+  test "success? method basic functionality" do
+    assert_respond_to(@err, :success?)
+    assert_nothing_raised{ @err.success? }
+    assert_boolean(@err.success?)
+  end
 
-   # 0 or nil are success, everything else is not.
-   def test_success
-      assert_equal(false, @err.success?)
-      begin; exit(0); rescue SystemExit => @err; end
-      assert_equal(true, @err.success?)
-      begin; rescue SystemExit => @err; end
-      assert_equal(true, @err.success?)
-   end
-   
-   # Only the SystemExit class implements this method
-   def test_success_expected_errors
-      assert_raise(ArgumentError){ @err.success?(99) }
-      begin; 1/0; rescue Exception => @err; end
-      assert_raise(NoMethodError){ @err.success? }
-   end
+  test "only 0 and nil are considered success" do
+    assert_false(@err.success?)
+    begin; exit(0); rescue SystemExit => @err; end
+    assert_true(@err.success?)
+    begin; rescue SystemExit => @err; end
+    assert_true(@err.success?)
+  end
 
-   def teardown
-      @err = nil
-   end
+  test "only the SystemExit class implements the success? method" do
+    assert_raise(ArgumentError){ @err.success?(99) }
+    begin; 1/0; rescue Exception => @err; end
+    assert_raise(NoMethodError){ @err.success? }
+  end
+
+  def teardown
+    @err = nil
+  end
 end
