c1c4b92e8bede29e2506d4205200dbb0f08bbf05
Daniel Berger <djberg96@gmail.com>
2012-05-25 22:42:08 -0600

    Updated the Exception#to_s tests.

--- test/core/Exception/instance/test_to_s.rb
+++ test/core/Exception/instance/test_to_s.rb
@@ -1,40 +1,38 @@
 ###############################################################################
-# tc_to_s.rb
+# test_to_s.rb
 #
 # Test case for the Exception#to_s instance method.
 ###############################################################################
 require 'test/helper'
-require 'test/unit'
 
 class TC_Exception_ToS_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @err1 = Exception.new
-      @err2 = Exception.new('hello')
-   end
+  def setup
+    @err1 = Exception.new
+    @err2 = Exception.new('hello')
+  end
 
-   def test_to_s_basic
-      assert_respond_to(@err1, :to_s)
-      assert_nothing_raised{ @err1.to_s }
-      assert_kind_of(String, @err1.to_s)
-   end
+  test "to_s basic functionality" do
+    assert_respond_to(@err1, :to_s)
+    assert_nothing_raised{ @err1.to_s }
+    assert_kind_of(String, @err1.to_s)
+  end
 
-   def test_to_s
-      assert_equal('Exception', @err1.to_s)
-      assert_equal('hello', @err2.to_s)
-   end
+  test "to_s returns the expected results" do
+    assert_equal('Exception', @err1.to_s)
+    assert_equal('hello', @err2.to_s)
+  end
 
-   # If the Exception object is tainted, so is the message
-   def test_to_s_tainted
-      assert_nothing_raised{ @err2.taint }
-      assert_equal(true, @err2.message.tainted?)
-   end
+  test "if the exception is tainted, so is its message" do
+    assert_nothing_raised{ @err2.taint }
+    assert_true(@err2.message.tainted?)
+  end
 
-   def test_to_s_expected_errors
-      assert_raise(ArgumentError){ @err1.to_s(true) }
-   end
+  test "to_s does not accept any arguments" do
+    assert_raise(ArgumentError){ @err1.to_s(true) }
+  end
 
-   def teardown
-      @err1 = nil
-      @err2 = nil
-   end
+  def teardown
+    @err1 = nil
+    @err2 = nil
+  end
 end
