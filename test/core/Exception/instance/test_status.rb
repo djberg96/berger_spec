b2c5b9bb7234b5c32fd8bb392dd3be10ba1a5cb8
Daniel Berger <djberg96@gmail.com>
2012-05-25 21:58:03 -0600

    Updated the Exception#status tests.

--- test/core/Exception/instance/test_status.rb
+++ test/core/Exception/instance/test_status.rb
@@ -1,38 +1,36 @@
 ###############################################################################
-# tc_status.rb
+# test_status.rb
 #
 # Test case for the Exception#status instance method. Actually, this method
 # only exists on the SystemExit subclass.
 ###############################################################################
 require 'test/helper'
-require 'test/unit'
 
 class TC_Exception_Status_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @err = nil
-      begin; exit(99); rescue SystemExit => @err; end
-   end
+  def setup
+    @err = nil
+    begin; exit(99); rescue SystemExit => @err; end
+  end
 
-   def test_status_basic
-      assert_respond_to(@err, :status)
-      assert_nothing_raised{ @err.status }
-      assert_kind_of(Fixnum, @err.status)
-   end
+  test "status method basic functionality" do
+    assert_respond_to(@err, :status)
+    assert_nothing_raised{ @err.status }
+    assert_kind_of(Fixnum, @err.status)
+  end
 
-   def test_status
-      assert_equal(99, @err.status)
-      begin; exit(-1); rescue SystemExit => @err; end
-      assert_equal(-1, @err.status)
-   end
-   
-   # Only the SystemExit class implements this method
-   def test_status_expected_errors
-      assert_raise(ArgumentError){ @err.status(99) }
-      begin; 1/0; rescue Exception => @err; end
-      assert_raise(NoMethodError){ @err.status }
-   end
+  test "status method returns the expected results" do
+    assert_equal(99, @err.status)
+    begin; exit(-1); rescue SystemExit => @err; end
+    assert_equal(-1, @err.status)
+  end
 
-   def teardown
-      @err = nil
-   end
+  test "only the SystemExit class implements the status method" do
+    assert_raise(ArgumentError){ @err.status(99) }
+    begin; 1/0; rescue Exception => @err; end
+    assert_raise(NoMethodError){ @err.status }
+  end
+
+  def teardown
+    @err = nil
+  end
 end
