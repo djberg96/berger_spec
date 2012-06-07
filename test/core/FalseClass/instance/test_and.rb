e04e211fbfbff848a1257935b9a021fec65a5034
Daniel Berger <djberg96@gmail.com>
2012-06-06 20:41:27 -0600

    Updated 'false & and && tests.

--- test/core/FalseClass/instance/test_and.rb
+++ test/core/FalseClass/instance/test_and.rb
@@ -1,50 +1,60 @@
 #####################################################################
-# tc_and.rb
+# test_and.rb
 #
 # Test case for 'false &'. We use the stringio class here to verify
 # the difference in handling between '&' and '&&'.
 #####################################################################
 require 'test/helper'
-require 'test/unit'
 require 'stringio'
 
 class TC_FalseClass_And_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @sio = StringIO.new
-   end
+  def setup
+    @sio = StringIO.new
+  end
 
-   def lookup(val)
-      @sio.write(val)
-      @sio.rewind
-   end
+  # Helper method we use in other tests
+  def lookup(val)
+    @sio.write(val)
+    @sio.rewind
+  end
 
-   def test_and_basic
-      assert_respond_to(false, :&)
-      assert_nothing_raised{ false.&(0) }
-      assert_nothing_raised{ false & 0 }
-   end
+  test "and basic functionality" do
+    assert_respond_to(false, :&)
+    assert_nothing_raised{ false.&(0) }
+    assert_nothing_raised{ false & 0 }
+  end
 
-   def test_single_and
-      assert_equal(false, false & 0)
-      assert_equal(false, false & 1)
-      assert_equal(false, false & true)
-      assert_equal(false, false & false)
+  test "false with single & and numeric RHS value" do
+    assert_false(false & 0)
+    assert_false(false & 1)
+  end
 
-      assert_nothing_raised{ false & lookup('cat') }
-      assert_equal('cat', @sio.read(100))
-   end
+  test "false with single & and boolean RHS value" do
+    assert_false(false & true)
+    assert_false(false & false)
+  end
 
-   def test_double_and
-      assert_equal(false, false && 0)
-      assert_equal(false, false && 1)
-      assert_equal(false, false && true)
-      assert_equal(false, false && false)
+  test "false with single & and string RHS value" do
+    assert_nothing_raised{ false & lookup('cat') }
+    assert_equal('cat', @sio.read(100))
+  end
 
-      assert_nothing_raised{ false && lookup('cat') }
-      assert_equal(nil, @sio.read(100))
-   end
+  test "false with double & and numeric RHS value" do
+    assert_false(false && 0)
+    assert_false(false && 1)
+  end
 
-   def teardown
-      @sio = nil
-   end
+  test "false with double & and boolean RHS value" do
+    assert_false(false && true)
+    assert_false(false && false)
+  end
+
+  test "false with double & and string RHS value" do
+    assert_nothing_raised{ false && lookup('cat') }
+    assert_nil(@sio.read(100))
+  end
+
+  def teardown
+    @sio = nil
+  end
 end
