b585c429292f2055ca41dd797aacc4578dfa2f53
Daniel Berger <djberg96@gmail.com>
2015-01-05 17:41:42 -0700

    Refactored and updated for Ruby 2.1.

--- test/stdlib/Tmpdir/test_tmpdir.rb
+++ test/stdlib/Tmpdir/test_tmpdir.rb
@@ -1,110 +1,134 @@
 ########################################################################
-# tc_tmpdir.rb
+# test_tmpdir.rb
 #
 # Test case for the tmpdir library.
 ########################################################################
-require 'test/unit'
+require 'test-unit'
 require 'test/helper'
 require 'tmpdir'
-require 'fileutils'
 
-class TC_Tmpdir_Stdlib < Test::Unit::TestCase
-   include Test::Helper
+class Test_Tmpdir_Stdlib < Test::Unit::TestCase
+  include Test::Helper
 
-   # Helper method used to unset the environment variables in order to
-   # validate the behavior when these cannot be found by the library.
-   def remove_env_tmpdirs
-      ENV['TMPDIR'] = nil
-      ENV['TMP'] = nil
-      ENV['TEMP'] = nil
-      ENV['USERPROFILE'] = nil
-   end
+  # Helper method used to unset the environment variables in order to
+  # validate the behavior when these cannot be found by the library.
+  def remove_env_tmpdirs
+    ENV['TMPDIR'] = nil
+    ENV['TMP'] = nil
+    ENV['TEMP'] = nil
+    ENV['USERPROFILE'] = nil
+  end
 
-   def setup
-      @pwd   = Dir.pwd
-      @tmp   = get_temp_path
-      @mytmp = File.join(@pwd, 'mytemp')
-      @ftmp  = 'tmpdir.tmp'
+  # Restore any environment variables we may have unset
+  def restore_env_tmpdirs
+    ENV['TMPDIR'] = @env_tmpdir
+    ENV['TMP'] = @env_tmp
+    ENV['TEMP'] = @env_temp
+    ENV['USERPROFILE'] = @env_userprofile
+  end
 
-      touch(@ftmp)
-      Dir.mkdir(@mytmp) unless File.exists?(@mytmp)
-   end
+  def setup
+    @tmp  = get_temp_path
+    @dtmp = File.join(Dir.pwd, 'mytemp')
+    @ftmp = 'tmpdir.tmp'
 
-   def test_tmpdir_basic
-      assert_respond_to(Dir, :tmpdir)
-      assert_nothing_raised{ Dir.tmpdir }
-      assert_equal(true, File.exists?(Dir.tmpdir))
-   end
+    @env_tmpdir = ENV['TMPDIR']
+    @env_tmp = ENV['TMP']
+    @env_temp = ENV['TEMP']
+    @env_userprofile = ENV['USERPROFILE']
 
-   def test_tmpdir
-      assert_equal(@tmp, Dir.tmpdir)
-   end
+    Dir.mkdir(@dtmp)
+  end
 
-   # Dir.tmpdir resorts to a specific directory if none of the standard
-   # ENV variables can be found.
-   #
-   def test_tmpdir_env_not_found
-      remove_env_tmpdirs
-      if WINDOWS
-         temp = File.join(get_windows_path, 'temp')
-         assert_equal(temp, Dir.tmpdir)
-      else
-         assert_equal('/tmp', Dir.tmpdir)
-      end
-   end
+  test "tmpdir basic functionality" do
+    assert_respond_to(Dir, :tmpdir)
+    assert_nothing_raised{ Dir.tmpdir }
+  end
 
-   # Dir.tmpdir defaults to '.' (or the WINDOWS directory) if the tmpdir
-   # exists but is not a directory.
-   #
-   def test_tmpdir_is_not_a_file
-      ENV['TMPDIR'] = @ftmp
-      if WINDOWS
-         temp = File.join(get_windows_path, 'temp')
-         assert_equal(temp, Dir.tmpdir)
-      else
-         assert_equal('/tmp', Dir.tmpdir)
-      end
-   end
+  test "tmpdir returns a string" do
+    assert_kind_of(String, Dir.tmpdir)
+    assert_true(Dir.tmpdir.size > 0)
+  end
 
-   # Dir.tmpdir defaults to '.' (or the WINDOWS directory) if the tmpdir
-   # exists but is not writable.
-   #--
-   # TODO: Fix. The File.chmod doesn't seem to be taking effect on Windows.
-   #
-   def test_tmpdir_is_not_writable
-      remove_env_tmpdirs
-      File.chmod(0444, @mytmp)
-      ENV['TMPDIR'] = @mytmp
+  test "tmpdir exists on the filesystem" do
+    assert_true(File.exist?(Dir.tmpdir))
+  end
 
-      if WINDOWS
-         temp = File.join(get_windows_path, 'temp')
-         assert_equal(temp, Dir.tmpdir)
-      else
-         assert_equal('/tmp', Dir.tmpdir)
-      end
-   end
+  test "tmpdir returns the expected result" do
+    assert_equal(@tmp, Dir.tmpdir)
+  end
 
-   # Dir.tmpdir defaults to your system's tmpdir (always) if the $SAFE
-   # level is greater than 0.
-   #
-   def test_with_safe_level
-      proc do
-         $SAFE = 1
-         if WINDOWS
-            temp = File.join(get_windows_path, 'temp')
-            assert_equal(temp, Dir.tmpdir)
-         else
-            assert_equal('/tmp', Dir.tmpdir)
-         end
-      end.call
-   end
+  test "tmpdir uses ENV['TMPDIR'] as it's first check by default" do
+    remove_env_tmpdirs
+    ENV['TMPDIR'] = @dtmp
+    assert_equal(Dir.tmpdir, @dtmp)
+  end
 
-   def teardown
-      FileUtils.rm_rf(@mytmp) if File.exists?(@mytmp)
-      File.delete(@ftmp) if File.exists?(@ftmp)
-      @pwd   = nil
-      @tmp   = nil
-      @mytmp = nil
-      @ftmp  = nil
-   end
+  test "tmpdir uses ENV['TMP'] as it's second check by default" do
+    remove_env_tmpdirs
+    ENV['TMP'] = Dir.pwd
+    assert_equal(Dir.tmpdir, Dir.pwd)
+  end
+
+  test "tmpdir uses ENV['TEMP'] as it's third check by default" do
+    remove_env_tmpdirs
+    ENV['TEMP'] = @dtmp
+    assert_equal(Dir.tmpdir, @dtmp)
+  end
+
+  test "tmpdir resorts to a specific directory if none of the relevant ENV variables are found" do
+    remove_env_tmpdirs
+    assert_equal(File.expand_path(Etc.systmpdir), Dir.tmpdir)
+  end
+
+  test "tmpdir defaults to the systmpdir if the tmpdir exists but is not writable" do
+    remove_env_tmpdirs
+    File.chmod(0444, @dtmp)
+    ENV['TMPDIR'] = @dtmp
+    assert_equal(File.expand_path(Etc.systmpdir), Dir.tmpdir)
+  end
+
+  test "tmpdir defaults to the systmpdir if the tmpdir exists but is not a directory" do
+    remove_env_tmpdirs
+    touch(@ftmp)
+    ENV['TMPDIR'] = @ftmp
+    assert_equal(File.expand_path(Etc.systmpdir), Dir.tmpdir)
+  end
+
+  test "tmpdir defaults to the systmpdir if the tmpdir exists but is world writable" do
+    omit_if(WINDOWS)
+    remove_env_tmpdirs
+    File.chmod(0777, @dtmp)
+    ENV['TMPDIR'] = @dtmp
+    assert_equal(File.expand_path(Etc.systmpdir), Dir.tmpdir)
+  end
+
+  test "tmpdir defaults to the systmpdir if the tmpdir exists but is sticky" do
+    omit_if(WINDOWS)
+    remove_env_tmpdirs
+    File.chmod(10770, @dtmp)
+    ENV['TMPDIR'] = @dtmp
+    assert_equal(File.expand_path(Etc.systmpdir), Dir.tmpdir)
+  end
+
+  test "tmpdir defaults to systmpdir if $SAFE level is greater than zero" do
+    remove_env_tmpdirs
+    proc do
+      $SAFE = 1
+      assert_equal(Etc.systmpdir, Dir.tmpdir)
+    end.call
+  end
+
+  test "mktmpdir basic functionality" do
+    assert_respond_to(Dir, :mktmpdir)
+  end
+
+  def teardown
+    FileUtils.rm_rf(@dtmp) if File.exist?(@dtmp)
+    File.delete(@ftmp) if File.exist?(@ftmp)
+    restore_env_tmpdirs
+    @tmp  = nil
+    @dtmp = nil
+    @ftmp = nil
+  end
 end
