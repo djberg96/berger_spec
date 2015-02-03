fff3575fe380944fc0414b1b5e4b9b04cc5f9341
Daniel Berger <djberg96@gmail.com>
2015-02-03 07:56:47 -0700

    Added Dir#fileno tests.

---
+++ test/core/Dir/instance/test_fileno.rb
@@ -0,0 +1,37 @@
+#########################################################
+# test_fileno.rb
+#
+# Test suite for the Dir#fileno instance method.
+#########################################################
+require 'test/helper'
+require 'test-unit'
+
+class TC_Dir_Fileno_InstanceMethod < Test::Unit::TestCase
+  include Test::Helper
+
+  def setup
+    @dir = Dir.new(Dir.pwd)
+  end
+
+  test "fileno basic functionality" do
+    omit_if(WINDOWS)
+    assert_respond_to(@dir, :fileno)
+    assert_nothing_raised{ @dir.fileno }
+    assert_kind_of(Numeric, @dir.fileno)
+  end
+
+  test "fileno returns expected results" do
+    omit_if(WINDOWS)
+    assert_true(@dir.fileno > 0)
+  end
+
+  test "fileno does not accept any parameters" do
+    omit_if(WINDOWS)
+    assert_raises(ArgumentError){ @dir.fileno('foo') }
+  end
+
+  def teardown
+    @dir.close
+    @dir = nil
+  end
+end
