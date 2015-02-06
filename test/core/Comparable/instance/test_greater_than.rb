ecd1d97645c9b514905c0787b6a362b576b79982
Daniel Berger <djberg96@gmail.com>
2015-02-06 15:33:57 -0700

    Refactored and updated for Ruby 2.2.

--- test/core/Comparable/instance/test_greater_than.rb
+++ test/core/Comparable/instance/test_greater_than.rb
@@ -1,49 +1,49 @@
 ######################################################################
-# tc_greater_than.rb
+# test_greater_than.rb
 #
 # Test case for the Comparable#< instance method.  For testing
 # purposes we setup a custom class that mixes in Comparable and
 # defines a basic <=> method.
 ######################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
 class TC_Comparable_GreaterThan_Instance < Test::Unit::TestCase
 
-   class Foo
-      include Comparable
-      attr :val
-      def initialize(val)
-         @val = val
-      end
-      def <=>(other)
-         @val <=> other.val
-      end
-   end
+  class Foo
+    include Comparable
+    attr :val
+    def initialize(val)
+      @val = val
+    end
+    def <=>(other)
+      @val <=> other.val
+    end
+  end
 
-   def setup
-      @f1 = Foo.new(1)
-      @f2 = Foo.new(2)
-      @f3 = Foo.new(nil)
-   end
+  def setup
+    @f1 = Foo.new(1)
+    @f2 = Foo.new(2)
+    @f3 = Foo.new(nil)
+  end
 
-   def test_greater_than_basic
-      assert_respond_to(@f1, :>)
-   end
+  test "> basic functionality" do
+    assert_respond_to(@f1, :>)
+  end
 
-   def test_greater_than
-      assert_equal(false, @f1 > @f2)
-      assert_equal(true, @f2 > @f1)
-   end
+  test "> returns expected results" do
+    assert_false(@f1 > @f2)
+    assert_true(@f2 > @f1)
+  end
 
-   def test_greater_than_expected_errors
-      assert_raises(ArgumentError){ @f1 > @f3 }
-      assert_raises(NoMethodError){ @f3 > @f1 }
-   end
+  test "> raises expected errors" do
+    assert_raises(ArgumentError){ @f1 > @f3 }
+    assert_raises(ArgumentError){ @f3 > @f1 }
+  end
 
-   def teardown
-      @f1 = nil
-      @f2 = nil
-      @f3 = nil
-   end
+  def teardown
+    @f1 = nil
+    @f2 = nil
+    @f3 = nil
+  end
 end
