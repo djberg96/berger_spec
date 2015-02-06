a0d08584ef7b07f9aa60ad934d00f68a7626f3c0
Daniel Berger <djberg96@gmail.com>
2015-02-06 15:42:58 -0700

    Refactored and updated for Ruby 2.2.

--- test/core/Comparable/instance/test_equals.rb
+++ test/core/Comparable/instance/test_equals.rb
@@ -1,52 +1,46 @@
 ######################################################################
-# tc_equals.rb
+# test_equals.rb
 #
 # Test case for the Comparable#< instance method.  For testing
 # purposes we setup a custom class that mixes in Comparable and
 # defines a basic <=> method.
 ######################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
 class TC_Comparable_Equals_Instance < Test::Unit::TestCase
 
-   class Foo
-      include Comparable
-      attr :val
-      def initialize(val)
-         @val = val
-      end
-      def <=>(other)
-         @val <=> other.val
-      end
-   end
+  class Foo
+    include Comparable
+    attr :val
+    def initialize(val)
+      @val = val
+    end
+    def <=>(other)
+      @val <=> other.val
+    end
+  end
 
-   def setup
-      @f1 = Foo.new(1)
-      @f2 = Foo.new(2)
-      @f3 = Foo.new(nil)
-   end
+  def setup
+    @f1 = Foo.new(1)
+    @f2 = Foo.new(2)
+    @f3 = Foo.new(nil)
+  end
 
-   def test_equals_basic
-      assert_respond_to(@f1, :==)
-      assert_kind_of(Boolean, @f1 == @f2)
-   end
+  test "== basic functionality" do
+    assert_respond_to(@f1, :==)
+    assert_boolean(@f1 == @f2)
+  end
 
-   def test_equals
-      assert_equal(false, @f1 == @f2)
-      assert_equal(true,  @f1 == @f1)
-      assert_equal(false, @f2 == @f1)
-   end
+  test "== returns expected results" do
+    assert_false(@f1 == @f2)
+    assert_true(@f1 == @f1)
+    assert_false(@f2 == @f1)
+  end
 
-   def test_equals_against_different_object_types
-      msg = "=> Possibly a bug. See ruby-core: 13448 and following"
-      assert_equal(false, @f1 == 0, msg)
-      assert_equal(false, @f1 == 'hello', msg)
-   end
-
-   def teardown
-      @f1 = nil
-      @f2 = nil
-      @f3 = nil
-   end
+  def teardown
+    @f1 = nil
+    @f2 = nil
+    @f3 = nil
+  end
 end
