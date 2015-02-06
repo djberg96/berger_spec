2332480625ff920f9d20def2628cdb65a3f2887b
Daniel Berger <djberg96@gmail.com>
2015-02-06 15:30:42 -0700

    Refactored tests and updated block test.

--- test/core/Class/singleton/test_new.rb
+++ test/core/Class/singleton/test_new.rb
@@ -7,45 +7,40 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Class_New_Class < Test::Unit::TestCase
-   class FooNew; end
+  class FooNew; end
 
-   def setup
-      @obj = nil
-      @foo = FooNew.new
-      @singleton = class << @foo; self; end
-      @block = lambda{
-         def hello
-            "hello"
-         end
-      }
-   end
+  def setup
+    @obj = nil
+    @foo = FooNew.new
+    @singleton = class << @foo; self; end
+  end
 
-   def test_new_basic
-      assert_nothing_raised{ @obj = Class.new }
-      assert_kind_of(Object, @obj)
-   end
+  test "constructor basic functionality" do
+    assert_nothing_raised{ @obj = Class.new }
+    assert_kind_of(Object, @obj)
+  end
 
-   def test_new_alternate_superclass
-      assert_nothing_raised{ @obj = Class.new(Array) }
-      assert_kind_of(Class, @obj)
-      assert_kind_of(Array, @obj.new)
-   end
+  test "constructor accepts a class argument that serves as a superclass" do
+    assert_nothing_raised{ @obj = Class.new(Array) }
+    assert_kind_of(Class, @obj)
+    assert_kind_of(Array, @obj.new)
+  end
 
-   def test_new_with_block
-      assert_nothing_raised{ Class.new{ } }
-      assert_nothing_raised{ @obj = Class.new(&@block) }
-      assert_equal('hello', @obj.new.hello)
-   end
+  test "constructor accepts a block" do
+    assert_nothing_raised{ Class.new{ } }
+    assert_nothing_raised{ @obj = Class.new{ def hello; "hello"; end } }
+    assert_equal('hello', @obj.new.hello)
+  end
 
-   def test_new_expected_errors
-      assert_raise(LocalJumpError){ Class.new{ yield } }
-      assert_raise(TypeError){ @singleton.new }
-   end
+  test "constructor raises expected errors" do
+    assert_raise(LocalJumpError){ Class.new{ yield } }
+    assert_raise(TypeError){ @singleton.new }
+  end
 
-   def teardown
-      @obj   = nil
-      @foo   = nil
-      @block = nil
-      @singleton = nil
-   end
+  def teardown
+    @obj   = nil
+    @foo   = nil
+    @block = nil
+    @singleton = nil
+  end
 end
