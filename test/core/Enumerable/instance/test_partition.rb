10e440255715df953c9d2d19963af8084167289a
Daniel Berger <djberg96@gmail.com>
2015-02-07 09:32:44 -0700

    Updated for Ruby 2.2.

--- test/core/Enumerable/instance/test_partition.rb
+++ test/core/Enumerable/instance/test_partition.rb
@@ -1,43 +1,49 @@
 ########################################################################
-# tc_partition.rb
+# test_partition.rb
 #
 # Test case for the Enumerable#partition instance method.
 ########################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
 class TC_Enumerable_Partition_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @enum = [1,2,3,4,5]
-   end
+  def setup
+    @enum = [1,2,3,4,5]
+  end
 
-   def test_partition_basic
-      assert_respond_to(@enum, :partition)
-      assert_nothing_raised{ @enum.partition{} }
-      assert_equal(2, @enum.partition{}.length)
-      assert_kind_of(Array, @enum.partition{}[0])
-      assert_kind_of(Array, @enum.partition{}[1])
-   end
+  test "partition basic functionality" do
+    assert_respond_to(@enum, :partition)
+    assert_nothing_raised{ @enum.partition{} }
+    assert_equal(2, @enum.partition{}.length)
+    assert_kind_of(Array, @enum.partition{}[0])
+    assert_kind_of(Array, @enum.partition{}[1])
+  end
 
-   def test_partition
-      assert_equal([[1,3,5],[2,4]], @enum.partition{ |e| e % 2 != 0 })
-      assert_equal([[1,2,3,4,5],[]], @enum.partition{ |e| e < 10 })
-      assert_equal([[],[1,2,3,4,5]], @enum.partition{ |e| e > 10 })
-   end
+  test "partition returns expected results" do
+    assert_equal([[1,3,5],[2,4]], @enum.partition{ |e| e % 2 != 0 })
+    assert_equal([[1,2,3,4,5],[]], @enum.partition{ |e| e < 10 })
+    assert_equal([[],[1,2,3,4,5]], @enum.partition{ |e| e > 10 })
+  end
 
-   def test_partition_edge_cases
-      assert_equal([[],[]], [].partition{ |e| e > 10 })
-      assert_equal([[nil],[false]], [nil,false].partition{ |e| e.nil? })
-      assert_equal([[false],[nil]], [nil,false].partition{ |e| e == false })
-      assert_equal([[],[nil, false]], [nil,false].partition{})
-   end
+  test "partition where no elements match block conditions returns expected results" do
+    assert_equal([[],[]], [].partition{ |e| e > 10 })
+  end
 
-   def test_partition_expected_errors
-      assert_raise(LocalJumpError){ @enum.partition }
-      assert_raise(ArgumentError){ @enum.partition(true) }
-   end
+  test "partition with explicit nil and false returns expected results" do
+    assert_equal([[nil],[false]], [nil,false].partition{ |e| e.nil? })
+    assert_equal([[false],[nil]], [nil,false].partition{ |e| e == false })
+    assert_equal([[],[nil, false]], [nil,false].partition{})
+  end
 
-   def teardown
-      @enum = nil
-   end
+  test "partition with no arguments returns an Enumerator object" do
+    assert_kind_of(Enumerator, @enum.partition)
+  end
+
+  test "partition does not accept any arguments" do
+    assert_raise(ArgumentError){ @enum.partition(true) }
+  end
+
+  def teardown
+    @enum = nil
+  end
 end
