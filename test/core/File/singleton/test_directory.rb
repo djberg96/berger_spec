c19697b69e1ff709ba2f79751f88c3b388775b8d
Daniel Berger <djberg96@gmail.com>
2015-02-07 20:11:27 -0700

    Refactored.

--- test/core/File/singleton/test_directory.rb
+++ test/core/File/singleton/test_directory.rb
@@ -1,41 +1,47 @@
 #####################################################################
-# tc_directory.rb
+# test_directory.rb
 #
 # Test case for the File.directory? class method.
 #####################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
-class TC_File_Directory_ClassMethod < Test::Unit::TestCase
-   include Test::Helper
+class TC_File_Directory_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def setup
-      if WINDOWS
-         @dir  = "C:\\"
-         @file = "C:\\winnt\\notepad.exe"
-      else
-         @dir  = "/"
-         @file = "/bin/ls"
-      end
-   end
+  def setup
+    if WINDOWS
+      @dir  = "C:\\"
+      @file = "C:\\winnt\\notepad.exe"
+    else
+      @dir  = "/"
+      @file = "/bin/ls"
+    end
+  end
 
-   def test_directory_basic
-      assert_respond_to(File, :directory?)
-      assert_nothing_raised{ File.directory?(@dir) }
-   end
+  test "directory? basic functionality" do
+    assert_respond_to(File, :directory?)
+    assert_nothing_raised{ File.directory?(@dir) }
+    assert_boolean(File.directory?(@dir))
+  end
 
-   def test_directory
-      assert_equal(true, File.directory?(@dir))
-      assert_equal(false, File.directory?(@file))
-   end
+  test "directory? returns expected value" do
+    assert_true(File.directory?(@dir))
+    assert_false(File.directory?(@file))
+  end
 
-   def test_directory_expected_errors
-      assert_raises(ArgumentError){ File.directory? }
-      assert_raises(ArgumentError){ File.directory?(@dir, @file) }
-      assert_raises(TypeError){ File.directory?(nil) }
-   end
+  test "directory? requires a single argument" do
+    assert_raises(ArgumentError){ File.directory? }
+    assert_raises(ArgumentError){ File.directory?(@dir, @file) }
+  end
 
-   def teardown
-      @dir = nil
-   end
+  test "argument to directory? must be a string" do
+    assert_raises(TypeError){ File.directory?(nil) }
+    assert_raises(TypeError){ File.directory?(true) }
+  end
+
+  def teardown
+    @dir = nil
+    @file = nil
+  end
 end
