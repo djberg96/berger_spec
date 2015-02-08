c88d00547d74f0b6d2a952f62750916ca7adac54
Daniel Berger <djberg96@gmail.com>
2015-02-08 08:21:08 -0700

    Refactored and updated.

--- test/core/File/singleton/test_exist.rb
+++ test/core/File/singleton/test_exist.rb
@@ -1,44 +1,42 @@
 #####################################################################
-# tc_exist.rb
+# test_exist.rb
 #
-# Test case for the File.exist? class method and the File.exists?
-# alias.
+# Test case for the File.exist? singleton method.
 #####################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
-class TC_File_Exist_ClassMethod < Test::Unit::TestCase
-   include Test::Helper
+class TC_File_Exist_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def setup
-      @file = 'temp.txt'
-      touch(@file)
-   end
+  def setup
+    @file = 'temp.txt'
+    touch(@file)
+  end
 
-   def test_exist_basic
-      assert_respond_to(File, :exist?)
-      assert_nothing_raised{ File.exist?(@file) }
-   end
+  test "exist? basic functionality" do
+    assert_respond_to(File, :exist?)
+    assert_nothing_raised{ File.exist?(@file) }
+    assert_boolean(File.exist?(@file))
+  end
 
-   def test_exist
-      assert_equal(true, File.exist?(@file))
-      assert_equal(false, File.exist?('bogus'))
-   end
+  test "exist? returns expected result" do
+    assert_true(File.exist?(@file))
+    assert_false(File.exist?('bogus'))
+  end
 
-   def test_exists_alias
-      assert_respond_to(File, :exists?)
-      assert_equal(true, File.exists?(@file))
-      assert_equal(false, File.exists?('bogus'))
-   end
+  test "exist? requires a single argument only" do
+    assert_raises(ArgumentError){ File.exist? }
+    assert_raises(ArgumentError){ File.exist?(@file, @file) }
+  end
 
-   def test_exist_expected_errors
-      assert_raises(ArgumentError){ File.exist? }
-      assert_raises(ArgumentError){ File.exist?(@file, @file) }
-      assert_raises(TypeError){ File.exist?(nil) }
-   end
+  test "argument to exist? must be a string" do
+    assert_raises(TypeError){ File.exist?(nil) }
+    assert_raises(TypeError){ File.exist?(true) }
+  end
 
-   def teardown
-      remove_file(@file)
-      @file = nil
-   end
+  def teardown
+    remove_file(@file)
+    @file = nil
+  end
 end
