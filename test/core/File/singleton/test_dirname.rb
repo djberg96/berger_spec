2edc7d8c3545f74b71666f25c6d0327b3b2a622a
Daniel Berger <djberg96@gmail.com>
2015-02-08 08:17:43 -0700

    Refactored and updated some root path tests.

--- test/core/File/singleton/test_dirname.rb
+++ test/core/File/singleton/test_dirname.rb
@@ -1,86 +1,90 @@
 ###########################################
-# tc_dirname.rb
+# test_dirname.rb
 #
 # Test suite for the File.dirname method.
 ###########################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
-class TC_File_Dirname_ClassMethod < Test::Unit::TestCase
-   include Test::Helper
-   
-   def setup
-      @msg  = '=> Known issue on MS Windows'
-      @path = WINDOWS ? "C:\\foo\\bar.txt" : "/foo/bar.txt"
-   end
-   
-   def test_basic
-      assert_respond_to(File, :dirname)
-      assert_nothing_raised{ File.dirname("foo") }
-      assert_kind_of(String, File.dirname("foo"))
-   end
+class TC_File_Dirname_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def test_dirname_unix
-      assert_equal(".", File.dirname("foo"))
-      assert_equal("/", File.dirname("/foo"))
-      assert_equal("/foo", File.dirname("/foo/bar"))
-      assert_equal("/foo", File.dirname("/foo/bar.txt"))
-      assert_equal("/foo/bar", File.dirname("/foo/bar/baz"))
-      assert_equal(".", File.dirname("./foo"))
-      assert_equal("./foo", File.dirname("./foo/bar"))      
-   end
+  def setup
+    @path = WINDOWS ? "C:\\foo\\bar.txt" : "/foo/bar.txt"
+  end
 
-   def test_dirname_edge_cases
-      assert_equal(".", File.dirname(""))
-      assert_equal(".", File.dirname("."))
-      assert_equal(".", File.dirname(".."))
-      assert_equal("/", File.dirname("/"))
-      assert_equal("/", File.dirname("/foo/"))
-      assert_equal("/", File.dirname("//foo"), @msg)
-      assert_equal("/", File.dirname("//foo//"))
-   end
+  test "dirname basic functionality" do
+    assert_respond_to(File, :dirname)
+    assert_nothing_raised{ File.dirname("foo") }
+    assert_kind_of(String, File.dirname("foo"))
+  end
 
-   def test_tainted_dirname_returns_tainted_string
-      assert_equal(false, File.dirname(@path).tainted?)
-      assert_nothing_raised{ @path.taint }
-      assert_equal(true, File.dirname(@path).tainted?)
-   end
+  test "dirname on unix style paths returns the expected result" do
+    assert_equal(".", File.dirname("foo"))
+    assert_equal("/", File.dirname("/foo"))
+    assert_equal("/foo", File.dirname("/foo/bar"))
+    assert_equal("/foo", File.dirname("/foo/bar.txt"))
+    assert_equal("/foo/bar", File.dirname("/foo/bar/baz"))
+    assert_equal(".", File.dirname("./foo"))
+    assert_equal("./foo", File.dirname("./foo/bar"))
+  end
 
-   def test_dirname_expected_errors
-      assert_raises(TypeError){ File.dirname(nil) }
-      assert_raises(TypeError){ File.dirname(0) }
-      assert_raises(TypeError){ File.dirname(true) }
-      assert_raises(TypeError){ File.dirname(false) }
-   end
+  test "dirname on '.', '..' or an empty string returns '.'" do
+    assert_equal(".", File.dirname(""))
+    assert_equal(".", File.dirname("."))
+    assert_equal(".", File.dirname(".."))
+  end
 
-   # Windows specific tests
-   if WINDOWS
-      def test_dirname_windows_forward_slash
-         assert_equal("C:/", File.dirname("C:/"))
-         assert_equal("C:/", File.dirname("C:/foo"))
-         assert_equal("C:/foo", File.dirname("C:/foo/bar"))
-         assert_equal("C:/foo", File.dirname("C:/foo/bar/"))
-         assert_equal("C:/foo", File.dirname("C:/foo/bar//"))
-      end
+  test "dirname returns root path when expected to" do
+    assert_equal("/", File.dirname("/"))
+    assert_equal("/", File.dirname("/foo/"))
+  end
 
-      def test_dirname_windows
-         assert_equal("C:\\foo\\bar", File.dirname("C:\\foo\\bar\\baz.txt"))
-         assert_equal("C:\\foo", File.dirname("C:\\foo\\bar"))
-         assert_equal("C:\\foo", File.dirname("C:\\foo\\bar\\"))
-         assert_equal("C:\\", File.dirname("C:\\foo"))
-         assert_equal("C:\\", File.dirname("C:\\"))
-      end
+  test "dirname with multiple leading slashes returns the expected result" do
+    assert_equal("//foo", File.dirname("//foo"))
+    assert_equal("//foo", File.dirname("//foo//"))
+  end
 
-      def test_dirname_windows_unc
-         assert_equal("\\\\foo\\bar", File.dirname("\\\\foo\\bar\\baz.txt"))
-         assert_equal("\\\\foo\\bar", File.dirname("\\\\foo\\bar\\baz"))
-         assert_equal("\\\\foo", File.dirname("\\\\foo"))
-         assert_equal("\\\\foo\\bar", File.dirname("\\\\foo\\bar"))
-      end
-   end
+  test "dirname returns a tainted string if its argument is tainted" do
+    assert_false(File.dirname(@path).tainted?)
+    assert_nothing_raised{ @path.taint }
+    assert_true(File.dirname(@path).tainted?)
+  end
 
-   def teardown
-      @msg  = nil
-      @path = nil
-   end
+  test "dirname requires a string argument" do
+    assert_raises(TypeError){ File.dirname(nil) }
+    assert_raises(TypeError){ File.dirname(0) }
+    assert_raises(TypeError){ File.dirname(true) }
+    assert_raises(TypeError){ File.dirname(false) }
+  end
+
+  # Windows specific tests
+  if WINDOWS
+    test "dirname handles windows paths with forward slashes" do
+      assert_equal("C:/", File.dirname("C:/"))
+      assert_equal("C:/", File.dirname("C:/foo"))
+      assert_equal("C:/foo", File.dirname("C:/foo/bar"))
+      assert_equal("C:/foo", File.dirname("C:/foo/bar/"))
+      assert_equal("C:/foo", File.dirname("C:/foo/bar//"))
+    end
+
+    test "dirname handles windows paths with backslashes" do
+      assert_equal("C:\\foo\\bar", File.dirname("C:\\foo\\bar\\baz.txt"))
+      assert_equal("C:\\foo", File.dirname("C:\\foo\\bar"))
+      assert_equal("C:\\foo", File.dirname("C:\\foo\\bar\\"))
+      assert_equal("C:\\", File.dirname("C:\\foo"))
+      assert_equal("C:\\", File.dirname("C:\\"))
+    end
+
+    test "dirname handles UNC paths properly" do
+      assert_equal("\\\\foo\\bar", File.dirname("\\\\foo\\bar\\baz.txt"))
+      assert_equal("\\\\foo\\bar", File.dirname("\\\\foo\\bar\\baz"))
+      assert_equal("\\\\foo", File.dirname("\\\\foo"))
+      assert_equal("\\\\foo\\bar", File.dirname("\\\\foo\\bar"))
+    end
+  end
+
+  def teardown
+    @path = nil
+  end
 end
