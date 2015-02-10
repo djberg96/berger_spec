f2432d8ad9a659c1f0f9392da0b79f2ba9d64f12
Daniel Berger <djberg96@gmail.com>
2015-02-10 12:17:50 -0700

    Updated and refactored.

--- test/core/File/singleton/test_file.rb
+++ test/core/File/singleton/test_file.rb
@@ -1,49 +1,50 @@
 #####################################################################
-# tc_file.rb
+# test_file.rb
 #
 # Test case for the File.file? class method.
 #####################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
-class TC_File_File_ClassMethod < Test::Unit::TestCase
-   include Test::Helper
-   
-   def setup
-      if WINDOWS
-         @null = "NUL"
-         @dir  = "C:\\"
-      else
-         @null = "/dev/null"
-         @dir  = "/bin"
-      end
+class TC_File_File_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-      @msg  = '=> May fail on MS Windows'
-      @file = "test.txt"
-      touch(@file)
-   end
+  def setup
+    if WINDOWS
+      @null = "NUL"
+      @dir  = "C:\\"
+    else
+      @null = "/dev/null"
+      @dir  = "/bin"
+    end
 
-   def test_file_basic
-      assert_respond_to(File, :file?)
-      assert_nothing_raised{ File.file?(@file) }
-   end
+    @msg  = '=> May fail on MS Windows'
+    @file = "test.txt"
+    touch(@file)
+  end
 
-   def test_file
-      assert_equal(true, File.file?(@file))
-      assert_equal(false, File.file?(@dir))
-      assert_equal(false, File.file?(@null), @msg)
-   end
+  test "file? basic functionality" do
+    assert_respond_to(File, :file?)
+    assert_nothing_raised{ File.file?(@file) }
+    assert_boolean(File.file?(@file))
+  end
 
-   def test_file_expected_errors
-      assert_raises(ArgumentError){ File.file? }
-      assert_raises(ArgumentError){ File.file?(@null, @file) }
-      assert_raises(TypeError){ File.file?(nil) }
-   end
+  test "file? returns expected results" do
+    assert_true(File.file?(@file))
+    assert_false(File.file?(@dir))
+    assert_false(File.file?(@null), @msg)
+  end
 
-   def teardown
-      remove_file(@file)
-      @null = nil
-      @file = nil
-      @msg  = nil
-   end
+  test "file? requires a single string argument" do
+    assert_raises(ArgumentError){ File.file? }
+    assert_raises(ArgumentError){ File.file?(@null, @file) }
+    assert_raises(TypeError){ File.file?(nil) }
+  end
+
+  def teardown
+    remove_file(@file)
+    @null = nil
+    @file = nil
+    @msg  = nil
+  end
 end
