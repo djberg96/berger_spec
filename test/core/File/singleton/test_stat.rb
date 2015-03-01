6b0f13b0e7a66712999f0d642ab4b7ae136d103b
Daniel Berger <djberg96@gmail.com>
2015-03-01 08:57:48 -0700

    Refactored.

--- test/core/File/singleton/test_stat.rb
+++ test/core/File/singleton/test_stat.rb
@@ -1,12 +1,12 @@
 ########################################################################
-# tc_stat.rb
+# test_stat.rb
 #
 # Test case for the File.stat method.
 ########################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
-class TC_File_Stat_ClassMethod < Test::Unit::TestCase
+class TC_File_Stat_SingletonMethod < Test::Unit::TestCase
   include Test::Helper
 
   def setup
@@ -20,28 +20,26 @@ class TC_File_Stat_ClassMethod < Test::Unit::TestCase
     end
   end
 
-  def test_stat_basic
+  test "stat basic functionality" do
     assert_respond_to(File, :stat)
   end
 
-  def test_stat
+  test "stat returns a File::Stat object" do
     assert_nothing_raised{ @stat = File.stat(@file) }
     assert_kind_of(File::Stat, File.stat(@file))
   end
 
-  unless WINDOWS
-    def test_stat_symlink
-      assert_nothing_raised{ @stat = File.stat(@link) }
-      assert_equal(false, @stat.symlink?)
-    end
+  test "stat object symlink method returns expected result" do
+    omit_if(WINDOWS)
+    assert_nothing_raised{ @stat = File.stat(@link) }
+    assert_false(@stat.symlink?)
   end
 
-  def test_stat_edge_cases
+  test "calling stat on a null device does not raise an error" do
     assert_nothing_raised{ File.stat(@null) }
   end
 
-  # Do some superficial testing of the File::Stat object.
-  def test_stat_instance_methods
+  test "stat object instance methods basic functionality" do
     assert_nothing_raised{ @stat = File.stat(@file) }
     assert_respond_to(@stat, :atime)
     assert_respond_to(@stat, :blksize)
@@ -50,11 +48,17 @@ class TC_File_Stat_ClassMethod < Test::Unit::TestCase
     assert_respond_to(@stat, :size)
   end
 
-  def test_stat_expected_errors
+  test "an error is raised if the file doesn't exist" do
     assert_raise(Errno::ENOENT){ File.stat('bogus') }
     assert_raise(Errno::ENOENT){ File.stat('') }
+  end
+
+  test "stat requires a single argument only" do
     assert_raise(ArgumentError){ File.stat }
     assert_raise(ArgumentError){ File.stat(@file, @file) }
+  end
+
+  test "argument to stat must be a string" do
     assert_raise(TypeError){ File.stat(1) }
   end
 
