16af6fcec72461a746355ce6fba3c804af6654dd
Daniel Berger <djberg96@gmail.com>
2015-03-02 09:36:47 -0700

    Refactored.

--- test/core/File/singleton/test_chown.rb
+++ test/core/File/singleton/test_chown.rb
@@ -1,7 +1,7 @@
 ##########################################################################
-# tc_chown.rb
+# test_chown.rb
 #
-# Test suite for the File.chown? class method.  These tests are skipped
+# Test suite for the File.chown? class method. These tests are skipped
 # on MS Windows.
 #
 # On UNIX systems, I require the 'etc' package in order to get a user and
@@ -22,57 +22,65 @@ require 'test/unit'
 require 'etc'
 
 class TC_File_Chown_ClassMethod < Test::Unit::TestCase
-   include Test::Helper
-   
-   if WINDOWS
-      def test_stub
-         # Stub to keep Test::Unit from whining about no defined tests.
-      end
-   else
-      def setup
-         @file1 = "temp1.txt"
-         @file2 = "temp2.txt"
-         @root  = Process.euid == 0
-         @uid   = Etc.getpwnam('nobody').uid
-         @gid   = Etc.getgrnam('nobody').gid
-         touch(@file1)
-         touch(@file2)
-      end
+  include Test::Helper
 
-      def test_chown_basic
-         assert_respond_to(File, :chown)
-         assert_nothing_raised{ File.chown(-1, -1, @file1) }
-         assert_nothing_raised{ File.chown(-1, -1) } # No files is ok (?)
-      end
+  def setup
+    @file1 = "temp1.txt"
+    @file2 = "temp2.txt"
+    @root  = Process.euid == 0
+    @uid   = Etc.getpwnam('nobody').uid
+    @gid   = Etc.getgrnam('nobody').gid
+    touch(@file1)
+    touch(@file2)
+  end
 
-      def test_chown
-         assert_equal(1, File.chown(-1, -1, @file1))
-         if @root
-            assert_equal(1, File.chown(@uid, -1, @file1))
-            assert_equal(1, File.chown(-1, @gid, @file1))
-            assert_equal(1, File.chown(@uid, @gid, @file1))
-            assert_equal(2, File.chown(@uid, @gid, @file1, @file2))
-         end
-      end
+  test "chown basic functionality" do
+    omit_if(WINDOWS)
+    assert_respond_to(File, :chown)
+    assert_nothing_raised{ File.chown(-1, -1, @file1) }
+    assert_kind_of(Numeric, File.chown(-1, -1, @file1))
+  end
 
-      def test_chown_edge_cases
-         assert_equal(0, File.chown(-1, -1)) # Odd
-      end
+  test "chown does not complain if no files are provided" do
+    omit_if(WINDOWS)
+    assert_nothing_raised{ File.chown(-1, -1) }
+    assert_equal(0, File.chown(-1, -1))
+  end
 
-      def test_chown_expected_errors
-         assert_raises(ArgumentError){ File.chown(-1) }
-         assert_raises(TypeError){ File.chown('bogus', -1) }
-         assert_raises(TypeError){ File.chown(-1, 'bogus') }
-      end
+  test "chown ignores nil or -1 arguments" do
+    omit_if(WINDOWS)
+    assert_equal(1, File.chown(-1, -1, @file1))
+    assert_equal(1, File.chown(nil, nil, @file1))
+    assert_equal(1, File.chown(-1, nil, @file1))
+    assert_equal(1, File.chown(nil, -1, @file1))
+  end
 
-      def teardown
-         File.delete(@file1) if File.exists?(@file1)
-         File.delete(@file2) if File.exists?(@file2)
+  test "chown works as expected" do
+    omit_if(WINDOWS)
+    omit_unless(@root)
+    assert_equal(1, File.chown(@uid, -1, @file1))
+    assert_equal(1, File.chown(-1, @gid, @file1))
+    assert_equal(1, File.chown(@uid, @gid, @file1))
+    assert_equal(2, File.chown(@uid, @gid, @file1, @file2))
+  end
 
-         @file = nil
-         @root = nil
-         @uid  = nil
-         @gid  = nil
-      end
-   end
+
+  test "chown requires at least two arguments" do
+    assert_raises(ArgumentError){ File.chown(-1) }
+  end
+
+  test "first two arguments to chown must be numeric if not nil" do
+    assert_raises(TypeError){ File.chown('bogus', -1) }
+    assert_raises(TypeError){ File.chown(-1, 'bogus') }
+  end
+
+  def teardown
+    File.delete(@file1) if File.exist?(@file1)
+    File.delete(@file2) if File.exist?(@file2)
+
+    @file = nil
+    @root = nil
+    @uid  = nil
+    @gid  = nil
+  end
 end
