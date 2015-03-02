278b9df5dec1cfff1c4f75d44d4868a5a81370f1
Daniel Berger <djberg96@gmail.com>
2015-03-02 05:35:37 -0700

    Refactored.

--- test/core/File/singleton/test_atime.rb
+++ test/core/File/singleton/test_atime.rb
@@ -1,34 +1,40 @@
 #####################################################################
-# tc_atime.rb
+# test_atime.rb
 #
 # Test case for the File.atime class method.
 #####################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
-class TC_File_Atime_ClassMethod < Test::Unit::TestCase
-   include Test::Helper
-   
-   def setup
-      @file = File.join(Dir.pwd, 'test.txt')
-      touch(@file)
-   end
+class TC_File_Atime_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def test_atime
-      assert_respond_to(File, :atime)
-      assert_nothing_raised{ File.atime(@file) }
-      assert_kind_of(Time, File.atime(@file))
-   end
+  def setup
+    @file = File.join(Dir.pwd, 'test.txt')
+    touch(@file)
+  end
 
-   def test_atime_expected_errors
-      assert_raises(Errno::ENOENT){ File.atime('bogus') }
-      assert_raises(ArgumentError){ File.atime }
-      assert_raises(ArgumentError){ File.atime(@file, @file) }
-      assert_raises(TypeError){ File.atime(1) }
-   end
+  test "atime basic functionality" do
+    assert_respond_to(File, :atime)
+    assert_nothing_raised{ File.atime(@file) }
+    assert_kind_of(Time, File.atime(@file))
+  end
 
-   def teardown
-      remove_file(@file)
-      @file = nil
-   end
+  test "atime raises an error if the file does not exist" do
+    assert_raises(Errno::ENOENT){ File.atime('bogus') }
+  end
+
+  test "atime requires a single argument" do
+    assert_raises(ArgumentError){ File.atime }
+    assert_raises(ArgumentError){ File.atime(@file, @file) }
+  end
+
+  test "argument to atime must be a string" do
+    assert_raises(TypeError){ File.atime(1) }
+  end
+
+  def teardown
+    remove_file(@file)
+    @file = nil
+  end
 end
