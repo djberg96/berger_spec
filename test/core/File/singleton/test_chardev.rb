f38d57cc7ada27c09dd9ebe2aad13b9096d6c7fb
Daniel Berger <djberg96@gmail.com>
2015-03-02 05:48:32 -0700

    Refactored.

--- test/core/File/singleton/test_chardev.rb
+++ test/core/File/singleton/test_chardev.rb
@@ -1,50 +1,54 @@
 #####################################################################
-# tc_chardev.rb
+# test_is_chardev.rb
 #
 # Test case for the File.chardev? class method.
 #####################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
-class TC_File_Chardev_ClassMethod < Test::Unit::TestCase
-   include Test::Helper
-   
-   def setup
-      if WINDOWS
-         @char_dev  = 'NUL'
-         @other_dev = "C:\\"
-      else
-         @char_dev  = "/dev/null"
-         @other_dev = "/"
-      end
-   end
+class TC_File_Chardev_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def test_chardev_basic
-      assert_respond_to(File, :chardev?)
-      assert_nothing_raised{ File.chardev?(@char_dev) }
-   end
+  def setup
+    if WINDOWS
+      @char_dev  = 'NUL'
+      @other_dev = "C:\\"
+    else
+      @char_dev  = "/dev/null"
+      @other_dev = "/"
+    end
+  end
 
-   # As of Ruby 1.8.6, the File.chardev? method is not supported on Windows,
-   # although it is possible to implement. See win32-file, for example.
-   # 
-   def test_chardev
-      if WINDOWS
-         assert_equal(false, File.chardev?(@char_dev))
-      else
-         assert_equal(true, File.chardev?(@char_dev))
-      end
-      assert_equal(false, File.chardev?(@other_dev))
-      assert_equal(false, File.chardev?('bogus')) # Errno::ENOENT?
-   end
+  test "chardev? basic functionality" do
+    assert_respond_to(File, :chardev?)
+    assert_nothing_raised{ File.chardev?(@char_dev) }
+    assert_boolean(File.chardev?(@char_dev))
+  end
 
-   def test_chardev_expected_errors
-      assert_raises(ArgumentError){ File.chardev? }
-      assert_raises(ArgumentError){ File.chardev?(@char_dev, @other_dev) }
-      assert_raises(TypeError){ File.chardev?(nil) }
-   end
+  test "chardev? returns the expected results" do
+    if WINDOWS
+      assert_false(File.chardev?(@char_dev))
+    else
+      assert_true(File.chardev?(@char_dev))
+    end
+    assert_false(File.chardev?(@other_dev))
+  end
 
-   def teardown
-      @char_dev  = nil
-      @other_dev = nil
-   end
+  test "chardev? returns false if the file does not exist" do
+    assert_false(File.chardev?('bogus'))
+  end
+
+  test "chardev? requires a single argument" do
+    assert_raises(ArgumentError){ File.chardev? }
+    assert_raises(ArgumentError){ File.chardev?(@char_dev, @other_dev) }
+  end
+
+  test "argument to chardev? must be a string" do
+    assert_raises(TypeError){ File.chardev?(nil) }
+  end
+
+  def teardown
+    @char_dev  = nil
+    @other_dev = nil
+  end
 end
