6f6f526acbaf16ea8b2b7241653fdd919c7cb2af
Daniel Berger <djberg96@gmail.com>
2015-03-02 13:40:40 -0700

    Refactored.

--- test/core/File/instance/test_chown.rb
+++ test/core/File/instance/test_chown.rb
@@ -1,5 +1,5 @@
 ##########################################################################
-# tc_chown.rb
+# test_chown.rb
 #
 # Test suite for the File#chown? instance method. These tests are skipped
 # on MS Windows.
@@ -15,64 +15,65 @@
 # skipped.
 ##########################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
 class TC_File_Chown_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
-   
-   if WINDOWS
-      def test_stub
-         # Stub to keep Test::Unit from whining about no defined tests.
-      end
-   else
-      def setup
-         @name1 = "temp1.txt"
-         @name2 = "temp2.txt"
+  include Test::Helper
 
-         touch(@name1)
-         touch(@name2)
+  def setup
+    @name1 = "temp1.txt"
+    @name2 = "temp2.txt"
 
-         @file1 = File.open(@name1)
-         @file2 = File.open(@name2)
-         @uid   = Etc.getpwnam('nobody').uid
-         @gid   = Etc.getgrnam('nobody').gid
-      end
+    touch(@name1)
+    touch(@name2)
 
-      def test_chown_basic
-         assert_respond_to(@file1, :chown)
-         assert_nothing_raised{ @file2.chown(-1, -1) }
-      end
+    @file1 = File.open(@name1)
+    @file2 = File.open(@name2)
+    @uid   = Etc.getpwnam('nobody').uid
+    @gid   = Etc.getgrnam('nobody').gid
+  end
 
-      def test_chown
-         assert_equal(0, @file1.chown(-1, -1))
-         if ROOT
-            assert_equal(0, @file1.chown(@uid, -1, @file1))
-            assert_equal(0, @file1.chown(-1, @gid))
-            assert_equal(0, @file1.chown(@uid, @gid))
-         end
-      end
+  def test_chown_basic
+    assert_respond_to(@file1, :chown)
+    assert_nothing_raised{ @file2.chown(-1, -1) }
+  end
 
-      def test_chown_fails_on_closed_handle
-         assert_nothing_raised{ @file1.close }
-         assert_raise(IOError){ @file1.chown(-1, -1) }
-      end
+  test "chown as standard user works as expected" do
+    omit_if(WINDOWS)
+    assert_equal(0, @file1.chown(-1, -1))
+  end
 
-      def test_chown_expected_errors
-         assert_raises(ArgumentError){ @file1.chown(-1) }
-         assert_raises(TypeError){ @file1.chown('bogus', -1) }
-         assert_raises(TypeError){ @file1.chown(-1, 'bogus') }
-      end
+  test "chown as privileged user works as expected" do
+    omit_if(WINDOWS)
+    omit_unless(ROOT)
+    assert_equal(0, @file1.chown(@uid, -1, @file1))
+    assert_equal(0, @file1.chown(-1, @gid))
+    assert_equal(0, @file1.chown(@uid, @gid))
+  end
 
-      def teardown
-         @file1.close unless @file1.closed?
-         @file2.close unless @file2.closed?
+  test "chown raises an error if the handle is closed" do
+    @file1.close
+    assert_raise(IOError){ @file1.chown(-1, -1) }
+  end
 
-         File.delete(@name1) if File.exists?(@name1)
-         File.delete(@name2) if File.exists?(@name2)
+  test "chown raises an error if the numeric argument is invalid" do
+    assert_raises(ArgumentError){ @file1.chown(-1) }
+  end
 
-         @file = nil
-         @uid  = nil
-         @gid  = nil
-      end
-   end
+  test "arguments to chown must be numeric" do
+    assert_raises(TypeError){ @file1.chown('bogus', -1) }
+    assert_raises(TypeError){ @file1.chown(-1, 'bogus') }
+  end
+
+  def teardown
+    @file1.close unless @file1.closed?
+    @file2.close unless @file2.closed?
+
+    File.delete(@name1) if File.exist?(@name1)
+    File.delete(@name2) if File.exist?(@name2)
+
+    @file = nil
+    @uid  = nil
+    @gid  = nil
+  end
 end
