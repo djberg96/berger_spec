ae47b65e7a5c22e747f9d8d3aab1228b5cc5c176
Daniel Berger <djberg96@gmail.com>
2015-03-07 07:10:40 -0700

    Updated tests for Windows.

--- test/core/File/singleton/test_split.rb
+++ test/core/File/singleton/test_split.rb
@@ -20,24 +20,25 @@ class TC_File_Split_SingletonMethod < Test::Unit::TestCase
     omit_unless(WINDOWS)
     assert_equal(['//foo/bar', 'baz'], File.split("//foo/bar/baz"))
     assert_equal(['//foo/bar', 'baz'], File.split("//foo/bar/baz/"))
-    assert_equal(['//foo/bar', ''], File.split("//foo/bar"))
-    assert_equal(['//foo/bar', ''], File.split("//foo/bar/"))
-    assert_equal(['//foo', ''], File.split("//foo"))
-    assert_equal(['//foo', ''], File.split("//foo/"))
+    assert_equal(['//foo/bar', '/'], File.split("//foo/bar"))
+    assert_equal(['//foo/bar', '/'], File.split("//foo/bar/"))
+    assert_equal(['//foo', '/'], File.split("//foo"))
+    assert_equal(['//foo', '/'], File.split("//foo/"))
   end
 
   test "split works as expected on root windows paths" do
     omit_unless(WINDOWS)
-    assert_equal(["C:\\", ''], File.split("C:\\"))
+    assert_equal(["C:/", '/'], File.split("C:/"))
+    assert_equal(["C:\\", '\\'], File.split("C:\\"))
     assert_equal(['C:/foo/bar', 'baz'], File.split("C:/foo/bar/baz"))
     assert_equal(["C:\\foo\\bar", "baz"], File.split("C:\\foo\\bar\\baz"))
   end
 
   test "split ignores leading slashes" do
     omit_unless(WINDOWS)
-    assert_equal(['//foo', ''], File.split('//////foo'))
-    assert_equal(['//foo', ''], File.split('//////foo///'))
-    assert_equal(['//foo', ''], File.split('//////foo//bar'))
+    assert_equal(['//foo', '/'], File.split('//////foo'))
+    assert_equal(['//foo', '/'], File.split('//////foo///'))
+    assert_equal(['//foo', 'bar'], File.split('//////foo//bar'))
   end
 
   test "split basic functionality" do
@@ -74,10 +75,17 @@ class TC_File_Split_SingletonMethod < Test::Unit::TestCase
     assert_equal(['/foo', 'bar'], File.split('/foo/bar////'))
   end
 
-  test "split returns expected results for edge cases" do
+  test "split returns expected result for empty string" do
     assert_equal(['.', ''], File.split(''))
-    assert_equal(['.', ' '], File.split(' '))
+  end
+
+  test "split returns expected result for '.' path" do
     assert_equal(['.', '.'], File.split('.'))
+  end
+
+  test "split returns expected result for whitespace" do
+    omit_if(WINDOWS)
+    assert_equal(['.', ' '], File.split(' '))
   end
 
   test "split requires a single argument" do
