38da32e5fc1355369356bc92d5f3df960cb98a4c
Daniel Berger <djberg96@gmail.com>
2015-03-07 13:14:02 -0700

    Removed mkmf-lite stuff.

--- test/core/File/singleton/test_lchmod.rb
+++ test/core/File/singleton/test_lchmod.rb
@@ -3,18 +3,11 @@
 #
 # Test case for the File.lchmod singleton method.
 #####################################################################
-require 'mkmf/lite'
 require 'test/helper'
 require 'test/unit'
 
 class TC_File_Lchmod_SingletonMethod < Test::Unit::TestCase
   include Test::Helper
-  extend Mkmf::Lite
-
-  def self.startup
-    @@lchmod_support = have_func('lchmod')
-    @@lchmod_message = "lchmod not supported on this platform"
-  end
 
   def setup
     @file1 = File.expand_path(__FILE__)
@@ -24,27 +17,27 @@ class TC_File_Lchmod_SingletonMethod < Test::Unit::TestCase
   end
 
   test "lchmod basic functionality" do
-    omit_unless(@@lchmod_support, @@lchmod_message)
+    omit_if(WINDOWS)
     assert_respond_to(File, :lchmod)
     assert_nothing_raised{ File.lchmod(0644, @file1) }
     assert_kind_of(Fixnum, File.lchmod(0644, @file1))
   end
 
   test "lchmod can be called on multiple files" do
-    omit_unless(@@lchmod_support, @@lchmod_message)
+    omit_if(WINDOWS)
     assert_nothing_raised{ File.lchmod(0644, @file1, @file2) }
     assert_equal('100644', File.stat(@file1).mode.to_s(8))
     assert_equal('100644', File.stat(@file2).mode.to_s(8))
   end
 
   test "lchmod returns the number of files modified" do
-    omit_unless(@@lchmod_support, @@lchmod_message)
+    omit_if(WINDOWS)
     assert_equal(1, File.lchmod(0644, @file1))
     assert_equal(2, File.lchmod(0644, @file1, @file2))
   end
 
   test "lchmod sets the permissions as expected" do
-    omit_unless(@@lchmod_support, @@lchmod_message)
+    omit_if(WINDOWS)
     assert_nothing_raised{ File.lchmod(0644, @file1) }
     assert_equal('100644', File.stat(@file1).mode.to_s(8))
     assert_nothing_raised{ File.lchmod(0444, @file2) }
@@ -52,24 +45,24 @@ class TC_File_Lchmod_SingletonMethod < Test::Unit::TestCase
   end
 
   test "lchmod does not actually require any file arguments" do
-    omit_unless(@@lchmod_support, @@lchmod_message)
+    omit_if(WINDOWS)
     notify("File.lchmod with no files should probably be an ArgumentError")
     assert_nothing_raised{ File.lchmod(0444) }
     assert_equal(0, File.lchmod(0444))
   end
 
   test "lchmod requires at least one argument" do
-    omit_unless(@@lchmod_support, @@lchmod_message)
+    omit_if(WINDOWS)
     assert_raises(ArgumentError){ File.lchmod }
   end
 
   test "the first argument to lchmod must be numeric" do
-    omit_unless(@@lchmod_support, @@lchmod_message)
+    omit_if(WINDOWS)
     assert_raises(TypeError){ File.lchmod('0644') }
   end
 
   def teardown
-    if @@lchmod_support
+    unless WINDOWS
       File.lchmod(@file1_mode, @file1)
       File.lchmod(@file2_mode, @file2)
     end
@@ -77,10 +70,5 @@ class TC_File_Lchmod_SingletonMethod < Test::Unit::TestCase
     @file2 = nil
     @file1_mode = nil
     @file2_mode = nil
-  end
-
-  def self.shutdown
-    @@lchmod_support = nil
-    @@lchmod_message = nil
   end
 end
