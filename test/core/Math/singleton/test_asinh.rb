5125b61ef1b5a80655953f233db1bbd3cade68af
Daniel Berger <djberg96@gmail.com>
2015-04-14 12:05:01 -0600

    Updated and refactored for Ruby 2.2.

--- test/core/Math/singleton/test_asinh.rb
+++ test/core/Math/singleton/test_asinh.rb
@@ -1,42 +1,50 @@
 #####################################################################
-# tc_asinh.rb
+# test_asinh.rb
 #
 # Test cases for the Math.asinh method.
 #####################################################################
 require 'test/helper'
 require 'test/unit'
 
-class TC_Math_Asinh_Class < Test::Unit::TestCase
-   def test_asinh_basic
-      assert_respond_to(Math, :asinh)
-      assert_nothing_raised{ Math.asinh(1) }
-      assert_kind_of(Float, Math.asinh(1))
-   end
-   
-   def test_asinh_positive
-      assert_nothing_raised{ Math.asinh(1) }
-      assert_nothing_raised{ Math.asinh(100) }
-      assert_in_delta(0.88, Math.asinh(1), 0.01)
-   end
+class TC_Math_Asinh_SingletonMethod < Test::Unit::TestCase
+  test "asinh basic functionality" do
+    assert_respond_to(Math, :asinh)
+    assert_nothing_raised{ Math.asinh(1) }
+    assert_kind_of(Float, Math.asinh(1))
+  end
 
-   def test_asinh_zero
-      assert_nothing_raised{ Math.asinh(0) }
-      assert_equal(0.0, Math.asinh(0))
-   end
+  test "asinh with positive integer argument" do
+    assert_nothing_raised{ Math.asinh(1) }
+    assert_nothing_raised{ Math.asinh(100) }
+    assert_in_delta(0.88, Math.asinh(1), 0.01)
+  end
 
-   def test_asinh_negative
-      assert_nothing_raised{ Math.asinh(-1) }
-      assert_nothing_raised{ Math.asinh(-100) }
-      assert_in_delta(-0.88, Math.asinh(-1), 0.01)
-   end
-   
-   def test_asinh_positive_float
-      assert_nothing_raised{ Math.asinh(0.345) }
-      assert_in_delta(0.338, Math.asinh(0.345), 0.01)
-   end
-   
-   def test_asinh_negative_float
-      assert_nothing_raised{ Math.asinh(-0.345) }
-      assert_in_delta(-0.338, Math.asinh(-0.345), 0.01)
-   end
+  test "asinh with zero argument" do
+    assert_nothing_raised{ Math.asinh(0) }
+    assert_equal(0.0, Math.asinh(0))
+  end
+
+  test "asinh with negative integer argument" do
+    assert_nothing_raised{ Math.asinh(-1) }
+    assert_nothing_raised{ Math.asinh(-100) }
+    assert_in_delta(-0.88, Math.asinh(-1), 0.01)
+  end
+
+  test "asinh with positive float argument" do
+    assert_nothing_raised{ Math.asinh(0.345) }
+    assert_in_delta(0.338, Math.asinh(0.345), 0.01)
+  end
+
+  test "asinh with negative float argument" do
+    assert_nothing_raised{ Math.asinh(-0.345) }
+    assert_in_delta(-0.338, Math.asinh(-0.345), 0.01)
+  end
+
+  test "asinh requires a numeric argument" do
+    assert_raises(TypeError){ Math.asinh('test') }
+  end
+
+  test "asinh requires a single argument only" do
+    assert_raises(ArgumentError){ Math.asinh(1,1) }
+  end
 end
