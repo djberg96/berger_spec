fe4b33d294eaf3084fd7bf4dd1a2d8e1017019b5
Daniel Berger <djberg96@gmail.com>
2015-04-14 11:59:34 -0600

    Refactored and updated for Ruby 2.2.

--- test/core/Math/singleton/test_asin.rb
+++ test/core/Math/singleton/test_asin.rb
@@ -1,5 +1,5 @@
 #####################################################################
-# tc_asin.rb
+# test_asin.rb
 #
 # Test cases for the Math.asin method.
 #####################################################################
@@ -7,39 +7,48 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Math_Asin_Class < Test::Unit::TestCase
-   def test_asin_basic
-      assert_respond_to(Math, :asin)
-      assert_nothing_raised{ Math.asin(1) }
-      assert_kind_of(Float, Math.asin(1))
-   end
-   
-   def test_asin_positive
-      assert_nothing_raised{ Math.asin(1) }
-      assert_in_delta(1.57, Math.asin(1), 0.01)
-   end
-   
-   def test_asin_zero
-      assert_nothing_raised{ Math.asin(0) }
-      assert_equal(0.0, Math.asin(0))
-   end
-   
-   def test_asin_negative
-      assert_nothing_raised{ Math.asin(-1) }
-      assert_in_delta(-1.57, Math.asin(-1), 0.01)
-   end
-   
-   def test_asin_positive_float
-      assert_nothing_raised{ Math.asin(0.345) }
-      assert_in_delta(0.35, Math.asin(0.345), 0.01)
-   end
-   
-   def test_asin_negative_float
-      assert_nothing_raised{ Math.asin(-0.345) }
-      assert_in_delta(-0.35, Math.asin(-0.345), 0.01)
-   end
-   
-   def test_asin_expected_errors
-       assert_raises(Errno::EDOM){ Math.asin(2) }
-       assert_raises(Errno::EDOM){ Math.asin(-2) }
-   end
+  test "asin basic functionality" do
+    assert_respond_to(Math, :asin)
+    assert_nothing_raised{ Math.asin(1) }
+    assert_kind_of(Float, Math.asin(1))
+  end
+
+  test "asin with positive integer argument returns expected values" do
+    assert_nothing_raised{ Math.asin(1) }
+    assert_in_delta(1.57, Math.asin(1), 0.01)
+  end
+
+  test "asin with zero argument returns expected values" do
+    assert_nothing_raised{ Math.asin(0) }
+    assert_equal(0.0, Math.asin(0))
+  end
+
+  test "asin with negative integer argument returns expected values" do
+    assert_nothing_raised{ Math.asin(-1) }
+    assert_in_delta(-1.57, Math.asin(-1), 0.01)
+  end
+
+  test "asin with positive float argument returns expected values" do
+    assert_nothing_raised{ Math.asin(0.345) }
+    assert_in_delta(0.35, Math.asin(0.345), 0.01)
+  end
+
+  test "asin with negative float argument returns expected values" do
+    assert_nothing_raised{ Math.asin(-0.345) }
+    assert_in_delta(-0.35, Math.asin(-0.345), 0.01)
+  end
+
+  test "asin raises an error if the argument is invalid" do
+    assert_raises(Math::DomainError){ Math.asin(2) }
+    assert_raises(Math::DomainError){ Math.asin(-2) }
+  end
+
+  test "asin requires a numeric argument" do
+    assert_raises(TypeError){ Math.asin('test') }
+  end
+
+  test "asin requires one argument only" do
+    assert_raises(ArgumentError){ Math.asin }
+    assert_raises(ArgumentError){ Math.asin(1, 1) }
+  end
 end
