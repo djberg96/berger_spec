cb23b7c643db7963c1808c9b8d304fac00dc1656
Daniel Berger <djberg96@gmail.com>
2015-04-15 14:30:31 -0600

    Refactored and updated.

--- test/core/Math/singleton/test_log.rb
+++ test/core/Math/singleton/test_log.rb
@@ -1,42 +1,60 @@
 #####################################################################
-# tc_log.rb
+# test_log.rb
 #
 # Test cases for the Math.log method.
 #####################################################################
 require 'test/helper'
 require 'test/unit'
 
-class TC_Math_Log_Class < Test::Unit::TestCase
-   include Test::Helper
+class TC_Math_Log_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def test_log_basic
-      assert_respond_to(Math, :log)
-      assert_nothing_raised{ Math.log(1) }
-      assert_nothing_raised{ Math.log(100) }
-      assert_kind_of(Float, Math.log(1))
-   end
+  test "log basic functionality" do
+    assert_respond_to(Math, :log)
+    assert_nothing_raised{ Math.log(1) }
+    assert_nothing_raised{ Math.log(100) }
+    assert_kind_of(Float, Math.log(1))
+  end
 
-   def test_log_positive
-      assert_nothing_raised{ Math.log(1) }
-      assert_in_delta(0.0, Math.log(1), 0.01)
-   end
-   
-   def test_log_positive_float
-      assert_nothing_raised{ Math.log(0.345) }
-      assert_in_delta(-1.06, Math.log(0.345), 0.01)
-   end
-   
-   if OSX || JRUBY
-      def test_log_returns_infinity
-         assert_equal('-Infinity', Math.log(0).to_s)
-      end
-   end
-   
-   # TODO: Shouldn't all non-numerics raise TypeError?
-   def test_log_expected_errors
-      assert_raises(Errno::ERANGE){ Math.log(0) } unless OSX || JRUBY
-      assert_raises(Errno::EDOM){ Math.log(-1) }
-      assert_raises(TypeError){ Math.log(nil) }
-      assert_raises(ArgumentError){ Math.log('test') }
-   end
+  test "log with positive integer argument" do
+    assert_nothing_raised{ Math.log(1) }
+    assert_in_delta(0.0, Math.log(1), 0.01)
+  end
+
+  test "log with positive integer argument and base" do
+    assert_nothing_raised{ Math.log(12, 8) }
+    assert_in_delta(1.1949, Math.log(12, 8), 0.01)
+  end
+
+  test "log with positive float argument" do
+    assert_nothing_raised{ Math.log(0.345) }
+    assert_in_delta(-1.06, Math.log(0.345), 0.01)
+  end
+
+  test "log with positive float argument and base" do
+    assert_nothing_raised{ Math.log(12.0, 8) }
+    assert_in_delta(1.1949, Math.log(12.0, 8), 0.01)
+  end
+
+  test "log with zero argument returns infinity" do
+    omit_if(OSX || JRUBY)
+    assert_equal('-Infinity', Math.log(0).to_s)
+  end
+
+  test "invalid argument to log raises an error" do
+    omit_unless(OSX || JRUBY)
+    assert_raises(Errno::ERANGE){ Math.log(0) }
+    assert_raises(Errno::EDOM){ Math.log(-1) }
+  end
+
+  test "argument to log must be numeric" do
+    assert_raises(TypeError){ Math.log(nil) }
+    assert_raises(TypeError){ Math.log('test') }
+    assert_raises(TypeError){ Math.log(10, nil) }
+  end
+
+  test "log takes one or two arguments only" do
+    assert_raise(ArgumentError){ Math.log }
+    assert_raise(ArgumentError){ Math.log(1,1,1) }
+  end
 end
