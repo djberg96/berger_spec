576c98ce4ec93875569c626aff2f9f0d5f3b1256
Daniel Berger <djberg96@gmail.com>
2015-04-15 10:19:14 -0600

    Refactored and updated.

--- test/core/Math/singleton/test_constants.rb
+++ test/core/Math/singleton/test_constants.rb
@@ -1,19 +1,22 @@
 #####################################################################
-# tc_constants.rb
+# test_constants.rb
 #
 # Test cases for constants in the Math module.
 #####################################################################
 require 'test/helper'
 require 'test/unit'
 
-class TC_Math_Constants_Class < Test::Unit::TestCase
-   def test_e
-      assert_not_nil(Math::E)
-      assert_kind_of(Float, Math::E)
-   end
+class TC_Math_Constants < Test::Unit::TestCase
+  # Euler's constant
+  test "Math::E is defined" do
+    assert_not_nil(Math::E)
+    assert_kind_of(Float, Math::E)
+    assert_in_delta(2.71828, Math::E, 0.001)
+  end
 
-   def test_pi
-      assert_not_nil(Math::PI)
-      assert_in_delta(3.14, Math::PI, 0.01)
-   end
+  test "Math::PI is defined" do
+    assert_not_nil(Math::PI)
+    assert_kind_of(Float, Math::PI)
+    assert_in_delta(3.14, Math::PI, 0.01)
+  end
 end
