5ed491296268cf03278c3e7a914138ce7737b216
Daniel Berger <djberg96@gmail.com>
2015-04-15 14:40:13 -0600

    Refactored and updated.

--- test/core/Math/singleton/test_log10.rb
+++ test/core/Math/singleton/test_log10.rb
@@ -1,42 +1,49 @@
 #####################################################################
-# tc_log10.rb
+# test_log10.rb
 #
 # Test cases for the Math.log10 method.
 #####################################################################
 require 'test/helper'
 require 'test/unit'
 
-class TC_Math_Log10_Class < Test::Unit::TestCase
-   include Test::Helper
-   
-   def test_log10_basic
-      assert_respond_to(Math, :log10)
-      assert_nothing_raised{ Math.log10(1) }
-      assert_nothing_raised{ Math.log10(100) }
-      assert_kind_of(Float, Math.log10(1))
-   end
+class TC_Math_Log10_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def test_log10_positive
-      assert_nothing_raised{ Math.log10(1) }
-      assert_in_delta(0.0, Math.log10(1), 0.01)
-   end
-   
-   def test_log10_positive_float
-      assert_nothing_raised{ Math.log10(0.345) }
-      assert_in_delta(-0.46, Math.log10(0.345), 0.01)
-   end
-   
-   if OSX || JRUBY
-      def test_log10_returns_infinity
-         assert_equal('-Infinity', Math.log10(0).to_s)
-      end
-   end
-   
-   # TODO: Shouldn't all non-numerics raise TypeError?
-   def test_log10_expected_errors
-      assert_raises(Errno::ERANGE){ Math.log10(0) } unless OSX || JRUBY
-      assert_raises(Errno::EDOM){ Math.log10(-1) }
-      assert_raises(TypeError){ Math.log10(nil) }
-      assert_raises(ArgumentError){ Math.log10('test') }
-   end
+  test "log10 basic functionality" do
+    assert_respond_to(Math, :log10)
+    assert_nothing_raised{ Math.log10(1) }
+    assert_nothing_raised{ Math.log10(100) }
+    assert_kind_of(Float, Math.log10(1))
+  end
+
+  test "log10 with positive integer argument" do
+    assert_nothing_raised{ Math.log10(1) }
+    assert_in_delta(0.0, Math.log10(1), 0.01)
+  end
+
+  test "log10 with positive float argument" do
+    assert_nothing_raised{ Math.log10(0.345) }
+    assert_in_delta(-0.46, Math.log10(0.345), 0.01)
+  end
+
+  test "log10 returns Infinity" do
+    omit_unless(OSX || JRUBY)
+    assert_equal('-Infinity', Math.log10(0).to_s)
+  end
+
+  test "log10 with invalid argument raises an error" do
+    omit_unless(OSX || JRUBY)
+    assert_raises(Errno::ERANGE){ Math.log10(0) }
+    assert_raises(Errno::EDOM){ Math.log10(-1) }
+  end
+
+  test "argument to log10 must be numeric" do
+    assert_raises(TypeError){ Math.log10(nil) }
+    assert_raises(TypeError){ Math.log10('test') }
+  end
+
+  test "log10 accepts one argument only" do
+    assert_raise(ArgumentError){ Math.log10 }
+    assert_raise(ArgumentError){ Math.log10(1,1) }
+  end
 end
