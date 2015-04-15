909e739910c52baf40f987445c147bb7b1d05f25
Daniel Berger <djberg96@gmail.com>
2015-04-15 15:14:14 -0600

    Refactored and updated.

--- test/core/Math/singleton/test_sqrt.rb
+++ test/core/Math/singleton/test_sqrt.rb
@@ -1,38 +1,45 @@
 #####################################################################
-# tc_sqrt.rb
+# test_sqrt.rb
 #
 # Test cases for the Math.sqrt method.
 #####################################################################
 require 'test/helper'
 require 'test/unit'
 
-class TC_Math_Sqrt_Class < Test::Unit::TestCase
-   def test_sqrt_basic
-      assert_respond_to(Math, :sqrt)
-      assert_nothing_raised{ Math.sqrt(1) }
-      assert_nothing_raised{ Math.sqrt(100) }
-      assert_kind_of(Float, Math.sqrt(1))
-   end
+class TC_Math_Sqrt_SingletonMethod < Test::Unit::TestCase
+  test "sqrt basic functionality" do
+    assert_respond_to(Math, :sqrt)
+    assert_nothing_raised{ Math.sqrt(1) }
+    assert_nothing_raised{ Math.sqrt(100) }
+    assert_kind_of(Float, Math.sqrt(1))
+  end
 
-   def test_sqrt_positive
-      assert_nothing_raised{ Math.sqrt(1) }
-      assert_in_delta(1.0, Math.sqrt(1), 0.01)
-   end
+  test "sqrt with positive integer" do
+    assert_nothing_raised{ Math.sqrt(1) }
+    assert_in_delta(1.0, Math.sqrt(1), 0.01)
+  end
 
-   def test_sqrt_zero
-      assert_nothing_raised{ Math.sqrt(0) }
-      assert_in_delta(0.0, Math.sqrt(0), 0.01)
-   end
-   
-   def test_sqrt_positive_float
-      assert_nothing_raised{ Math.sqrt(0.345) }
-      assert_in_delta(0.58, Math.sqrt(0.345), 0.01)
-   end
-   
-   # TODO: Shouldn't all non-numerics raise TypeError?
-   def test_sqrt_expected_errors
-      assert_raises(Errno::EDOM){ Math.sqrt(-1) }
-      assert_raises(TypeError){ Math.sqrt(nil) }
-      assert_raises(ArgumentError){ Math.sqrt('test') }
-   end
+  test "sqrt with zero argument" do
+    assert_nothing_raised{ Math.sqrt(0) }
+    assert_in_delta(0.0, Math.sqrt(0), 0.01)
+  end
+
+  test "sqrt with positive float argument" do
+    assert_nothing_raised{ Math.sqrt(0.345) }
+    assert_in_delta(0.58, Math.sqrt(0.345), 0.01)
+  end
+
+  test "sqrt with invalid argument raises an error" do
+    assert_raises(Math::DomainError){ Math.sqrt(-1) }
+  end
+
+  test "sqrt requires a numeric argument" do
+    assert_raises(TypeError){ Math.sqrt(nil) }
+    assert_raises(TypeError){ Math.sqrt('test') }
+  end
+
+  test "sqrt takes a single argument only" do
+    assert_raise(ArgumentError){ Math.sqrt }
+    assert_raise(ArgumentError){ Math.sqrt(4,4) }
+  end
 end
