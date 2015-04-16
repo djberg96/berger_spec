3ec90e6dea7f78e00ad9ae069570a7eb7f201a14
Daniel Berger <djberg96@gmail.com>
2015-04-16 17:08:52 -0600

    Updated.

--- test/core/Hash/instance/test_select.rb
+++ test/core/Hash/instance/test_select.rb
@@ -4,34 +4,30 @@
 # Test suite for the Hash#select instance method.
 ###########################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
 class TC_Hash_Select_InstanceMethod < Test::Unit::TestCase
   include Test::Helper
 
   def setup
-    @hash = {"a", 1, "b", 2, "c", 3}
+    @hash = Hash["a", 1, "b", 2, "c", 3]
   end
 
   test "select basic functionality" do
     assert_respond_to(@hash, :select)
     assert_nothing_raised{ @hash.select{} }
-    assert_kind_of(Array, @hash.select{})
+    assert_kind_of(Hash, @hash.select{})
   end
 
   test "select returns expected results" do
-    assert_equal([["a",1],["b",2]], @hash.select{ |k,v| v <=2 })
-    assert_equal([["a",1],["b",2],["c",3]], @hash.select{ true })
-    assert_equal([], @hash.select{ false })
-    assert_equal([], @hash.select{ |k,v| v > 99 })
+    assert_equal(Hash["a",1,"b",2], @hash.select{ |k,v| v <=2 })
+    assert_equal(Hash["a",1,"b",2,"c",3], @hash.select{ true })
+    assert_equal({}, @hash.select{ false })
+    assert_equal({}, @hash.select{ |k,v| v > 99 })
   end
 
   test "select behaves as expected if no block is provided" do
-    if PRE187
-      assert_raise(LocalJumpError){ @hash.select }
-    else
-      assert_kind_of(Enumerable::Enumerator, @hash.select)
-    end
+    assert_kind_of(Enumerator, @hash.select)
   end
 
   test "select does not accept any arguments" do
