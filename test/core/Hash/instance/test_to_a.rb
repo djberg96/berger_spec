8c7b9a4b1e393653345568f3cc0e4307c915a39a
Daniel Berger <djberg96@gmail.com>
2015-04-16 17:12:47 -0600

    Updated.

--- test/core/Hash/instance/test_to_a.rb
+++ test/core/Hash/instance/test_to_a.rb
@@ -4,11 +4,11 @@
 # Test suite for the Hash#to_a instance method.
 ############################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
 class TC_Hash_ToA_InstanceMethod < Test::Unit::TestCase
   def setup
-    @hash = {"a",1,"b",2,"c",3}
+    @hash = Hash["a",1,"b",2,"c",3]
   end
 
   test "to_a basic functionality" do
@@ -19,17 +19,17 @@ class TC_Hash_ToA_InstanceMethod < Test::Unit::TestCase
 
   test "to_a returns expected results" do
     assert_equal([["a",1],["b",2],["c",3]], @hash.to_a)
-    assert_equal([['a', [1,2]], ['b', [3,4]]], {'a', [1,2], 'b', [3,4]}.to_a)
+    assert_equal([['a', [1,2]], ['b', [3,4]]], Hash['a', [1,2], 'b', [3,4]].to_a)
     assert_equal([], {}.to_a)
   end
 
   test "a hash with identical keys returns the expected result" do
-    assert_equal([['a', 3]], {'a', 1, 'a', 2, 'a', 3}.to_a)
+    assert_equal([['a', 3]], Hash['a', 1, 'a', 2, 'a', 3].to_a)
   end
 
   test "a hash of nil or false keys and values returns the expected result" do
-    assert_equal([[nil, nil]], {nil, nil, nil, nil}.to_a)
-    assert_equal([[false, false]], {false, false, false, false}.to_a)
+    assert_equal([[nil, nil]], Hash[nil, nil, nil, nil].to_a)
+    assert_equal([[false, false]], Hash[false, false, false, false].to_a)
   end
 
   test "to_a does not accept any arguments" do
