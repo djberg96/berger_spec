1e9ee00609ba8fb96b1e733fb625707212d33ebc
Daniel Berger <djberg96@gmail.com>
2015-04-18 08:08:10 -0600

    Updated and refactored.

--- test/core/IO/instance/test_pos.rb
+++ test/core/IO/instance/test_pos.rb
@@ -4,74 +4,53 @@
 # Test case for the IO#pos instance method, as well as the IO#tell alias.
 ###############################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
 class TC_IO_Pos_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @file   = 'test_pos.rb'
-      @handle = File.new(@file, 'wb+')
-      @handle.print("hello\n")
-      @handle.rewind
-   end
+  include Test::Helper
 
-   def test_pos_basic
-      assert_respond_to(@handle, :pos)
-      assert_nothing_raised{ @handle.pos }
-      assert_kind_of(Fixnum, @handle.pos)
-   end
+  def setup
+    @file   = 'test_pos.rb'
+    @handle = File.new(@file, 'wb+')
+    @handle.print("hello\n")
+    @handle.rewind
+  end
 
-   def test_tell_alias_basic
-      assert_respond_to(@handle, :tell)
-      assert_nothing_raised{ @handle.tell }
-      assert_kind_of(Fixnum, @handle.tell)
-   end
+  test "pos basic functionality" do
+    assert_respond_to(@handle, :pos)
+    assert_nothing_raised{ @handle.pos }
+    assert_kind_of(Numeric, @handle.pos)
+  end
 
-   def test_pos
-      assert_equal(0, @handle.pos)
-      assert_nothing_raised{ @handle.getc }
-      assert_equal(1, @handle.pos)
-      assert_nothing_raised{ @handle.gets }
-      assert_equal(6, @handle.pos)
-      assert_nothing_raised{ @handle.gets } # Try again...
-      assert_equal(6, @handle.pos)          # And make sure it didn't move
-   end
+  test "tell is an alias for pos" do
+    assert_alias_method(@handle, :pos, :tell)
+  end
 
-   def test_tell_alias
-      assert_equal(0, @handle.tell)
-      assert_nothing_raised{ @handle.getc }
-      assert_equal(1, @handle.tell)
-      assert_nothing_raised{ @handle.gets }
-      assert_equal(6, @handle.tell)
-      assert_nothing_raised{ @handle.gets } # Try again...
-      assert_equal(6, @handle.tell)          # And make sure it didn't move
-   end
+  test "pos returns the expected results" do
+    assert_equal(0, @handle.pos)
+    assert_nothing_raised{ @handle.getc }
+    assert_equal(1, @handle.pos)
+    assert_nothing_raised{ @handle.gets }
+    assert_equal(6, @handle.pos)
+    assert_nothing_raised{ @handle.gets } # Try again...
+    assert_equal(6, @handle.pos)          # And make sure it didn't move
+  end
 
-   def test_pos_after_rewind
-      assert_nothing_raised{ @handle.getc }
-      assert_equal(1, @handle.pos)
-      assert_nothing_raised{ @handle.rewind }
-      assert_equal(0, @handle.pos)
-   end
+  test "pos returns expected result after rewind" do
+    assert_nothing_raised{ @handle.getc }
+    assert_equal(1, @handle.pos)
+    assert_nothing_raised{ @handle.rewind }
+    assert_equal(0, @handle.pos)
+  end
 
-   def test_tell_alias_after_rewind
-      assert_nothing_raised{ @handle.getc }
-      assert_equal(1, @handle.tell)
-      assert_nothing_raised{ @handle.rewind }
-      assert_equal(0, @handle.tell)
-   end
+  test "pos does not accept any arguments" do
+    assert_raise(ArgumentError){ @handle.pos(3) }
+  end
 
-   def test_pos_expected_errors
-      assert_raise(ArgumentError){ @handle.pos(3) }
-   end
-
-   def test_tell_alias_expected_errors
-      assert_raise(ArgumentError){ @handle.tell(3) }
-   end
-
-   def teardown
-      @handle.close if @handle && !@handle.closed?
-      File.delete(@file) if File.exists?(@file)
-      @handle = nil
-      @file   = nil
-   end
+  def teardown
+    @handle.close if @handle && !@handle.closed?
+    File.delete(@file) if File.exist?(@file)
+    @handle = nil
+    @file   = nil
+  end
 end
