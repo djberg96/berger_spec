817d0211efa58cac65d9fc3ed10ac58dc6a2d96f
Daniel Berger <djberg96@gmail.com>
2015-04-18 13:41:14 -0600

    Updated.

--- test/core/IO/singleton/test_pipe.rb
+++ test/core/IO/singleton/test_pipe.rb
@@ -1,43 +1,58 @@
 ######################################################################
 # test_pipe.rb
 #
-# Test case for the IO.pipe class method. These tests are skipped
-# on MS Windows (via the Rakefile).
+# Test case for the IO.pipe class method.
 ######################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
 class TC_IO_Pipe_SingletonMethod < Test::Unit::TestCase
-   def setup
-      @read  = nil
-      @write = nil
-   end
+  def setup
+    @read  = nil
+    @write = nil
+  end
 
-   def test_pipe_basic
-      assert_respond_to(IO, :pipe)
-      assert_kind_of(Array, IO.pipe)
-   end
+  test "pipe basic functionality" do
+    assert_respond_to(IO, :pipe)
+    assert_kind_of(Array, IO.pipe)
+  end
 
-   def test_pipe
-      assert_nothing_raised{ @read, @write = IO.pipe }
-      assert_kind_of(IO, @read)
-      assert_kind_of(IO, @write)
-   end
+  test "pipe returns two IO objects" do
+    assert_nothing_raised{ @read, @write = IO.pipe }
+    assert_kind_of(IO, @read)
+    assert_kind_of(IO, @write)
+  end
 
-   def test_pipe_read_and_write
-      assert_nothing_raised{ @read, @write = IO.pipe }
-      assert_nothing_raised{ @write.print "hello" }
-      assert_nothing_raised{ @write.close }
-      assert_equal("hello", @read.read)
-      assert_nothing_raised{ @read.close }
-   end
+  test "objects returned by pipe behave as expected" do
+    assert_nothing_raised{ @read, @write = IO.pipe }
+    assert_nothing_raised{ @write.print "hello" }
+    assert_nothing_raised{ @write.close }
+    assert_equal("hello", @read.read)
+    assert_nothing_raised{ @read.close }
+  end
 
-   def test_pipe_expected_errors
-      assert_raise(ArgumentError){ IO.pipe(2) }
-   end
+  test "pipe method accepts a block" do
+    assert_nothing_raised{ IO.pipe{ } }
+    rv = IO.pipe{ |r,w|
+      assert_kind_of(IO, r)
+      assert_kind_of(IO, w)
+    }
+    assert_nil(rv)
+  end
 
-   def teardown
-      @read.close if @read && !@read.closed?
-      @write.close if @write && !@write.closed?
-   end
+  test "pipe method takes an optional encoding argument" do
+    current = Encoding.default_external
+    assert_nothing_raised{ IO.pipe('ASCII') }
+    assert_nothing_raised{ IO.pipe("#{current}:UTF-16LE") }
+    assert_nothing_raised{ IO.pipe('ASCII'){ } }
+  end
+
+  #test "an error is raised if an invalid encoding is passed" do
+  #  assert_raise(ArgumentError){ IO.pipe('bogus') }
+  #end
+
+  def teardown
+    @read.close if @read && !@read.closed?
+    @write.close if @write && !@write.closed?
+  end
 end
