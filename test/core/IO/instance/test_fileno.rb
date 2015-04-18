3d0994dd495db31a64dff3f974c411fa98a61721
Daniel Berger <djberg96@gmail.com>
2015-04-18 15:38:14 -0600

    Updated and refactored for JRuby.

--- test/core/IO/instance/test_fileno.rb
+++ test/core/IO/instance/test_fileno.rb
@@ -3,60 +3,47 @@
 #
 # Test case for the IO#fileno instance method and the IO#to_i alias.
 ######################################################################
-require 'test/unit'
+require 'test-unit'
 require 'test/helper'
 
 class TC_IO_Fileno_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @file   = 'fileno_test.txt'
-      @handle = File.new(@file, 'w')
-   end
+  def setup
+    @file   = 'fileno_test.txt'
+    @handle = File.new(@file, 'w')
+  end
 
-   def test_fileno
-      assert_respond_to(@handle, :fileno)
-      assert_kind_of(Fixnum, @handle.fileno)
-      assert_equal(true, @handle.fileno > 2)
-   end
+  test "fileno basic functionality" do
+    assert_respond_to(@handle, :fileno)
+    assert_kind_of(Fixnum, @handle.fileno)
+    assert_true(@handle.fileno > 2)
+  end
 
-   def test_to_i_alias
-      assert_respond_to(@handle, :fileno)
-      assert_kind_of(Fixnum, @handle.fileno)
-      assert_equal(true, @handle.fileno > 2)
-   end
+  test "to_i is an alias for fileno" do
+    assert_alias_method(@handle, :fileno, :to_i)
+  end
 
-   def test_fileno_special_handles
-      assert_respond_to(STDIN, :fileno)
-      assert_respond_to(STDOUT, :fileno)
-      assert_respond_to(STDERR, :fileno)
+  test "special handles respond to fileno method" do
+    assert_respond_to(STDIN, :fileno)
+    assert_respond_to(STDOUT, :fileno)
+    assert_respond_to(STDERR, :fileno)
+  end
 
-      assert(STDIN.fileno == 0)
-      assert(STDOUT.fileno == 1)
-      assert(STDERR.fileno == 2)
-   end
+  test "special handles return expected fileno values" do
+    omit_if(JRUBY)
+    assert(STDIN.fileno == 0)
+    assert(STDOUT.fileno == 1)
+    assert(STDERR.fileno == 2)
+  end
 
-   def test_to_i_alias_special_handles
-      assert_respond_to(STDIN, :to_i)
-      assert_respond_to(STDOUT, :to_i)
-      assert_respond_to(STDERR, :to_i)
+  test "fileno does not take any arguments" do
+    assert_raise(ArgumentError){ @handle.fileno(4) }
+  end
 
-      assert(STDIN.to_i == 0)
-      assert(STDOUT.to_i == 1)
-      assert(STDERR.to_i == 2)
-   end
-
-   def test_fileno_expected_errors
-      assert_raise(ArgumentError){ @handle.fileno(4) }
-   end
-
-   def test_to_i_alias_expected_errors
-      assert_raise(ArgumentError){ @handle.fileno(4) }
-   end
-
-   def teardown
-      @handle.close if @handle && !@handle.closed?
-      @handle = nil
-      remove_file(@file)
-   end
+  def teardown
+    @handle.close if @handle && !@handle.closed?
+    @handle = nil
+    remove_file(@file)
+  end
 end
