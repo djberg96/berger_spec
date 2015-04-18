863f2ff4bbad6e7bd0f979b86b16718e99d78981
Daniel Berger <djberg96@gmail.com>
2015-04-18 11:56:13 -0600

    Updated.

--- test/core/IO/instance/test_getc.rb
+++ test/core/IO/instance/test_getc.rb
@@ -7,43 +7,42 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_IO_Getc_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @file   = 'test_getc.txt'
-      @handle = File.new(@file, 'wb+')
-      @handle.print "wo\tr\nld5"
-      @handle.rewind
-   end
+  def setup
+    @file   = 'test_getc.txt'
+    @handle = File.new(@file, 'wb+')
+    @handle.print "wo\tr\nld5"
+    @handle.rewind
+  end
 
-   def test_getc_basic
-      assert_respond_to(@handle, :getc)
-      assert_nothing_raised{ @handle.getc }
-      assert_kind_of(Fixnum, @handle.getc)
-   end
+  test "getc basic functionality" do
+    assert_respond_to(@handle, :getc)
+    assert_nothing_raised{ @handle.getc }
+    assert_kind_of(String, @handle.getc)
+  end
 
-   # We'll call it once for each character, plus two more to verify
-   # that it returns nil at eof.
-   #
-   def test_getc
-      assert_equal(119, @handle.getc)
-      assert_equal(111, @handle.getc)
-      assert_equal(9, @handle.getc)
-      assert_equal(114, @handle.getc)
-      assert_equal(10, @handle.getc)
-      assert_equal(108, @handle.getc)
-      assert_equal(100, @handle.getc)
-      assert_equal(53, @handle.getc)
-      assert_equal(nil, @handle.getc)
-      assert_equal(nil, @handle.getc)
-   end
+  # We'll call it once for each character, plus two more to verify
+  # that it returns nil at eof.
 
-   def test_getc_expected_errors
-      assert_raise(ArgumentError){ @handle.getc(1) }
-   end
+  test "getc returns expected results" do
+    assert_equal('w', @handle.getc)
+    assert_equal('o', @handle.getc)
+    assert_equal("\t", @handle.getc)
+    assert_equal('r', @handle.getc)
+    assert_equal("\n", @handle.getc)
+    assert_equal('l', @handle.getc)
+    assert_equal('d', @handle.getc)
+    assert_equal('5', @handle.getc)
+    assert_equal(nil, @handle.getc)
+  end
 
-   def teardown
-      @handle.close if @handle && !@handle.closed?
-      File.delete(@file) if File.exists?(@file)
-      @file   = nil
-      @handle = nil
-   end
+  test "getc does not take any arguments" do
+    assert_raise(ArgumentError){ @handle.getc(1) }
+  end
+
+  def teardown
+    @handle.close if @handle && !@handle.closed?
+    File.delete(@file) if File.exist?(@file)
+    @file   = nil
+    @handle = nil
+  end
 end
