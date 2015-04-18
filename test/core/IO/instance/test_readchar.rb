5293d3bec7b5387a6b3430f5cd4ca880f6a97af9
Daniel Berger <djberg96@gmail.com>
2015-04-18 08:48:50 -0600

    Updated for Ruby 2.2.

--- test/core/IO/instance/test_readchar.rb
+++ test/core/IO/instance/test_readchar.rb
@@ -8,42 +8,45 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_IO_Readchar_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @file   = 'test_readchar.txt'
-      @handle = File.new(@file, 'wb+')
-      @handle.print "wo\tr\nld5"
-      @handle.rewind
-   end
+  def setup
+    @file   = 'test_readchar.txt'
+    @handle = File.new(@file, 'wb+')
+    @handle.print "wo\tr\nld5"
+    @handle.rewind
+  end
 
-   def test_readchar_basic
-      assert_respond_to(@handle, :readchar)
-      assert_nothing_raised{ @handle.readchar }
-      assert_kind_of(Fixnum, @handle.readchar)
-   end
+  test "readchar basic functionality" do
+    assert_respond_to(@handle, :readchar)
+    assert_nothing_raised{ @handle.readchar }
+    assert_kind_of(String, @handle.readchar)
+  end
 
-   # We'll call it once for each character
-   def test_readchar
-      assert_equal(119, @handle.readchar)
-      assert_equal(111, @handle.readchar)
-      assert_equal(9, @handle.readchar)
-      assert_equal(114, @handle.readchar)
-      assert_equal(10, @handle.readchar)
-      assert_equal(108, @handle.readchar)
-      assert_equal(100, @handle.readchar)
-      assert_equal(53, @handle.readchar)
-   end
+  # We'll call it once for each character
+  test "readchar returns expected result" do
+    assert_equal('w', @handle.readchar)
+    assert_equal('o', @handle.readchar)
+    assert_equal("\t", @handle.readchar)
+    assert_equal('r', @handle.readchar)
+    assert_equal("\n", @handle.readchar)
+    assert_equal('l', @handle.readchar)
+    assert_equal('d', @handle.readchar)
+    assert_equal('5', @handle.readchar)
+  end
 
-   def test_readchar_expected_errors
-      assert_raise(ArgumentError){ @handle.readchar(1) }
-      assert_raise(EOFError){ 10.times{ @handle.readchar } }
-   end
+  test "readchar does not take any arguments" do
+    assert_raise(ArgumentError){ @handle.readchar(1) }
+  end
 
-   def teardown
-      @handle.close if @handle && !@handle.closed?
-      File.delete(@file) if File.exists?(@file)
-      @file   = nil
-      @handle = nil
-   end
+  test "readchar raises an error if you read past the end of file" do
+    assert_raise(EOFError){ 10.times{ @handle.readchar } }
+  end
+
+  def teardown
+    @handle.close if @handle && !@handle.closed?
+    File.delete(@file) if File.exist?(@file)
+    @file   = nil
+    @handle = nil
+  end
 end
