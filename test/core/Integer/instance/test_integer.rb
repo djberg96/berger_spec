93b8d73ce07289adce4269928f079da4dc984bd5
Daniel Berger <djberg96@gmail.com>
2015-04-19 11:25:23 -0600

    Updated.

--- test/core/Integer/instance/test_integer.rb
+++ test/core/Integer/instance/test_integer.rb
@@ -1,5 +1,5 @@
 #####################################################################
-# tc_integer?.rb
+# test_is_integer.rb
 #
 # Test case for the Integer#integer? method.
 #####################################################################
@@ -7,26 +7,31 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Integer_IsInteger_Instance < Test::Unit::TestCase
-   def setup
-      @int1 = 65
-   end
+  def setup
+    @int1 = 65
+  end
 
-   def test_integer_basic
-      assert_respond_to(@int1, :integer?)
-   end
+  test "integer? basic functionality" do
+    assert_respond_to(@int1, :integer?)
+    assert_nothing_raised{ @int1.integer? }
+  end
 
-   def test_integer
-      assert_nothing_raised{ @int1.integer? }
-      assert(@int1.integer?)
-      assert(-1.integer?)
-      assert(0.integer?)
-   end
+  test "integer? returns true for integer objects" do
+    assert_true(@int1.integer?)
+    assert_true(-1.integer?)
+    assert_true(0.integer?)
+  end
 
-   def test_integer_expected_errors
-      assert_raises(ArgumentError){ @int1.integer?(2) }
-   end
+  test "integer? returns false for non-integer objects" do
+    assert_false(-1.5.integer?)
+    assert_false(0.0.integer?)
+  end
 
-   def teardown
-      @int1 = nil
-   end
+  test "integer? does not accept any arguments" do
+    assert_raises(ArgumentError){ @int1.integer?(2) }
+  end
+
+  def teardown
+    @int1 = nil
+  end
 end
