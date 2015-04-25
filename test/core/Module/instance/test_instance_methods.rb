b3eff7af7cb794f92d631a04779fde33e97f2ab2
Daniel Berger <djberg96@gmail.com>
2015-04-24 23:04:54 -0600

    Refactored and updated.

--- test/core/Module/instance/test_instance_methods.rb
+++ test/core/Module/instance/test_instance_methods.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_instance_methods.rb
+# test_instance_methods.rb
 #
 # Test case for the Module#instance_methods instance method.
 ########################################################################
@@ -7,37 +7,37 @@ require 'test/helper'
 require 'test/unit'
 
 module InstMethA
-   def method_a
-   end
+  def method_a
+  end
 end
 
 class InstMethB
-   include InstMethA
+  include InstMethA
 end
 
 class InstMethC < InstMethB
 end
 
 class TC_Module_InstanceMethods_InstanceMethod < Test::Unit::TestCase
-   def test_instance_methods_basic
-      assert_respond_to(InstMethA, :instance_methods)
-      assert_nothing_raised{ InstMethA.instance_methods }
-      assert_kind_of(Array, InstMethA.instance_methods)
-   end
+  test "instance_methods basic functionality" do
+    assert_respond_to(InstMethA, :instance_methods)
+    assert_nothing_raised{ InstMethA.instance_methods }
+    assert_kind_of(Array, InstMethA.instance_methods)
+  end
 
-   def test_instance_methods
-      assert_equal(['method_a'], InstMethA.instance_methods)
-      assert_equal(true, InstMethB.instance_methods.include?('method_a'))
-      assert_equal(true, InstMethC.instance_methods.include?('method_a'))
-   end
+  test "instance_methods returns the expected results" do
+    assert_equal([:method_a], InstMethA.instance_methods)
+    assert_true(InstMethB.instance_methods.include?(:method_a))
+    assert_true(InstMethC.instance_methods.include?(:method_a))
+  end
 
-   def test_instance_methods_with_super
-      assert_equal(['method_a'], InstMethA.instance_methods(false))
-      assert_equal([], InstMethB.instance_methods(false))
-      assert_equal([], InstMethC.instance_methods(false))
-   end
+  test "instance_methods with false super argument returns expected result" do
+    assert_equal([:method_a], InstMethA.instance_methods(false))
+    assert_equal([], InstMethB.instance_methods(false))
+    assert_equal([], InstMethC.instance_methods(false))
+  end
 
-   def test_instance_methods_expected_errors
-      assert_raise(ArgumentError){ InstMethA.instance_methods(true, false) }
-   end
+  test "instance_methods accepts a maximum of one argument" do
+    assert_raise(ArgumentError){ InstMethA.instance_methods(true, false) }
+  end
 end
