db0e47f88c22a5a08a8f834d8564d4b5b736616e
Daniel Berger <djberg96@gmail.com>
2015-04-24 22:12:05 -0600

    Replaced single array with individual tests.

--- test/core/Kernel/singleton/test_global_variables.rb
+++ test/core/Kernel/singleton/test_global_variables.rb
@@ -1,35 +1,70 @@
 ########################################################################
-# tc_global_variables.rb
+# test_global_variables.rb
 #
 # Test case for the Kernel.global_variables module method.
 ########################################################################
 require 'test/unit'
 
 class TC_Kerel_GlobalVariables_ModuleMethod < Test::Unit::TestCase
-   def test_global_variables_basic
-      assert_respond_to(Kernel, :global_variables)
-      assert_nothing_raised{ Kernel.global_variables }
-      assert_kind_of(Array, Kernel.global_variables)
-   end
+  test "global_variables basic functionality" do
+    assert_respond_to(Kernel, :global_variables)
+    assert_nothing_raised{ Kernel.global_variables }
+    assert_kind_of(Array, Kernel.global_variables)
+  end
 
-   # Because rake, etc, can add their own global variables, we'll just
-   # ensure that this core group is included.
-   def test_global_variables
-      expected = [
-         "$!", "$\"", "$$", "$&", "$'", "$*", "$+", "$,", "$-0", "$-F", "$-I",
-         "$-K", "$-a", "$-d", "$-i", "$-l", "$-p", "$-v", "$-w", "$.", "$/",
-         "$0", "$:", "$;", "$<", "$=", "$>", "$?", "$@", "$DEBUG",
-         "$FILENAME", "$KCODE", "$LOADED_FEATURES", "$LOAD_PATH",
-         "$PROGRAM_NAME", "$SAFE", "$VERBOSE", "$\\", "$_", "$`", "$deferr",
-         "$defout", "$stderr", "$stdin", "$stdout", "$~"
-      ]
+  test '$! is included in global_variables' do
+    assert_true(Kernel.global_variables.include?(:$!))
+  end
 
-      expected.each{ |var|
-         assert_equal(true, Kernel.global_variables.include?(var))
-      }
-   end
+  test '$" is included in global_variables' do
+    assert_true(Kernel.global_variables.include?(:$"))
+  end
 
-   def test_global_variables_expected_errors
-      assert_raise(ArgumentError){ Kernel.global_variables(true) }
-   end
+  test '$$ is included in global_variables' do
+    assert_true(Kernel.global_variables.include?(:$$))
+  end
+
+  test '$& is included in global_variables' do
+    assert_true(Kernel.global_variables.include?(:$&))
+  end
+
+  test "$' is included in global_variables" do
+    assert_true(Kernel.global_variables.include?(:$'))
+  end
+
+  test "$* is included in global_variables" do
+    assert_true(Kernel.global_variables.include?(:$*))
+  end
+
+  test "$+ is included in global_variables" do
+    assert_true(Kernel.global_variables.include?(:$+))
+  end
+
+  test "$, is included in global_variables" do
+    assert_true(Kernel.global_variables.include?(:$,))
+  end
+
+  test "$-0 is included in global_variables" do
+    assert_true(Kernel.global_variables.include?(:$-0))
+  end
+
+  test "$-F is included in global_variables" do
+    assert_true(Kernel.global_variables.include?(:$-F))
+  end
+
+  test "$-I is included in global_variables" do
+    assert_true(Kernel.global_variables.include?(:$-I))
+  end
+
+  test "$LOAD_PATH is included in global_variables" do
+    assert_true(Kernel.global_variables.include?(:$LOAD_PATH))
+  end
+
+  test "$SAFE is included in global_variables" do
+    assert_true(Kernel.global_variables.include?(:$SAFE))
+  end
+
+  test "the global_variables method does not take an argument" do
+    assert_raise(ArgumentError){ Kernel.global_variables(true) }
+  end
 end
