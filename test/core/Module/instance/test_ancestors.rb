eeda3780f6d504d62c944049e5d0a57efd386260
Daniel Berger <djberg96@gmail.com>
2015-04-24 22:19:17 -0600

    Refactored.

--- test/core/Module/instance/test_ancestors.rb
+++ test/core/Module/instance/test_ancestors.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_ancestors.rb
+# test_ancestors.rb
 #
 # Test case for the Module#ancestors instance method.
 ########################################################################
@@ -9,22 +9,25 @@ require 'test/unit'
 module AncModA; end
 
 class AncFoo
-   module AncModB; end
-
-   include AncModA
-   include AncModB
+  module AncModB; end
+  include AncModA
+  include AncModB
 end
 
 class TC_Module_Ancestors_InstanceMethod < Test::Unit::TestCase
-   def test_ancestors_basic
-      assert_respond_to(AncFoo, :ancestors)
-      assert_respond_to(AncModA, :ancestors)
-      assert_respond_to(AncFoo::AncModB, :ancestors)
-   end
+  test "ancestors basic functionality" do
+    assert_respond_to(AncFoo, :ancestors)
+    assert_respond_to(AncModA, :ancestors)
+    assert_respond_to(AncFoo::AncModB, :ancestors)
+  end
 
-   def test_ancestors
-      assert_equal([AncFoo, AncFoo::AncModB, AncModA], AncFoo.ancestors[0..2])
-      assert_equal([AncFoo::AncModB], AncFoo::AncModB.ancestors)
-      assert_equal([AncModA], AncModA.ancestors)
-   end
+  test "ancestors returns expected results" do
+    assert_equal([AncFoo, AncFoo::AncModB, AncModA], AncFoo.ancestors[0..2])
+    assert_equal([AncFoo::AncModB], AncFoo::AncModB.ancestors)
+    assert_equal([AncModA], AncModA.ancestors)
+  end
+
+  test "ancestors method does not accept any arguments" do
+    assert_raise(ArgumentError){ AncFoo.ancestors('test') }
+  end
 end
