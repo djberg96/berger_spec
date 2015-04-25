fe8e345dcf2403678c81556ae6935b9aa1b9fd50
Daniel Berger <djberg96@gmail.com>
2015-04-24 22:58:11 -0600

    Refactored and updated.

--- test/core/Module/instance/test_constants.rb
+++ test/core/Module/instance/test_constants.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_constants.rb
+# test_constants.rb
 #
 # Test suite for the Module#constants instance method.
 ########################################################################
@@ -7,37 +7,42 @@ require 'test/helper'
 require 'test/unit'
 
 module MConA
-   M_CON_A = 1
+  M_CON_A = 1
 end
 
 module MConB
-   include MConA
-   M_CON_B = 1
+  include MConA
+  M_CON_B = 1
 end
 
 module MConC
 end
 
 class CConA
-   include MConA
-   include MConB
+  include MConA
+  include MConB
 end
 
 class TC_Module_Constants_InstanceMethods < Test::Unit::TestCase
-   def test_constants_basic
-      assert_respond_to(MConA, :constants)
-      assert_nothing_raised{ MConA.constants }
-      assert_kind_of(Array, MConA.constants)
-   end
+  test "constants method basic functionality" do
+    assert_respond_to(MConA, :constants)
+    assert_nothing_raised{ MConA.constants }
+    assert_kind_of(Array, MConA.constants)
+  end
 
-   def test_constants
-      assert_equal(['M_CON_A'], MConA.constants)
-      assert_equal(['M_CON_A', 'M_CON_B'], MConB.constants.sort)
-      assert_equal(['M_CON_A', 'M_CON_B'], CConA.constants.sort)
-      assert_equal([], MConC.constants)
-   end
+  test "constants method returns expected result" do
+    assert_equal([:M_CON_A], MConA.constants)
+    assert_equal([:M_CON_A, :M_CON_B], MConB.constants.sort)
+    assert_equal([:M_CON_A, :M_CON_B], CConA.constants.sort)
+    assert_equal([], MConC.constants)
+  end
 
-   def test_constants_expected_errors
-      assert_raise(ArgumentError){ MConA.constants(true) }
-   end
+  test "constants method with false argument returns expected result" do
+    assert_equal([:M_CON_B], MConB.constants(false))
+    assert_equal([], CConA.constants(false))
+  end
+
+  test "constants method takes a maximum of one argument" do
+    assert_raise(ArgumentError){ MConA.constants(true, false) }
+  end
 end
