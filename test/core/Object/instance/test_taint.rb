bd18c6f8c92f49724178cf2c79ecb5fcbeb08cc2
Daniel Berger <djberg96@gmail.com>
2015-04-26 08:14:18 -0600

    Updated and refactored.

--- test/core/Object/instance/test_taint.rb
+++ test/core/Object/instance/test_taint.rb
@@ -1,55 +1,58 @@
 ########################################################################
-# tc_taint.rb
+# test_taint.rb
 #
 # Test case for the Object#taint instance method.
 ########################################################################
 require 'test/helper'
-require 'test/unit'
+require 'test-unit'
 
 class TC_Object_Taint_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
-   
-   def setup
-      @object = Object.new
-   end
+  include Test::Helper
 
-   def test_taint_basic
-      assert_respond_to(@object, :taint)
-      assert_nothing_raised{ @object.taint }
-   end
+  def setup
+    @object = Object.new
+  end
 
-   def test_taint
-      assert_equal(@object, @object.taint)
-      assert_equal(true, @object.tainted?)
-   end
+  test "taint basic functionality" do
+    assert_respond_to(@object, :taint)
+    assert_nothing_raised{ @object.taint }
+    assert_equal(@object, @object.taint)
+  end
 
-   def test_taint_frozen_objects
-      assert_nothing_raised{ @object.freeze }
-      assert_raise(TypeError){ @object.taint }
-   end
+  test "taint works as expected" do
+    assert_false(@object.tainted?)
+    assert_nothing_raised{ @object.taint }
+    assert_true(@object.tainted?)
+  end
 
-   unless JRUBY
-      def test_taint_safe_environment
-         assert_nothing_raised{
-            proc do
-               $SAFE = 3
-               @object.taint
-            end.call
-         }
-         assert_raise(SecurityError){
-            proc do
-               $SAFE = 4
-               @object.taint
-            end.call
-         }
-      end
-   end
+  test "calling taint multiple times is ok" do
+    assert_nothing_raised{ @object.taint }
+    assert_nothing_raised{ @object.taint }
+    assert_nothing_raised{ @object.taint }
+    assert_true(@object.tainted?)
+  end
 
-   def test_taint_expected_errors
-      assert_raise(ArgumentError){ @object.taint(true) }
-   end
+  test "calling taint on a frozen object raises an error" do
+    assert_nothing_raised{ @object.freeze }
+    assert_raise(RuntimeError){ @object.taint }
+  end
 
-   def teardown
-      @object = nil
-   end
+  test "calling taint in a $SAFE environment works as expected" do
+    omit_if(JRUBY)
+
+    assert_nothing_raised{
+      proc do
+        $SAFE = 3
+        @object.taint
+      end.call
+    }
+  end
+
+  test "taint does not accept any arguments" do
+    assert_raise(ArgumentError){ @object.taint(true) }
+  end
+
+  def teardown
+    @object = nil
+  end
 end
