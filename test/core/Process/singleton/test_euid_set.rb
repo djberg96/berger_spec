a77ece6910d7ea721bda8ec1010fb95c3a6e1498
Daniel Berger <dberger@redhat.com>
2016-05-21 10:22:37 -0600

    Updated error check and other minor changes.

--- test/core/Process/singleton/test_euid_set.rb
+++ test/core/Process/singleton/test_euid_set.rb
@@ -22,9 +22,10 @@ class TC_Process_Euid_Set_SingletonMethod < Test::Unit::TestCase
     omit_if(WINDOWS, "Process.euid= tests skipped on MS Windows")
     omit_unless(ROOT, "Process.euid= tests skipped unless run as root")
 
-    assert_nothing_raised{ @euid = Process.euid }
+    @euid = Process.euid
     assert_nothing_raised{ Process.euid = @user.gid }
     assert_equal(@user.gid, Process.euid)
+
     assert_nothing_raised{ Process.euid = @euid }
     assert_equal(@euid, Process.euid)
   end
@@ -35,9 +36,9 @@ class TC_Process_Euid_Set_SingletonMethod < Test::Unit::TestCase
     assert_equal(@euid, Process.euid = @euid)
   end
 
-  test "euid= requires a numeric argument" do
+  test "euid= requires a valid user" do
     omit_if(WINDOWS, "Process.euid= tests skipped on MS Windows")
-    assert_raise(TypeError){ Process.euid = "test" }
+    assert_raise(ArgumentError){ Process.euid = "bogus" }
   end
 
   def teardown
