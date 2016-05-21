ec9ee980be40c774892ec5a2e25d3a884985bab7
Daniel Berger <dberger@redhat.com>
2016-05-20 21:56:38 -0600

    Refactored and fixed hash syntax.

--- test/core/ObjectSpace/singleton/test_define_finalizer.rb
+++ test/core/ObjectSpace/singleton/test_define_finalizer.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_define_finalizer.rb
+# test_define_finalizer.rb
 #
 # Test case for the ObjectSpace.define_finalizer.
 #
@@ -9,50 +9,45 @@
 require 'test/helper'
 require 'test/unit'
 
-class TC_ObjectSpace_DefineFinalizer_ClassMethod < Test::Unit::TestCase
-   def setup
-      @array  = [1,2,3]
-      @hash   = {1,2,3,4}
-      @string = 'hello'
-      @final  = nil
-      @proc   = proc{ @final = 'test' }
-   end
+class TC_ObjectSpace_DefineFinalizer_SingletonMethod < Test::Unit::TestCase
+  def setup
+    @array  = [1,2,3]
+    @hash   = {1 => 2, 3 => 4}
+    @string = 'hello'
+    @final  = nil
+    @proc   = proc{ @final = 'test' }
+  end
 
-   def test_define_finalizer_basic
-      assert_respond_to(ObjectSpace, :define_finalizer)
-   end
+  test "define_finalizer basic functionality" do
+    assert_respond_to(ObjectSpace, :define_finalizer)
+  end
 
-   def test_define_finalizer_with_proc
-      assert_nothing_raised{ ObjectSpace.define_finalizer(@array, @proc) }
-      assert_nil(@final)
-   end
+  test "define_finalizer with proc basic functionality" do
+    assert_nothing_raised{ ObjectSpace.define_finalizer(@array, @proc) }
+    assert_nil(@final)
+  end
 
-   def test_define_finalizer_with_block
-      assert_nothing_raised{
-         ObjectSpace.define_finalizer(@hash){ @final = 'test2' }
-      }
-      assert_nil(@final)
-   end
+  test "define_finalizer with block basic functionality" do
+    assert_nothing_raised{ ObjectSpace.define_finalizer(@hash){ @final = 'test2' } }
+    assert_nil(@final)
+  end
 
-   # What should happen here?
-   def test_define_finalizer_with_block_and_proc
-      assert_nothing_raised{
-         ObjectSpace.define_finalizer(@string, @proc){ @final = 'test3' }
-      }
-      assert_nil(@final)
-   end
+  test "define_finalizer with block and proc basic functionality" do
+    assert_nothing_raised{ ObjectSpace.define_finalizer(@string, @proc){ @final = 'test3' } }
+    assert_nil(@final)
+  end
 
-   def test_define_finalizer_expected_errors
-      assert_raise(ArgumentError){ ObjectSpace.define_finalizer }
-      assert_raise(ArgumentError){ ObjectSpace.define_finalizer(@array) }
-      assert_raise(ArgumentError){ ObjectSpace.define_finalizer(@array, 7) }
-      assert_raise(ArgumentError){ ObjectSpace.define_finalizer(@array, @proc, 7) }
-   end
+  test "define_finalizer requires valid arguments" do
+    assert_raise(ArgumentError){ ObjectSpace.define_finalizer }
+    assert_raise(ArgumentError){ ObjectSpace.define_finalizer(@array) }
+    assert_raise(ArgumentError){ ObjectSpace.define_finalizer(@array, 7) }
+    assert_raise(ArgumentError){ ObjectSpace.define_finalizer(@array, @proc, 7) }
+  end
 
-   def teardown
-      @array = nil
-      @hash  = nil
-      @final = nil
-      @proc  = nil
-   end
+  def teardown
+    @array = nil
+    @hash  = nil
+    @final = nil
+    @proc  = nil
+  end
 end
