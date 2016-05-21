85869714dc106564336b619ed39a13b20a5daf6c
Daniel Berger <dberger@redhat.com>
2016-05-21 10:17:08 -0600

    Refactored to use ENV['USER'].

--- test/core/Process/singleton/test_uid_set.rb
+++ test/core/Process/singleton/test_uid_set.rb
@@ -11,7 +11,7 @@ class TC_Process_Uid_Set_SingletonMethod < Test::Unit::TestCase
 
   def setup
     @uid = nil
-    @user = Etc.getpwnam('nobody')
+    @etc = Etc.getpwnam(ENV['USER'])
   end
 
   test "uid= basic functionality" do
@@ -21,27 +21,31 @@ class TC_Process_Uid_Set_SingletonMethod < Test::Unit::TestCase
   test "uid= works as expected" do
     omit_if(WINDOWS, "Process.uid= tests skipped on MS Windows")
     omit_unless(ROOT, "Process.uid= tests skipped unless run as root")
-
-    assert_nothing_raised{ @uid = Process.uid }
-    assert_nothing_raised{ Process.uid = @user.gid }
-    assert_equal(@user.gid, Process.uid)
-    assert_nothing_raised{ Process.uid = @uid }
-    assert_equal(@uid, Process.uid)
+    assert_nothing_raised{ Process.uid = @etc.gid }
+    assert_equal(@etc.gid, Process.uid)
   end
 
   test "uid= returns the assigned value" do
     omit_if(WINDOWS, "Process.uid= tests skipped on MS Windows")
+    omit_unless(ROOT, "Process.uid= tests skipped unless run as root")
     @uid = Process.uid
     assert_equal(@uid, Process.uid = @uid)
   end
 
-  test "uid= requires a numeric argument" do
+  test "uid= accepts an a string argument" do
     omit_if(WINDOWS, "Process.uid= tests skipped on MS Windows")
-    assert_raise(TypeError){ Process.uid = "test" }
+    omit_unless(ROOT, "Process.uid= tests skipped unless run as root")
+    assert_nothing_raised{ Process.uid = 'nobody' }
+  end
+
+  test "uid= requires a valid user" do
+    omit_if(WINDOWS, "Process.uid= tests skipped on MS Windows")
+    omit_unless(ROOT, "Process.uid= tests skipped unless run as root")
+    assert_raise(ArgumentError){ Process.uid = "bogus" }
   end
 
   def teardown
     @uid = nil
-    @user = nil
+    @etc = nil
   end
 end
