14f646886d2e8aa5a59ac00ee03c2a11710f5260
Daniel Berger <djberg96@gmail.com>
2016-05-22 08:42:36 -0600

    Ignore all tests on Windows, use helper methods.

--- test/core/Process/singleton/test_getpriority.rb
+++ test/core/Process/singleton/test_getpriority.rb
@@ -14,47 +14,51 @@ class TC_Process_Getpriority_SingletonMethod < Test::Unit::TestCase
   end
 
   test "getpriority basic functionality" do
+    omit_if_windows('Process.getpriority')
     assert_respond_to(Process, :getpriority)
   end
 
   test "getpriority returns expected results" do
-    omit_if(WINDOWS, "Process.getpriority tests skipped on MS Windows")
+    omit_if_windows('Process.getpriority')
+
     assert_kind_of(Fixnum, Process.getpriority(@kind, 0))
     assert_true(Process.getpriority(@kind, Process.pid) >= 0)
     assert_true(Process.getpriority(@kind, Process.pid) <= 100000)
   end
 
   test "getpriority accepts one of three process types" do
-    omit_if(WINDOWS, "Process.getpriority tests skipped on MS Windows")
+    omit_if_windows('Process.getpriority')
+
     assert_nothing_raised{ Process.getpriority(Process::PRIO_PGRP, 0) }
     assert_nothing_raised{ Process.getpriority(Process::PRIO_PROCESS, 0) }
     assert_nothing_raised{ Process.getpriority(Process::PRIO_USER, 0) }
   end
 
   test "getpriority requires two argument" do
-    omit_if(WINDOWS, "Process.getpriority tests skipped on MS Windows")
+    omit_if_windows('Process.getpriority')
+
     assert_raise(ArgumentError){ Process.getpriority }
     assert_raise(ArgumentError){ Process.getpriority(@kind, 0, 0) }
   end
 
   test "getpriority requires a valid process type" do
-    omit_if(WINDOWS, "Process.getpriority tests skipped on MS Windows")
+    omit_if_windows('Process.getpriority')
     assert_raise(Errno::EINVAL){ Process.getpriority(9999, 0) }
   end
 
   test "getpriority requires a numeric process type" do
-    omit_if(WINDOWS, "Process.getpriority tests skipped on MS Windows")
+    omit_if_windows('Process.getpriority')
     assert_raise(TypeError){ Process.getpriority("test", 0) }
   end
 
   test "getpriority requires a numeric pid" do
-    omit_if(WINDOWS, "Process.getpriority tests skipped on MS Windows")
+    omit_if_windows('Process.getpriority')
     assert_raise(TypeError){ Process.getpriority(@kind, "test") }
   end
 
   test "getpriority fails at $SAFE level 2 or higher" do
-    omit_if(WINDOWS, "Process.getpriority tests skipped on MS Windows")
-    omit_if(JRUBY, "Process.getpriority with $SAFE test skipped on JRuby")
+    omit_if_windows('Process.getpriority')
+    omit_if_jruby('Process.getpriority with $SAFE')
 
     assert_raise(SecurityError){
       proc do
@@ -65,8 +69,8 @@ class TC_Process_Getpriority_SingletonMethod < Test::Unit::TestCase
   end
 
   test "getpriority works at $SAFE level 1 or lower" do
-    omit_if(WINDOWS, "Process.getpriority tests skipped on MS Windows")
-    omit_if(JRUBY, "Process.getpriority with $SAFE test skipped on JRuby")
+    omit_if_windows('Process.getpriority')
+    omit_if_jruby('Process.getpriority with $SAFE')
 
     assert_nothing_raised{
       proc do
