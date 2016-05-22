2304e048ca915b25de803b922ec2ffe00c76d1ad
Daniel Berger <djberg96@gmail.com>
2016-05-22 08:34:37 -0600

    Skip all tests on Windows, use helper methods.

--- test/core/Process/singleton/test_getpgid.rb
+++ test/core/Process/singleton/test_getpgid.rb
@@ -14,25 +14,26 @@ class TC_Process_Getpgid_SingletonMethod < Test::Unit::TestCase
   end
 
   test "getpgid basic functionality" do
-    omit_if(WINDOWS, "Process.getpgid tests skipped on MS Windows")
+    omit_if_windows('Process.getpgid')
     assert_respond_to(Process, :getpgid)
     assert_nothing_raised{ Process.getpgid(@pid) }
     assert_kind_of(Fixnum, Process.getpgid(@pid))
   end
 
   test "getpgid returns expected results" do
-    omit_if(WINDOWS, "Process.getpgid tests skipped on MS Windows")
+    omit_if_windows('Process.getpgid')
     assert_true(Process.getpgid(@pid) > 0)
     assert_true(Process.getpgid(@pid) < 100000)
   end
 
   test "getpgid requires one argument" do
+    omit_if_windows('Process.getpgid')
     assert_raise(ArgumentError){ Process.getpgid }
     assert_raise(ArgumentError){ Process.getpgid(@pid, @pid) }
   end
 
   test "getpgid requires a numeric argument" do
-    omit_if(WINDOWS, "Process.getpgid tests skipped on MS Windows")
+    omit_if_windows('Process.getpgid')
     assert_raise(TypeError){ Process.getpgid("test") }
   end
 
