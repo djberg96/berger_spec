9ec9ba55187ac31da690e983ec36c835747907b8
Daniel Berger <djberg96@gmail.com>
2016-05-22 08:02:37 -0600

    Skip another test on Windows, add a permissions test.

--- test/core/Process/singleton/test_uid_set.rb
+++ test/core/Process/singleton/test_uid_set.rb
@@ -15,33 +15,39 @@ class TC_Process_Uid_Set_SingletonMethod < Test::Unit::TestCase
   end
 
   test "uid= basic functionality" do
+    omit_if_windows('Process.uid=')
     assert_respond_to(Process, :uid=)
   end
 
   test "uid= works as expected" do
-    omit_if(WINDOWS, "Process.uid= tests skipped on MS Windows")
-    omit_unless(ROOT, "Process.uid= tests skipped unless run as root")
+    omit_if_windows('Process.uid=')
+    omit_unless_root('Process.uid=')
     assert_nothing_raised{ Process.uid = @etc.gid }
     assert_equal(@etc.gid, Process.uid)
   end
 
   test "uid= returns the assigned value" do
-    omit_if(WINDOWS, "Process.uid= tests skipped on MS Windows")
-    omit_unless(ROOT, "Process.uid= tests skipped unless run as root")
+    omit_if_windows('Process.uid=')
+    omit_unless_root('Process.uid=')
     @uid = Process.uid
     assert_equal(@uid, Process.uid = @uid)
   end
 
   test "uid= accepts an a string argument" do
-    omit_if(WINDOWS, "Process.uid= tests skipped on MS Windows")
-    omit_unless(ROOT, "Process.uid= tests skipped unless run as root")
+    omit_if_windows('Process.uid=')
+    omit_unless_root('Process.uid=')
     assert_nothing_raised{ Process.uid = 'nobody' }
   end
 
   test "uid= requires a valid user" do
-    omit_if(WINDOWS, "Process.uid= tests skipped on MS Windows")
-    omit_unless(ROOT, "Process.uid= tests skipped unless run as root")
+    omit_if_windows('Process.uid=')
+    omit_unless_root('Process.uid=')
     assert_raise(ArgumentError){ Process.uid = "bogus" }
+  end
+
+  test "uid= raises an error without proper privileges" do
+    omit_if_root('Process.uid=')
+    assert_raise(Errno::EPERM){ Process.uid = @uid }
   end
 
   def teardown
