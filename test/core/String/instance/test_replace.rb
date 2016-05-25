1352dde85d4e4964f5391db830fe2fc5b84dcb88
Daniel Berger <dberger@redhat.com>
2016-05-25 04:16:30 -0600

    Updated and refactored.

--- test/core/String/instance/test_replace.rb
+++ test/core/String/instance/test_replace.rb
@@ -7,64 +7,83 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_String_Replace_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
    
-   def setup
-      @string1 = '<html><b>hello</b></html>'
-      @string2 = @string1
-   end
+  def setup
+    @string1 = '<html><b>hello</b></html>'
+    @string2 = @string1
+  end
 
-   def test_replace_basic
-      assert_respond_to(@string1, :replace)
-      assert_nothing_raised{ @string1.replace("") }
-   end
+  test "replace basic functionality" do
+    assert_respond_to(@string1, :replace)
+    assert_nothing_raised{ @string1.replace("") }
+  end
 
-   def test_replace
-      assert_equal("x,y,z", @string1.replace("x,y,z"))
-      assert_equal("x,y,z", @string1)
-      assert_equal(@string2, @string1)
-      assert_equal(@string2.object_id, @string1.object_id)
-   end
+  test "replace returns expected value" do
+    assert_equal("x,y,z", @string1.replace("x,y,z"))
+  end
+
+  test "replace modifies its receiver" do
+    @string1.replace("x,y,z")
+    assert_equal("x,y,z", @string1)
+  end
+
+  test "replace modifies any referent" do
+    @string1.replace("x,y,z")
+    assert_equal(@string2, @string1)
+    assert_equal(@string2.object_id, @string1.object_id)
+  end
+ 
+  test "replacing a string with itself is legal" do
+    assert_nothing_raised{ @string1.replace(@string1) }
+    assert_equal('<html><b>hello</b></html>', @string1)
+  end
+
+  test "replacing a string with itself is effectively a no-op" do
+    assert_equal(@string1.object_id, @string1.replace(@string1).object_id)
+  end
+
+  test "replacing a string with a tainted string makes it tainted" do
+    assert_false(@string1.replace('world').tainted?)
+    assert_true(@string1.replace('world'.taint).tainted?)
+  end
    
-   def test_replace_with_itself
-      assert_nothing_raised{ @string1.replace(@string1) }
-      assert_equal('<html><b>hello</b></html>', @string1)
-      assert_equal(@string1.object_id, @string1.replace(@string1).object_id)
-   end
+  test "attempting a replace a string in $SAFE mode 4 raises an error" do
+    omit_if_jruby('String#replace')
+    assert_raise(ArgumentError){
+      suppress_warning{
+        proc do
+          $SAFE = 4
+          @string1.replace('world')
+        end.call
+      }
+    }
+  end
+  
+  test "attempting a replace a string in $SAFE mode 3 or less does not raise an error" do
+    omit_if_jruby('String#replace')
+    assert_nothing_raised{
+      suppress_warning{
+        proc do
+          $SAFE = 3
+          @string1.taint
+          @string1.replace('test')
+        end.call
+      }
+    }
+  end
 
-   def test_replace_with_tainted_string
-      assert_equal(false, @string1.replace('world').tainted?)
-      assert_equal(true, @string1.replace('world'.taint).tainted?)
-   end
-   
-   # String#replace is illegal in $SAFE level >= 4 for untainted strings.
-   unless JRUBY
-      def test_replace_in_safe_mode
-         assert_raise(SecurityError){
-            proc do
-               $SAFE = 4
-               @string1.replace('world')
-            end.call
-         }
-      
-         assert_nothing_raised{
-            proc do
-               $SAFE = 3
-               @string1.taint
-               @string1.replace('test')
-            end.call
-         }
-      end
-   end
+  test "replace accepts a single string argument only" do
+    assert_raise(ArgumentError){ @string1.replace("x","y") }
+    assert_raise(TypeError){ @string1.replace(1) }
+  end
 
-   def test_replace_expected_errors
-      assert_raise(ArgumentError){ @string1.replace("x","y") }
-      assert_raise(TypeError){ @string1.replace(1) }
-      assert_raise(TypeError){ @string1.freeze.replace('') }
-   end
+  test "replace raises an error on a frozen string" do
+    assert_raise(RuntimeError){ @string1.freeze.replace('') }
+  end
 
-   def teardown
-      @string1 = nil
-      @string2 = nil
-   end
+  def teardown
+    @string1 = nil
+    @string2 = nil
+  end
 end
