7379e3644be2d948591f202961e5656b12cbdce7
Daniel Berger <dberger@redhat.com>
2016-05-25 04:56:42 -0600

    Revised and refactored.

--- test/core/String/instance/test_upto.rb
+++ test/core/String/instance/test_upto.rb
@@ -7,47 +7,59 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_String_Upto_Instance < Test::Unit::TestCase
-   def setup
-      @string1 = "a1"
-      @string2 = "hello"
-      @string3 = "\220"
-      @array   = []
-   end
+  def setup
+    @string_alpha   = "a1"
+    @string_numeric = "90"
+    @string_unicode = "\u00D7"
+    @array = []
+  end
 
-   def test_upto_basic
-      assert_respond_to(@string1, :upto)
-      assert_nothing_raised{ @string1.upto("a9"){} }
-   end
+  test "upto basic functionality" do
+    assert_respond_to(@string_alpha, :upto)
+    assert_nothing_raised{ @string_alpha.upto("a9") }
+    assert_nothing_raised{ @string_alpha.upto("a9"){} }
+  end
 
-   def test_upto
-      @string1.upto("a3"){ |str| @array << str }
-      assert_equal(['a1','a2','a3'], @array)
-      @array = []
+  test "upto returns an enumerator without a block" do
+    assert_kind_of(Enumerator, @string_alpha.upto("a9"))
+  end
 
-      @string2.upto("hellq"){ |str| @array << str }
-      assert_equal(['hello', 'hellp', 'hellq'], @array)
-      @array = []
+  test "upto behaves as expected with standard alpha characters" do
+    @string_alpha.upto("a3"){ |str| @array << str }
+    assert_equal(['a1','a2','a3'], @array)
+  end
 
-      @string3.upto("\222"){ |str| @array << str }
-      assert_equal(["\220", "\221", "\222"], @array)
-      @array = []
-   end
+  test "upto behaves as expected with numeric characters" do
+    @string_numeric.upto("92"){ |str| @array << str }
+    assert_equal(["90", "91", "92"], @array)
+  end
 
-   def test_upto_edge_cases
-      assert_nothing_raised{ @string1.upto(""){} }
-      assert_nothing_raised{ " ".upto("xxx"){} }
-      assert_nothing_raised{ "x".upto("a"){} }
-   end
+  test "upto behaves as expected with unicode characters" do
+    @string_unicode.upto("\u00D9"){ |str| @array << str }
+    assert_equal(["×", "Ø", "Ù"], @array)
+  end
 
-   def test_upto_expected_errors
-      assert_raises(TypeError){ @string1.upto(nil) }
-      assert_raises(LocalJumpError){ @string1.upto("a9") } # No block provided
-   end
+  test "passing an empty string argument is legal" do
+    assert_nothing_raised{ @string_alpha.upto(""){} }
+  end
 
-   def teardown
-      @string1 = nil
-      @string2 = nil
-      @string3 = nil
-      @array   = nil
-   end
+  test "calling upto on a blank string is legal" do 
+    assert_nothing_raised{ " ".upto("xxx"){} }
+  end
+
+  test "calling upto with an argument lower than itself is legal" do
+    assert_nothing_raised{ "x".upto("a") }
+    assert_nothing_raised{ "x".upto("a"){} }
+  end
+
+  test "calling upto with a nil argument raises an error in block form" do
+    assert_raises(TypeError){ @string_alpha.upto(nil){} }
+  end
+
+  def teardown
+    @string1 = nil
+    @string2 = nil
+    @string3 = nil
+    @array   = nil
+  end
 end
