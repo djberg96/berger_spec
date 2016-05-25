61e59a912ff39f122bcbc8c2f9efb6b3aefc0886
Daniel Berger <dberger@redhat.com>
2016-05-25 06:28:10 -0600

    Updated and refactored.

--- test/core/String/instance/test_tr_s.rb
+++ test/core/String/instance/test_tr_s.rb
@@ -6,65 +6,67 @@
 require 'test/helper'
 require 'test/unit'
 
-class TC_String_Tr_S_Instance < Test::Unit::TestCase
-   def setup
-      @string1 = "hello"                    # Simple
-      @string2 = "C:\\Program Files\\Test"  # Backslashes
-      @string3 = "\221\222\223\224\225"     # Accented characters
-   end
+class TC_String_Tr_S_InstanceMethod < Test::Unit::TestCase
+  def setup
+    @string_simple    = "hello"
+    @string_backslash = "C:\\Program Files\\Test"
+    @string_unicode   = "\u0092\u0093\u0094\u0095\u0096"
+  end
 
-   def test_tr_s_basic
-      assert_respond_to(@string1, :tr_s)
-      assert_nothing_raised{ @string1.tr_s('h', '*') }
-      assert_kind_of(String, @string1.tr_s('h', '*'))
-   end
+  test "tr_s basic functionality" do
+    assert_respond_to(@string_simple, :tr_s)
+    assert_nothing_raised{ @string_simple.tr_s('h', '*') }
+    assert_kind_of(String, @string_simple.tr_s('h', '*'))
+  end
 
-   def test_tr_s_bang_basic
-      assert_respond_to(@string1, :tr_s!)
-      assert_nothing_raised{ @string1.tr_s!('h', '*') }
-      assert_equal(nil, @string1.tr_s!('h', '*'))
-   end
+  test "tr_s works as expected with a single replacement character" do
+    assert_equal('h*ll*', @string_simple.tr_s('aeiou', '*'))
+    assert_equal('C:/Program Files/Test', @string_backslash.tr_s("\\", '/'))
+    assert_equal("\u0092\u0092\u0094\u0095\u0096" , @string_unicode.tr_s("\u0093", "\u0092"))
+  end
 
-   def test_tr_s_single_character
-      assert_equal('h*ll*', @string1.tr_s('aeiou', '*'))
-      assert_equal('C:/Program Files/Test', @string2.tr_s("\\", '/'))
-      assert_equal("\220\222\223\224\225", @string3.tr_s("\221", "\220"))
-   end
+  test "tr_s works as expected when multiple characters are replaced" do
+    assert_equal('hero', @string_simple.tr_s('l', 'r'))
+    assert_equal('h*o', @string_simple.tr_s('el', '*'))
+    assert_equal('hhxo', @string_simple.tr_s('el', 'hx'))
+  end
 
-   def test_tr_s_duplicate_character_removal
-      assert_equal('hero', @string1.tr_s('l', 'r'))
-      assert_equal('h*o', @string1.tr_s('el', '*'))
-      assert_equal('hhxo', @string1.tr_s('el', 'hx'))
-   end
+  test "tr_s works as expected when selecting and replacing multiple characters" do
+    assert_equal('hipo', @string_simple.tr_s('el', 'ip'))
+    assert_equal("C:\\Program Filsh\\Tsht", @string_backslash.tr_s('es', 'sh'))
+    assert_equal("\u0092\u0096\u0097\u0095\u0096" , @string_unicode.tr_s("\u0093\u0094", "\u0096\u0097"))
+  end
 
-   def test_tr_s_multiple_characters
-      assert_equal('hipo', @string1.tr_s('el', 'ip'))
-      assert_equal("C:\\Program Filsh\\Tsht", @string2.tr_s('es', 'sh'))
-      assert_equal("\226\227\223\224\225", @string3.tr_s("\221\222", "\226\227"))
-   end
+  test "tr_s works with negation as expected" do
+    assert_equal('*e*o', @string_simple.tr_s('^aeiou', '*'))
+  end
 
-   def test_tr_s_negation
-      assert_equal('*e*o', @string1.tr_s('^aeiou', '*'))
-   end
-
-   def test_tr_s_with_range
-      assert_equal('ifmp', @string1.tr_s('a-y', 'b-z'))
-   end
+  test "tr_s works with a range as expected" do
+    assert_equal('ifmp', @string_simple.tr_s('a-y', 'b-z'))
+  end
    
-   def test_tr_s_edge_cases
-      assert_equal('helli', @string1.tr_s('o', 'icopter')) # To longer than From
-      assert_equal('hexo', @string1.tr_s('ll', 'x'))       # From longer than To
-      assert_equal('hello', @string1.tr_s('x', 'y'))       # From not found
-   end
-   
-   def test_tr_s_expected_failures
-      assert_raises(TypeError){ @string1.tr_s('l', nil) }
-      assert_raises(ArgumentError){ @string1.tr_s('l') }
-   end
+  test "tr_s works as expected if the second argument is longer than the first" do
+    assert_equal('helli', @string_simple.tr_s('o', 'icopter'))
+  end
 
-   def teardown
-      @string1 = nil
-      @string2 = nil
-      @string3 = nil
-   end
+  test "tr_s works as expected if the first argument is longer than the second" do
+    assert_equal('hexo', @string_simple.tr_s('ll', 'x'))
+  end
+
+  test "tr_s works as expected if the first argument is not found" do
+    assert_equal('hello', @string_simple.tr_s('x', 'y'))
+  end
+
+  test "tr_s requires two string arguments" do
+    assert_raises(ArgumentError){ @string_simple.tr_s }
+    assert_raises(ArgumentError){ @string_simple.tr_s('l') }
+    assert_raises(TypeError){ @string_simple.tr_s('l', nil) }
+    assert_raises(TypeError){ @string_simple.tr_s('l', 1) }
+  end
+
+  def teardown
+    @string_simple    = nil
+    @string_backslash = nil
+    @string_unicode   = nil
+  end
 end
