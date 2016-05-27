4a036d9a0600bf69524d70a1cadc2fb454e76641
Daniel Berger <dberger@redhat.com>
2016-05-27 08:59:09 -0600

    Formatting and comment updates.
    Updated and refactored.

--- test/core/String/instance/test_swapcase_bang.rb
+++ test/core/String/instance/test_swapcase_bang.rb
@@ -2,48 +2,49 @@
 # test_swapcase_bang.rb
 #
 # Test case for the String#swapcase! instance method. Tests for the
-# String#swapcase method can be found in the tc_swapcase.rb instance
+# String#swapcase method can be found in the test_swapcase.rb instance
 # method.
-#
-# TODO: Add unicode tests.
 ###############################################################################
 require 'test/helper'
 require 'test/unit'
 
 class TC_String_SwapcaseBang_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @string = 'hello 123 WORLD'
-   end
+  def setup
+    @string_basic = 'hello 123 WORLD'
+  end
 
-   def test_swapcase_basic
-      assert_respond_to(@string, :swapcase)
-      assert_nothing_raised{ @string.swapcase! }
-      assert_kind_of(String, 'hello'.swapcase!)
-   end
+  test "swapcase! basic functionality" do
+    assert_respond_to(@string_basic, :swapcase!)
+    assert_nothing_raised{ @string_basic.swapcase! }
+    assert_kind_of(String, 'hello'.swapcase!)
+  end
 
-   def test_swapcase
-      assert_equal('HELLO 123 world', @string.swapcase!)
-      assert_equal('hello 123 WORLD', @string.swapcase!)
-   end
+  test "swapcase! returns the expected results" do
+    assert_equal('HELLO 123 world', @string_basic.swapcase!)
+    assert_equal('hello 123 WORLD', @string_basic.swapcase!)
+  end
 
-   def test_swapcase_original_string_modified
-      assert_nothing_raised{ @string.swapcase! }
-      assert_equal('HELLO 123 world', @string)
-   end
+  test "swapcase! modifies its receiver" do
+    @string_basic.swapcase!
+    assert_equal('HELLO 123 world', @string_basic)
+  end
 
-   def test_swapcase_edge_cases
-      assert_equal(nil, ''.swapcase!)
-      assert_equal(nil, ' '.swapcase!)
-      assert_equal(nil, '123'.swapcase!)
-      assert_equal(nil, '!@#$%^&*()'.swapcase!)
-   end
+  test "swapcase returns nil if no changes were made" do
+    assert_nil(''.swapcase!)
+    assert_nil(' '.swapcase!)
+    assert_nil('123'.swapcase!)
+    assert_nil('!@#$%^&*()'.swapcase!)
+  end
 
-   def test_swapcase_expected_errors
-      assert_raise(ArgumentError){ @string.swapcase!(1) }
-      assert_raise(TypeError){ @string.freeze.swapcase! }
-   end
+  test "swapcase! does not accept any arguments" do
+    assert_raise(ArgumentError){ @string_basic.swapcase!(1) }
+  end
 
-   def teardown
-      @string = nil
-   end
+  test "calling swapcase! on a frozen string raises an error" do
+    assert_raise(RuntimeError){ @string_basic.freeze.swapcase! }
+  end
+
+  def teardown
+    @string_basic = nil
+  end
 end
