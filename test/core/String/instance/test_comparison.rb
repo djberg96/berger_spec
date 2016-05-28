cd13bc4c1ff70449691ad2b0cecdd957057c48c2
Daniel Berger <dberger@redhat.com>
2016-05-28 10:03:26 -0600

    Updated and refactored.

--- test/core/String/instance/test_comparison.rb
+++ test/core/String/instance/test_comparison.rb
@@ -14,71 +14,70 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_String_Comparison_InstanceMethod < Test::Unit::TestCase
-   class StringCompare
-      include Comparable
-      attr :str
+  class StringCompare
+    include Comparable
+    attr :str
 
-      def initialize(str)
-         @str = str
-      end
+    def initialize(str)
+      @str = str
+    end
 
-      def <=>(other)
-         str.downcase <=> other.downcase
-      end
+    def <=>(other)
+      str.downcase <=> other.downcase
+    end
 
-      def to_str
-         "123"
-      end
-   end
+    def to_str
+      @str
+    end
+  end
 
-   def setup
-      @long   = "abcdef"
-      @short  = "abc"
-      @caps   = "ABCDEF"
-      @custom = StringCompare.new('ABC')
-   end
+  def setup
+    @long   = "abcdef"
+    @short  = "abc"
+    @caps   = "ABCDEF"
+    @custom = StringCompare.new('ABC')
+  end
 
-   def test_comparison_basic
-      assert_respond_to(@long, :<=>)
-      assert_nothing_raised{ @long <=> @short }
-      assert_kind_of(Fixnum, @long <=> @short)
-   end
+  test "comparison basic functionality" do
+    assert_respond_to(@long, :<=>)
+    assert_nothing_raised{ @long <=> @short }
+    assert_kind_of(Fixnum, @long <=> @short)
+  end
 
-   def test_comparison
-      assert_equal(0, @long <=> "abcdef")
-      assert_equal(1, @long <=> @caps)
-      assert_equal(1, @long <=> @short)
-      assert_equal(-1, @short <=> @long)
-   end
+  test "comparison of strings against other strings works as expected" do
+    assert_equal(0, @long <=> "abcdef")
+    assert_equal(1, @long <=> @caps)
+    assert_equal(1, @long <=> @short)
+    assert_equal(-1, @short <=> @long)
+  end
 
-   def test_comparison_edge_cases
-      assert_equal(0, '' <=> '')
-      assert_equal(-1, '' <=> ' ')
-      assert_equal(-1, ' ' <=> '       ')
-   end
+  test "comparison of blank or empty strings works as expected" do
+    assert_equal(0, '' <=> '')
+    assert_equal(-1, '' <=> ' ')
+    assert_equal(-1, ' ' <=> '       ')
+  end
 
-   def test_comparison_against_non_strings
-      assert_nil(@long <=> 1)
-      assert_nil(@long <=> [1,2,3])
-      assert_nil(@long <=> nil)
-      assert_nil(@long <=> true)
-      assert_nil(@long <=> false)
-   end
+  test "comparison of strings against non-strings works as expected" do
+    assert_nil(@long <=> 1)
+    assert_nil(@long <=> [1,2,3])
+    assert_nil(@long <=> nil)
+    assert_nil(@long <=> true)
+    assert_nil(@long <=> false)
+  end
 
-   # This is a caseless compare
-   def test_comparison_against_custom_compare
-      assert_equal(0, @short <=> @custom)
-      assert_equal(1, @long <=> @custom)
-      assert_equal(1, @caps <=> @custom)
-   end
+  test "caseless comparison against custom object" do
+    assert_equal(1, @short <=> @custom)
+    assert_equal(1, @long <=> @custom)
+    assert_equal(1, @caps <=> @custom)
+  end
 
-   def test_comparison_expected_errors
-      assert_raise(ArgumentError){ @long.send(:<=>, 'hello', 'world') }
-   end
+  test "comparison operator accepts one argument only" do
+    assert_raise(ArgumentError){ @long.send(:<=>, 'hello', 'world') }
+  end
 
-   def teardown
-      @long  = nil
-      @short = nil
-      @caps  = nil
-   end
+  def teardown
+    @long  = nil
+    @short = nil
+    @caps  = nil
+  end
 end
