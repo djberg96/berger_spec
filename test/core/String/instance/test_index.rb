a9078d94d0b58088aae214fc23bf83fe08774f6c
Daniel Berger <dberger@redhat.com>
2016-05-28 09:52:41 -0600

    Updated and refactored.

--- test/core/String/instance/test_index.rb
+++ test/core/String/instance/test_index.rb
@@ -6,76 +6,65 @@
 require 'test/helper'
 require "test/unit"
 
-class TC_String_Index_Instance < Test::Unit::TestCase
-   def setup
-      @simple  = "hello"
-      @complex = "p1031'/> <b><c n='field'/><c n='fl"
-   end
+class TC_String_Index_InstanceMethod < Test::Unit::TestCase
+  def setup
+    @simple  = "hello"
+    @complex = "p1031'/> <b><c n='field'/><c n='fl"
+  end
 
-   def test_index_basic
-      assert_respond_to(@simple, :index)
-      assert_nothing_raised{ @simple.index(0) }
-      assert_nothing_raised{ @simple.index("h") }
-      assert_nothing_raised{ @simple.index(/\w+/) }
-   end
+  test "index basic functionality" do
+    assert_respond_to(@simple, :index)
+    assert_nothing_raised{ @simple.index('h') }
+    assert_nothing_raised{ @simple.index(/\w+/) }
+    assert_kind_of(Numeric, @simple.index('h'))
+  end
 
-   # This test added due to a bug report (ruby-core:6721) where more
-   # complex substrings would return nil on 64 bit platforms.
-   #
-   def test_index_string_complex
-      assert_equal(9, @complex.index("<b><c n='field'/><c n='fl"))
-   end
+  test "index returns expected value" do
+    assert_equal(0, @simple.index("h"))
+    assert_equal(1, @simple.index("ell"))
+    assert_nil(@simple.index("z"))
+  end
 
-   def test_index_string
-      assert_equal(0, @simple.index("h"))
-      assert_equal(1, @simple.index("ell"))
-      assert_nil(@simple.index("z"))
-   end
+  # This test added due to a bug report (ruby-core:6721) where more
+  # complex substrings would return nil on 64 bit platforms.
+  #
+  test "index on complex string works as expected" do
+    assert_equal(9, @complex.index("<b><c n='field'/><c n='fl"))
+  end
 
-   def test_index_string_with_offset
-      assert_equal(3, @simple.index("l",3))
-      assert_nil(@simple.index("l",4))
-      assert_nil(@simple.index("h",1))
-      assert_nil(@simple.index("z",1))
-      assert_nil(@simple.index("z",99))
-   end
+  test "index with string plus offset works as expected" do
+    assert_equal(3, @simple.index("l", 3))
+    assert_nil(@simple.index("l", 4))
+    assert_nil(@simple.index("h", 1))
+    assert_nil(@simple.index("z", 1))
+    assert_nil(@simple.index("z", 99))
+  end
 
-   def test_index_int
-      assert_equal(0, @simple.index(?h))
-      assert_equal(1, @simple.index(?e))
-      assert_nil(@simple.index(?z))
-   end
+  test "index with regular expression works as expected" do
+    assert_equal(0, @simple.index(/h/))
+    assert_equal(2, @simple.index(/ll./))
+    assert_nil(@simple.index(/z./))
+  end
 
-   def test_index_int_with_offset
-      assert_equal(0, @simple.index(?h,0))
-      assert_equal(3, @simple.index(?l,3))
-      assert_equal(1, @simple.index(?e,0))
-      assert_nil(@simple.index(?l,4))
-      assert_nil(@simple.index(?h,1))
-      assert_nil(@simple.index(?z,0))
-   end
+  test "index with regular expression plus offset works as expected" do
+    assert_equal(0, @simple.index(/h/, 0))
+    assert_equal(2, @simple.index(/ll./, 0))
+    assert_nil(@simple.index(/ll./, 3))
+    assert_nil(@simple.index(/h/, 1))
+    assert_nil(@simple.index(/z./, 0))
+  end
 
-   def test_index_regex
-      assert_equal(0, @simple.index(/h/))
-      assert_equal(2, @simple.index(/ll./))
-      assert_nil(@simple.index(/z./))
-   end
+  test "index raises an error if the argument is invalid" do
+    assert_raises(TypeError){ @simple.index(1) }
+    assert_raises(TypeError){ @simple.index(1..3) }
+  end
 
-   def test_index_regex_with_offset
-      assert_equal(0, @simple.index(/h/, 0))
-      assert_equal(2, @simple.index(/ll./, 0))
-      assert_nil(@simple.index(/ll./, 3))
-      assert_nil(@simple.index(/h/, 1))
-      assert_nil(@simple.index(/z./, 0))
-   end
+  test "index accepts a maximum of two arguments" do
+    assert_raises(ArgumentError){ @simple.index('l',1,2) }
+  end
 
-   def test_index_expected_errors
-      assert_raises(TypeError){ @simple.index(1..3) }
-      assert_raises(ArgumentError){ @simple.index(1,2,3) }
-   end
-
-   def teardown
-      @simple  = nil
-      @complex = nil
-   end
+  def teardown
+    @simple  = nil
+    @complex = nil
+  end
 end
