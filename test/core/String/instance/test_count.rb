2c81644eae17a3f169b3d50ab4f82e1898288bd6
Daniel Berger <dberger@redhat.com>
2016-05-28 03:40:18 -0600

    Updated and refactored.

--- test/core/String/instance/test_count.rb
+++ test/core/String/instance/test_count.rb
@@ -6,57 +6,57 @@
 require 'test/helper'
 require "test/unit"
 
-class TC_String_Count_Instance < Test::Unit::TestCase
-   def setup
-      @str = "<html><b>Hello</b></html>\r\n\t"
-      @hob = "hel-[()]-lo012^"
-   end
+class TC_String_Count_InstanceMethod < Test::Unit::TestCase
+  def setup
+    @str = "<html><b>Hello</b></html>\r\n\t"
+    @hob = "hel-[()]-lo012^"
+  end
 
-   def test_count_basic
-      assert_respond_to(@str, :count)
-      assert_nothing_raised{ @str.count("l") }
-      assert_nothing_raised{ @str.count("hello","^l") }
-   end
+  test "count basic functionality" do
+    assert_respond_to(@str, :count)
+    assert_nothing_raised{ @str.count("l") }
+    assert_nothing_raised{ @str.count("hello", "^l") }
+    assert_kind_of(Numeric, @str.count("l"))
+  end
 
-   def test_count
-      assert_equal(2, @str.count("h"))
-      assert_equal(1, @str.count("H"))
-      assert_equal(4, @str.count("l"))
-      assert_equal(4, @str.count("<"))
-      assert_equal(4, @str.count(">"))
-      assert_equal(2, @str.count("/"))
-      assert_equal(1, @str.count("\n"))
-      assert_equal(1, @str.count("\r"))
-      assert_equal(1, @str.count("\t"))
-      assert_equal(0, @str.count(""))     # note
-   end
+  test "count returns the expected result" do
+    assert_equal(2, @str.count("h"))
+    assert_equal(1, @str.count("H"))
+    assert_equal(4, @str.count("l"))
+    assert_equal(4, @str.count("<"))
+    assert_equal(4, @str.count(">"))
+    assert_equal(2, @str.count("/"))
+    assert_equal(1, @str.count("\n"))
+    assert_equal(1, @str.count("\r"))
+    assert_equal(1, @str.count("\t"))
+    assert_equal(0, @str.count(""))
+  end
 
-   def test_count_negation
-      assert_equal(0, @str.count("b","^b"))
-      assert_equal(6, @str.count("html","^l"))
-      assert_equal(2, @str.count("\r\n\t","^\n"))
-   end
+  test "count with negation works as expected" do
+    assert_equal(0, @str.count("b","^b"))
+    assert_equal(6, @str.count("html","^l"))
+    assert_equal(2, @str.count("\r\n\t","^\n"))
+  end
 
-   def test_count_sequence
-      assert_equal(6, @str.count("l-m"))
-      assert_equal(0, @str.count("m-l")) # note
-      assert_equal(4, @str.count("-l"))
-   end
+  test "count with character range works as expected" do
+    assert_equal(6, @str.count("l-m"))
+    assert_equal(4, @str.count("-l"))
+  end
    
-   # Inspired by JRUBY-1720
-   def test_count_high_order_bytes
-      assert_equal(1, @hob.count('['))
-      assert_equal(1, @hob.count('^'))
-      assert_equal(15, @hob.count("\x00-\xFF")) 
-   end
+  # Inspired by JRUBY-1720
+  test "count with high order bytes works as expected" do
+    assert_equal(1, @hob.count('['))
+    assert_equal(1, @hob.count('^'))
+  end
 
-   def test_count_expected_errors
-      assert_raises(ArgumentError){ @str.count }
-      assert_raises(TypeError){ @str.count(1) }
-   end
+  test "count requires a valid argument" do
+    assert_raises(ArgumentError){ @str.count("m-l") }
+    assert_raises(ArgumentError){ @str.count }
+    assert_raises(TypeError){ @str.count(1) }
+  end
 
-   def teardown
-      @str = nil
-      @hob = nil
-   end
+  def teardown
+    @str = nil
+    @hob = nil
+  end
 end
