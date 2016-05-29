b49ea97eaa15283146d97e404d3ade26d8957b45
Daniel Berger <dberger@redhat.com>
2016-05-29 12:13:51 -0600

    Updated and refactored.

--- test/core/Time/instance/test_to_s.rb
+++ test/core/Time/instance/test_to_s.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_to_s.rb
+# test_to_s.rb
 #
 # Test case for the Time#to_s instance method.
 ########################################################################
@@ -7,41 +7,43 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Time_ToS_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @time   = Time.local(2007, 2, 3, 4, 5, 6)
-      @offset = get_tz_offset
+  def setup
+    @time   = Time.local(2007, 2, 3, 4, 5, 6)
+    @offset = get_tz_offset
 
-      if @offset < 10
-         @offset = "-0#{@offset}00"
-      else
-         @offset = "-#{@offset}00"
-      end
-   end
+    if @offset < 10
+      @offset = "-0#{@offset}00"
+    else
+      @offset = "-#{@offset}00"
+    end
+  end
 
-   def test_to_s_basic
-      assert_respond_to(:@time, :to_s)
-      assert_nothing_raised{ @time.to_s }
-      assert_kind_of(String, @time.to_s)
-   end
+  test "to_s basic functionality" do
+    assert_respond_to(:@time, :to_s)
+    assert_nothing_raised{ @time.to_s }
+    assert_kind_of(String, @time.to_s)
+  end
 
-   # MS Windows provides the Standard Name for '%z' instead of the offset
-   def test_to_s
-      assert_equal("Sat Feb 03 04:05:06 #{@offset} 2007", @time.to_s)
-      assert_equal("Thu Jan 01 00:00:00 UTC 1970", Time.gm(1970).to_s)
-      unless WINDOWS
-         assert_equal(@time.to_s, @time.strftime("%a %b %d %H:%M:%S %z %Y"))
-      end
-   end
+  test "to_s returns the expected string" do
+    assert_equal("2007-02-03 04:05:06 -0700", @time.to_s)
+    assert_equal("1970-01-01 00:00:00 UTC", Time.gm(1970).to_s)
+  end
 
-   def test_to_s_expected_errors
-      assert_raise(ArgumentError){ @time.to_s(1) }
-      assert_raise(NoMethodError){ @time.to_s = 1 }
-   end
+  # MS Windows provides the Standard Name for '%z' instead of the offset
+  test "to_s with %z" do
+    omit_if_windows("strftime")
+    assert_equal(@time.strftime("%Y-%m-%d %H:%M:%S %z"), @time.to_s)
+  end
 
-   def teardown
-      @time   = nil
-      @offset = nil
-   end
+  test "to_s does not accept any arguments" do
+    assert_raise(ArgumentError){ @time.to_s(1) }
+    assert_raise(NoMethodError){ @time.to_s = 1 }
+  end
+
+  def teardown
+    @time   = nil
+    @offset = nil
+  end
 end
