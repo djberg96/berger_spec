a072dbfaa11f4bba55f94ff294352488df4856b5
Daniel Berger <dberger@redhat.com>
2016-05-29 11:23:56 -0600

    Updated and refactored.

--- test/core/Time/instance/test_comparable.rb
+++ test/core/Time/instance/test_comparable.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_comparable.rb
+# test_comparable.rb
 #
 # These tests validate the various Comparable mixin methods for the
 # Time class.
@@ -8,97 +8,115 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Time_Comparable_InstanceMethods < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @now    = Time.now
-      @future = @now + 86400
-      @past   = @now - 86400
-   end
+  def setup
+    @now    = Time.now
+    @future = @now + 86400
+    @past   = @now - 86400
+  end
 
-   def test_between
-      assert_boolean(@now.between?(@past, @future))
-      assert_true(@now.between?(@past, @future))
-      assert_false(@now.between?(@future, @past))
-      assert_true(@now.between?(@now, @now))
-      assert_true(@now.between?(@now, @future))
-      assert_false(@now.between?(@now, @past))
-   end
+  test "between? returns expected results" do
+    assert_boolean(@now.between?(@past, @future))
+    assert_true(@now.between?(@past, @future))
+    assert_false(@now.between?(@future, @past))
+    assert_true(@now.between?(@now, @now))
+    assert_true(@now.between?(@now, @future))
+    assert_false(@now.between?(@now, @past))
+  end
 
-   def test_expected_between_failures
-      assert_raise(ArgumentError){ @now.between?(0, @future) }
-      assert_raise(ArgumentError){ @now.between?(@now, nil) }
-      assert_raise(ArgumentError){ @now.between?(@now, @now, @now) }
-   end
+  test "between? raises an error if arguments are invalid" do
+    assert_raise(ArgumentError){ @now.between?(0, @future) }
+    assert_raise(ArgumentError){ @now.between?(@now, nil) }
+  end
 
-   def test_greater_than
-      assert_boolean(@now > @future)
-      assert_false(@now > @now)
-      assert_false(@now > @future)
-      assert_true(@future > @now)
-   end
+  test "between? requires two arguments" do
+    assert_raise(ArgumentError){ @now.between?(@now) }
+    assert_raise(ArgumentError){ @now.between?(@now, @now, @now) }
+  end
 
-   def test_expected_greater_than_failures
-      assert_raise(ArgumentError){ @now > 1000 }
-      assert_raise(ArgumentError){ @now > true }
-      assert_raise(ArgumentError){ @now.send(:>, @future, 1) }
-   end
+  test "> returns expected results" do
+    assert_boolean(@now > @future)
+    assert_false(@now > @now)
+    assert_false(@now > @future)
+    assert_true(@future > @now)
+  end
 
-   def test_greater_than_or_equal_to
-      assert_boolean(@now >= @future)
-      assert_true(@now >= @now)
-      assert_false(@now >= @future)
-      assert_true(@future >= @now)
-   end
+  test "> raises an error if arguments are invalid" do
+    assert_raise(ArgumentError){ @now > 1000 }
+    assert_raise(ArgumentError){ @now > true }
+  end
 
-   def test_expected_greater_than_or_equal_to_failures
-      assert_raise(ArgumentError){ @now >= 1000 }
-      assert_raise(ArgumentError){ @now >= true }
-      assert_raise(ArgumentError){ @now.send(:>=, @future, 1) }
-   end
+  test "> requires one argument only" do
+    assert_raise(ArgumentError){ @now.send(:>, @future, 1) }
+  end
 
-   def test_equals
-      assert_boolean(@now == @future)
-      assert_true(@now == @now)
-      assert_false(@now == @future)
-      assert_false(@future == @now)
-   end
+  test ">= returns expected results" do
+    assert_boolean(@now >= @future)
+    assert_true(@now >= @now)
+    assert_false(@now >= @future)
+    assert_true(@future >= @now)
+  end
 
-   def test_expected_equals_failures
-      err = "=> Known issue in MRI as of 1.8.6 p111"
-      assert_raise(ArgumentError, err){ @now == 1000 }
-      assert_raise(ArgumentError, err){ @now == true }
-      assert_raise(ArgumentError){ @now.send(:==, @future, 1) }
-   end
+  test ">= raises an error if arguments are invalid" do
+    assert_raise(ArgumentError){ @now >= 1000 }
+    assert_raise(ArgumentError){ @now >= true }
+  end
 
-   def test_less_than_or_equal_to
-      assert_boolean(@now <= @future)
-      assert_true(@now <= @now)
-      assert_true(@now <= @future)
-      assert_false(@future <= @now)
-   end
+  test ">= requires one argument only" do
+    assert_raise(ArgumentError){ @now.send(:>=, @future, 1) }
+  end
 
-   def test_expected_less_than_or_equal_to_failures
-      assert_raise(ArgumentError){ @now <= 1000 }
-      assert_raise(ArgumentError){ @now <= true }
-      assert_raise(ArgumentError){ @now.send(:<=, @future, 1) }
-   end
+  test "== returns expected results" do
+    assert_boolean(@now == @future)
+    assert_true(@now == @now)
+    assert_false(@now == @future)
+    assert_false(@future == @now)
+  end
 
-   def test_less_than
-      assert_boolean(@now < @future)
-      assert_equal(true, @now < @future)
-      assert_equal(false, @future < @now)
-   end
+  test "== returns boolean for seemingly illegal equality checks" do
+    assert_false(@now == 1000) 
+    assert_false(@now == true)
+  end
 
-   def test_expected_less_than_failures
-      assert_raise(ArgumentError){ @now < 1000 }
-      assert_raise(ArgumentError){ @now < true }
-      assert_raise(ArgumentError){ @now.send(:<, @future, 1) }
-   end
+  test "== requires one argument only" do
+    assert_raise(ArgumentError){ @now.send(:==, @future, 1) }
+  end
 
-   def teardown
-      @now    = nil
-      @future = nil
-      @past   = nil
-   end
+  test "<= returns expected results" do
+    assert_boolean(@now <= @future)
+    assert_true(@now <= @now)
+    assert_true(@now <= @future)
+    assert_false(@future <= @now)
+  end
+
+  test "<= raises an error if arguments are invalid" do
+    assert_raise(ArgumentError){ @now <= 1000 }
+    assert_raise(ArgumentError){ @now <= true }
+  end
+
+  test "<= requires one argument only" do
+    assert_raise(ArgumentError){ @now.send(:<=, @future, 1) }
+  end
+
+  test "< returns expected results" do
+    assert_boolean(@now < @future)
+    assert_true(@now < @future)
+    assert_false(@future < @now)
+  end
+
+  test "< raises an error if arguments are invalid" do
+    assert_raise(ArgumentError){ @now < 1000 }
+    assert_raise(ArgumentError){ @now < true }
+  end
+
+  test "< requires one argument only" do
+    assert_raise(ArgumentError){ @now.send(:<, @future, 1) }
+  end
+
+  def teardown
+    @now    = nil
+    @future = nil
+    @past   = nil
+  end
 end
