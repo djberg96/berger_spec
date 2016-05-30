fc96d55ae7b2c8fd0f7b17b9c008a43a69154c36
Daniel Berger <dberger@redhat.com>
2016-05-30 10:45:49 -0600

    Updated and refactored.

--- test/core/Thread/instance/test_aref.rb
+++ test/core/Thread/instance/test_aref.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_aref.rb
+# test_aref.rb
 #
 # Test case for the Thread#[] instance method.
 ########################################################################
@@ -7,28 +7,28 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Thread_Aref_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @thread = Thread.new{ Thread.current['name'] = 'A'; Thread.stop }
-   end
+  def setup
+    @thread = Thread.new{ Thread.current[:name] = 'A' }
+    @thread.join
+  end
    
-   def test_aref_basic
-      assert_respond_to(@thread, :[])
-      assert_nothing_raised{ Thread.list.each{ |t| t['name'] } }
-   end
+  test "aref basic functionality" do
+    assert_respond_to(@thread, :[])
+    assert_nothing_raised{ Thread.list.each{ |t| t[:name] } }
+  end
    
-   def test_aref
-      assert_equal('A', @thread['name'])
-      assert_equal('A', @thread[:name])
-      assert_nil(@thread['bogus'])
-   end
+  test "aref returns expected result" do
+    assert_equal('A', @thread[:name])
+    assert_nil(@thread[:bogus])
+  end
+
+  test "aref requires one argument" do
+    assert_raise(ArgumentError){ @thread[] }
+    assert_raise(ArgumentError){ @thread['name', 1] }
+  end
    
-   def test_aref_expected_errors
-      assert_raise(ArgumentError){ @thread[] }
-      assert_raise(ArgumentError){ @thread['name', 1] }
-   end
-   
-   def teardown
-      @thread.exit
-      @thread = nil
-   end
+  def teardown
+    @thread.exit
+    @thread = nil
+  end
 end
