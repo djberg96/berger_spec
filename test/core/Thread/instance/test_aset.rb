cb720719d4d5b0cb5e7d7042f774203d00cf0ea1
Daniel Berger <dberger@redhat.com>
2016-05-30 10:51:21 -0600

    Updated and refactored.

--- test/core/Thread/instance/test_aset.rb
+++ test/core/Thread/instance/test_aset.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_aset.rb
+# test_aset.rb
 #
 # Test case for the Thread#[]= instance method.
 ########################################################################
@@ -7,29 +7,29 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Thread_Aset_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @thread = Thread.new{ Thread.current['name'] = 'A'; Thread.stop }
-   end
+  def setup
+    @thread = Thread.new{ Thread.current[:name] = 'A' }
+    @thread.join
+  end
    
-   def test_aset_basic
-      assert_respond_to(@thread, :[]=)
-      assert_nothing_raised{ Thread.list.each{ |t| t['name'] = 'test' } }
-   end
+  test "[]= basic functionality" do
+    assert_respond_to(@thread, :[]=)
+    assert_nothing_raised{ Thread.list.each{ |t| t['name'] = 'test' } }
+  end
+ 
+  test "[]= works as expected" do
+    assert_equal('test', @thread[:name] = 'test')
+    assert_equal('test', @thread[:name])
+  end
+ 
+  test "[]= requires an argument and a value" do
+    assert_raise(ArgumentError){ @thread.send(:[]=) }
+    assert_raise(ArgumentError){ @thread.send(:[]=, 'x') }
+    assert_raise(ArgumentError){ @thread.send(:[]=, 'x', 'y', 'z') }
+  end
    
-   def test_aset
-      assert_equal('test', @thread['name'] = 'test')
-      assert_equal('test', @thread['name'])
-      assert_equal('test', @thread[:name])
-   end
-   
-   def test_aset_expected_errors
-      assert_raise(ArgumentError){ @thread.send(:[]=) }
-      assert_raise(ArgumentError){ @thread.send(:[]=, 'x') }
-      assert_raise(ArgumentError){ @thread.send(:[]=, 'x', 'y', 'z') }
-   end
-   
-   def teardown
-      @thread.exit
-      @thread = nil
-   end
+  def teardown
+    @thread.exit
+    @thread = nil
+  end
 end
