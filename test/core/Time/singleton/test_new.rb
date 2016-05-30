cbb217862add324f893d60e2d4a763fa6c3fb963
Daniel Berger <dberger@redhat.com>
2016-05-29 21:51:25 -0600

    Removed invalid test.

--- test/core/Time/singleton/test_new.rb
+++ test/core/Time/singleton/test_new.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_new.rb
+# test_new.rb
 #
 # Test case for the Time.new class method.
 ########################################################################
@@ -7,13 +7,9 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Time_New_ClassMethod < Test::Unit::TestCase
-   def test_new
-      assert_respond_to(Time, :new)
-      assert_nothing_raised{ Time.new }
-      assert_kind_of(Time, Time.new)
-   end
-
-   def test_new_expected_errors
-      assert_raise(ArgumentError){ Time.new(0) }
-   end
+  test "new basic functionality" do
+    assert_respond_to(Time, :new)
+    assert_nothing_raised{ Time.new }
+    assert_kind_of(Time, Time.new)
+  end
 end
