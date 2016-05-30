1c8ef1711b4719bb71e95ae20b9fd75bb143e8ba
Daniel Berger <dberger@redhat.com>
2016-05-30 08:27:34 -0600

    Updated and refactored.

--- test/core/Thread/instance/test_priority.rb
+++ test/core/Thread/instance/test_priority.rb
@@ -3,9 +3,6 @@
 #
 # Test case for the Thread#priority and Thread#priority= instance
 # methods.
-#
-# TODO: This will have to be updated for JRuby because of different
-# underlying priority levels in the JVM.
 ########################################################################
 require 'test/helper'
 require 'test/unit'
@@ -15,37 +12,42 @@ class TC_Thread_Priority_InstanceMethod < Test::Unit::TestCase
     @thread = Thread.new{ sleep 1 while true }
   end
 
-  def test_priority_basic
+  test "priority basic functionality" do
     assert_respond_to(@thread, :priority)
     assert_nothing_raised{ @thread.priority }
     assert_kind_of(Integer, @thread.priority)
   end
 
-  def test_priority_set_basic
+  test "priority returns the expected value" do
     assert_respond_to(@thread, :priority=)
     assert_nothing_raised{ @thread.priority = 5 }
     assert_kind_of(Integer, @thread.priority = 5)
   end
 
-  # We'll combine priority and priority= here
-  def test_priority
+  test "the default priority is zero" do
     assert_equal(0, @thread.priority) # The default
-    assert_nothing_raised{ @thread.priority = 5 }
-    assert_equal(5, @thread.priority)
   end
 
-  def test_priority_edge_cases
-    assert_nothing_raised{ @thread.priority = -5 }
-    assert_equal(-5, @thread.priority)
+  test "priority returns the set value" do
+    assert_nothing_raised{ @thread.priority = 3 }
+    assert_true(@thread.priority > 0)
+  end
+
+  test "setting priority to a negative value is legal" do
+    assert_nothing_raised{ @thread.priority = -3 }
+    assert_true(@thread.priority < 0)
+  end
+
+  test "setting priority to a float value is legal" do
     assert_nothing_raised{ @thread.priority = 2.5 }
     assert_equal(2, @thread.priority) # Converted to int
   end
 
-  def test_priority_expected_errors
+  test "priority method does not take any arguments" do
     assert_raise(ArgumentError){ @thread.priority(1) }
   end
 
-  def test_priority_set_expected_errors
+  test "priority= method accepts only one argument" do
     assert_raise(ArgumentError){ @thread.send(:priority=, 1, 2) }
   end
 
