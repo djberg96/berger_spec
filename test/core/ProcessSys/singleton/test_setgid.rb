1b03b7f68185399d6bd8d84cfed12d0b5f869cbb
Daniel Berger <dberger@redhat.com>
2016-05-30 15:17:31 -0600

    Updated and refactored.

--- test/core/ProcessSys/singleton/test_setgid.rb
+++ test/core/ProcessSys/singleton/test_setgid.rb
@@ -1,5 +1,5 @@
 ######################################################################
-# tc_setgid.rb
+# test_setgid.rb
 #
 # Test case for the Process::Sys.setgid module method.
 #
@@ -10,40 +10,40 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_ProcessSys_Setgid_ModuleMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   unless WINDOWS
-      def setup
-         @nobody_gid = Etc.getgrnam('nobody').gid
-         @login_gid  = Etc.getpwnam(Etc.getlogin).gid
-      end
-   end
+  def setup
+    @nobody_gid = Etc.getgrnam('nobody').gid
+    @login_gid  = Etc.getpwnam(Etc.getlogin).gid
+  end
 
-   def test_setgid_basic
-      assert_respond_to(Process::Sys, :setgid)
-   end
+  test "setgid basic" do
+    assert_respond_to(Process::Sys, :setgid)
+  end
 
-   if ROOT && !WINDOWS
-      def test_setgid
-         assert_nothing_raised{ Process::Sys.setgid(@nobody_gid) }
-         assert_equal(@nobody_gid, Process.gid)
-         assert_nothing_raised{ Process::Sys.setgid(@login_gid) }
-         assert_equal(@login_gid, Process.gid)
-      end
-   end
+  test "setgid works as expected" do
+    omit_unless_root('Process::Sys.setgid')
+    omit_if_windows('Process::Sys.setgid')
 
-   def test_gid_expected_errors
-      if WINDOWS
-         assert_raises(NotImplementedError){ Process::Sys.setgid(1) }
-      else
-         assert_raises(TypeError){ Process::Sys.setgid('bogus') }
-      end
-   end
+    assert_nothing_raised{ Process::Sys.setgid(@nobody_gid) }
+    assert_equal(@nobody_gid, Process.gid)
+    assert_nothing_raised{ Process::Sys.setgid(@login_gid) }
+    assert_equal(@login_gid, Process.gid)
+  end
 
-   unless WINDOWS
-      def teardown
-         @nobody_gid = nil
-         @login_gid  = nil
-      end
-   end
+  test "setgid raises a NotImplementedError on Windows" do
+    omit_unless_windows('Process::Sys.setgid')
+    assert_raises(NotImplementedError){ Process::Sys.setgid(1) }
+  end
+
+  test "setgid raises an ArgumentError if the group is not found" do
+    omit_if_windows('Process::Sys.setgid')
+    omit_unless_root('Process::Sys.setgid')
+    assert_raises(ArgumentError){ Process::Sys.setgid('bogus') }
+  end
+
+  def teardown
+    @nobody_gid = nil
+    @login_gid  = nil
+  end
 end
