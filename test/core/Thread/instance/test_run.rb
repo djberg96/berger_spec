9723f2f1dd5a54123bbe35cc5ae6f5eaebb356ba
Daniel Berger <dberger@redhat.com>
2016-05-30 11:33:39 -0600

    Updated and refactored.

--- test/core/Thread/instance/test_run.rb
+++ test/core/Thread/instance/test_run.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_run.rb
+# test_run.rb
 #
 # Test case for the Thread#run instance method.
 ########################################################################
@@ -8,44 +8,49 @@ require 'test/unit'
 
 class TC_Thread_Run_InstanceMethod < Test::Unit::TestCase
 
-   # For the setup I've created multiple break points to ensure that a call
-   # to Thread#run only runs up to the next breakpoint.
-   #
-   def setup
-      @num1 = 0
-      @num2 = 0
-      @thread = Thread.new{
-         Thread.stop
-         @num1 = 88
-         Thread.stop
-         @num2 = 99
-      }
-   end
+  # For the setup I've created multiple break points to ensure that a call
+  # to Thread#run only runs up to the next breakpoint.
+  #
+  def setup
+    @num1 = 0
+    @num2 = 0
 
-   def test_run_basic
-      assert_respond_to(@thread, :run)
-      assert_nothing_raised{ @thread.run }
-   end
+    @thread = Thread.new{
+      Thread.stop
+      @num1 = 88
+      Thread.stop
+      @num2 = 99
+    }
+  end
 
-   def test_run
-      assert_equal(0, @num1)
-      assert_equal(0, @num2)
+  test "run basic functionality" do
+    assert_respond_to(@thread, :run)
+    assert_nothing_raised{ @thread.run }
+  end
 
-      assert_nothing_raised{ @thread.run }
-      assert_equal(88, @num1)
-      assert_equal(0, @num2)
+  test "variables are set to expected values before run is called" do
+    assert_equal(0, @num1)
+    assert_equal(0, @num2)
+  end
 
-      assert_nothing_raised{ @thread.run }
-      assert_equal(88, @num1)
-      assert_equal(99, @num2)
-   end
+  test "variables are set to expected values after run is called once" do
+    @thread.run
+    assert_equal(88, @num1)
+    assert_equal(0, @num2)
+  end
 
-   def test_run_expected_errors
-      assert_raise(ArgumentError){ @thread.run(1) }
-   end
+  test "variables are set to expected values after run is called multiple times" do
+    2.times{ @thread.run }
+    assert_equal(88, @num1)
+    assert_equal(99, @num2)
+  end
 
-   def teardown
-      @thread.exit
-      @thread = nil
-   end
+  test "run does not accept any arguments" do
+    assert_raise(ArgumentError){ @thread.run(1) }
+  end
+
+  def teardown
+    @thread.exit
+    @thread = nil
+  end
 end
