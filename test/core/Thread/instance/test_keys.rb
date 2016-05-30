d9ca1cc3a3c8fda0b4f9e468009de32a78e4c467
Daniel Berger <dberger@redhat.com>
2016-05-30 11:39:30 -0600

    Refactored and updated.

--- test/core/Thread/instance/test_keys.rb
+++ test/core/Thread/instance/test_keys.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_keys.rb
+# test_keys.rb
 #
 # Test case for the Thread#keys instance method.
 ########################################################################
@@ -7,30 +7,32 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Thread_Keys_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @thread = Thread.new{
-         Thread.current['foo'] = 'test'
-         Thread.current[:bar] = 7
-      }
-   end
+  def setup
+    @thread = Thread.new{
+      Thread.current['foo'] = 'test'
+      Thread.current[:bar] = 7
+    }
 
-   def test_keys_basic
-      assert_respond_to(@thread, :keys)
-      assert_nothing_raised{ @thread.keys }
-      assert_kind_of(Array, @thread.keys)
-   end
+    @thread.join
+  end
 
-   def test_keys
-      assert_equal(['bar', 'foo'], @thread.keys.map{ |k| k.to_s }.sort)
-      assert_equal([], Thread.new{}.keys)
-   end
+  test "keys basic functionality" do
+    assert_respond_to(@thread, :keys)
+    assert_nothing_raised{ @thread.keys }
+    assert_kind_of(Array, @thread.keys)
+  end
 
-   def test_keys_expected_errors
-      assert_raise(ArgumentError){ @thread.keys(1) }
-   end
+  test "keys returns expected result" do
+    assert_equal(['bar', 'foo'], @thread.keys.map{ |k| k.to_s }.sort)
+    assert_equal([], Thread.new{}.keys)
+  end
 
-   def teardown
-      @thread.exit
-      @thread = nil
-   end
+  test "keys does not accept any arguments" do
+    assert_raise(ArgumentError){ @thread.keys(1) }
+  end
+
+  def teardown
+    @thread.exit
+    @thread = nil
+  end
 end
