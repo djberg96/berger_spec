ea16e1af570932a5f30758df94523b789414f4a3
Daniel Berger <dberger@redhat.com>
2016-05-30 08:45:03 -0600

    Updated and refactored.

--- test/core/Thread/instance/test_alive.rb
+++ test/core/Thread/instance/test_alive.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_alive?.rb
+# test_alive.rb
 #
 # Test case for the Thread#alive? instance method.
 ########################################################################
@@ -7,29 +7,29 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Thread_Alive_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @thread = Thread.new{ sleep 1 while true }
-   end
+  def setup
+    @thread = Thread.new{ sleep 1 while true }
+  end
 
-   def test_alive_basic
-      assert_respond_to(@thread, :alive?)
-      assert_boolean(@thread.alive?)
-   end
+  test "alive? basic functionality" do
+    assert_respond_to(@thread, :alive?)
+    assert_boolean(@thread.alive?)
+  end
 
-   def test_alive
-      assert_equal(true, @thread.alive?)
-      assert_nothing_raised{ @thread.exit }
-      assert_equal(false, @thread.alive?)
-   end
+  test "alive? returns expected results" do
+    assert_true(@thread.alive?)
+    @thread.exit; sleep 0.1
+    assert_false(@thread.alive?)
+  end
 
-   def test_alive?_expected_errors
-      assert_raise(ArgumentError){ Thread.alive?(true) }
-   end
+  test "alive? does not accept any arguments" do
+    assert_raise(ArgumentError){ @thread.alive?(true) }
+  end
 
-   def teardown
-      @thread.exit
-      @thread = nil
-   end
+  def teardown
+    @thread.exit
+    @thread = nil
+  end
 end
