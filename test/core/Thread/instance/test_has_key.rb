7856071e32ac90c42ee63c60e4dfb2ce1e39dd54
Daniel Berger <dberger@redhat.com>
2016-05-30 10:43:26 -0600

    Updated and refactored.

--- test/core/Thread/instance/test_has_key.rb
+++ test/core/Thread/instance/test_has_key.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_has_key.rb
+# test_has_key.rb
 #
 # Test case for the Thread#key? instance method.
 ########################################################################
@@ -7,33 +7,39 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Thread_HasKey_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @thread = Thread.new{
-         Thread.current['foo'] = 'test'
-         Thread.current[:bar] = 7
-      }
-   end
+  def setup
+    @thread = Thread.new{
+      Thread.current[:foo] = 'test'
+      Thread.current[:bar] = 7
+    }
 
-   def test_has_key_basic
-      assert_respond_to(@thread, :key?)
-      assert_nothing_raised{ @thread.key?(:foo) }
-      assert_boolean(@thread.key?(:foo))
-   end
+    @thread.join
+  end
 
-   def test_has_key
-      assert_equal(true, @thread.key?(:foo))
-      assert_equal(true, @thread.key?(:bar))
-      assert_equal(false, @thread.key?(:baz))
-   end
+  test "key? basic functionality" do
+    assert_respond_to(@thread, :key?)
+    assert_nothing_raised{ @thread.key?(:foo) }
+    assert_boolean(@thread.key?(:foo))
+  end
 
-   def test_has_key_expected_errors
-      assert_raise(ArgumentError){ @thread.key? }
-      assert_raise(ArgumentError){ @thread.key?(:foo, :bar) }
-      assert_raise(TypeError){ @thread.key?([]) }
-   end
+  test "key? returns expected result" do
+    assert_true(@thread.key?(:foo))
+    assert_true(@thread.key?(:bar))
+    assert_false(@thread.key?(:baz))
+  end
 
-   def teardown
-      @thread.exit
-      @thread = nil
-   end
+  test "key? requires a single argument" do
+    assert_raise(ArgumentError){ @thread.key? }
+    assert_raise(ArgumentError){ @thread.key?(:foo, :bar) }
+  end
+
+  test "key? raises an error if the argument is invalid" do
+    assert_raise(TypeError){ @thread.key?([]) }
+  end
+
+  def teardown
+    @thread.exit
+    @thread = nil
+    @current = nil
+  end
 end
