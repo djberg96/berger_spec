a43226002b5e88f241660c7a5f028ffe49c3358a
Daniel Berger <dberger@redhat.com>
2016-05-30 18:39:51 -0600

    Updated and refactored.

--- test/core/ProcessUID/singleton/test_re_exchangeable.rb
+++ test/core/ProcessUID/singleton/test_re_exchangeable.rb
@@ -1,5 +1,5 @@
 ######################################################################
-# tc_re_exchangeable.rb
+# test_re_exchangeable.rb
 #
 # Test case for the Process::UID.re_exchangeable module method.
 ######################################################################
@@ -7,23 +7,22 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_ProcessUID_ReExchangeable_ModuleMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def test_re_exchangeable_basic
-      assert_respond_to(Process::UID, :re_exchangeable?)
-      assert_nothing_raised{ Process::UID.re_exchangeable? }
-   end
+  test "re_exchangeable? basic functionality" do
+    omit_if_windows('Process::UID.re_exchangeable?')
+    assert_respond_to(Process::UID, :re_exchangeable?)
+    assert_nothing_raised{ Process::UID.re_exchangeable? }
+  end
 
-   def test_re_exchangeable
-      if WINDOWS
-         assert_equal(false, Process::UID.re_exchangeable?)
-      else
-         assert_equal(true, Process::UID.re_exchangeable?)
-      end
-   end
+  test "re_exchangeable? returns the expected value" do
+    omit_if_windows('Process::UID.re_exchangeable?')
+    assert_true(Process::UID.re_exchangeable?)
+  end
 
-   def test_re_exchangeable_expected_failures
-      assert_raises(ArgumentError){ Process::UID.re_exchangeable?(1) }
-      assert_raises(ArgumentError){ Process::UID.re_exchangeable?(1, 2) }
-   end
+  test "re_exchangeable? does not accept any arguments" do
+    omit_if_windows('Process::UID.re_exchangeable?')
+    assert_raises(ArgumentError){ Process::UID.re_exchangeable?(1) }
+    assert_raises(ArgumentError){ Process::UID.re_exchangeable?(1, 2) }
+  end
 end
