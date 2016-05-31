d11780b3f9c97c5f913a7e00c84ceea438f6eb6e
Daniel Berger <dberger@redhat.com>
2016-05-30 22:53:23 -0600

    Updated and refactored.

--- test/core/Thread/instance/test_abort_on_exception.rb
+++ test/core/Thread/instance/test_abort_on_exception.rb
@@ -1,5 +1,5 @@
 ###############################################################################
-# tc_abort_on_exception.rb
+# test_abort_on_exception.rb
 #
 # Test case for the Thread#abort_on_exception and Thread#abort_on_exception=
 # instance methods
@@ -8,38 +8,39 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Thread_AbortOnException_InstanceMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @thread = Thread.new{ raise 'AOE TEST' }
-   end
+  def setup
+    @thread = Thread.new{ raise 'AOE TEST' }
+  end
 
-   def test_abort_on_exception_basic
-      assert_respond_to(@thread, :abort_on_exception)
-      assert_respond_to(@thread, :abort_on_exception=)
-      assert_boolean(@thread.abort_on_exception)
-   end
+  test "abort_on_exception basic functionality" do
+    assert_respond_to(@thread, :abort_on_exception)
+    assert_boolean(@thread.abort_on_exception)
+  end
 
-   def test_abort_on_exception
-      assert_equal(false, @thread.abort_on_exception)
-      assert_equal(true, @thread.abort_on_exception = true)
-      assert_equal(true, @thread.abort_on_exception)
-   end
+  test "abort_on_exception= basic functionality" do
+    assert_respond_to(@thread, :abort_on_exception=)
+    assert_false(@thread.abort_on_exception = false)
+  end
 
-   # TODO: How do I test Thread#abort_on_exception = true without breaking
-   # the test suite?
-   #
-   def test_abort_on_exception_behavior
-      assert_raise(RuntimeError){ @thread.join }
-   end
+  test "abort_on_exception returns the expected value" do
+    assert_false(@thread.abort_on_exception)
+    assert_true(@thread.abort_on_exception = true)
+    assert_true(@thread.abort_on_exception)
+  end
 
-   def test_abort_on_exception_expected_errors
-      assert_raise(ArgumentError){ Thread.abort_on_exception(true) }
-   end
+  test "thread with exception raises error when joined" do
+    assert_raise(RuntimeError){ @thread.join }
+  end
 
-   def teardown
-      @thread.exit
-      @thread.abort_on_exception = false
-      @thread = nil
-   end
+  test "abort_on exception does not accept any arguments" do
+    assert_raise(ArgumentError){ Thread.abort_on_exception(true) }
+  end
+
+  def teardown
+    @thread.exit
+    @thread.abort_on_exception = false
+    @thread = nil
+  end
 end
