75cda67d889e8a0cfbabce805b82e1c099ee0755
Daniel Berger <dberger@redhat.com>
2016-05-30 22:36:30 -0600

    Refactor and update, remove tests until further notice.

--- test/core/ProcessUID/singleton/test_switch.rb
+++ test/core/ProcessUID/singleton/test_switch.rb
@@ -1,5 +1,5 @@
 ######################################################################
-# tc_switch.rb
+# test_switch.rb
 #
 # Test case for the Process::UID.switch module method. Most of these
 # tests only run on Unix systems, and then only as root.
@@ -13,35 +13,29 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_ProcessUID_Switch_ModuleMethod < Test::Unit::TestCase
-   include Test::Helper
+  include Test::Helper
 
-   def setup
-      @uid  = Process.uid
-      @euid = Process.euid
+  def setup
+    @uid  = Process.uid
+    @euid = Process.euid
+    @user = 'nobody'
+  end
 
-      if !WINDOWS && ROOT && @uid == @euid
-         Process.euid = Etc.getpwnam('nobody').uid
-         @euid = Process.euid
-      end
-   end
+  test "switch basic functionality" do
+    assert_respond_to(Process::UID, :switch)
+  end
 
-   def test_switch_basic
-      assert_respond_to(Process::UID, :switch)
-   end
+  test "switch behaves as expected" do
+    omit_if_windows('Process::UID.switch')
+    omit_unless_root('Process::UID.switch')
+    Process.euid = Etc.getpwnam(@user).uid
 
-   if ROOT && !WINDOWS
-      def test_switch
-         assert_nothing_raised{ Process::UID.switch }
-         #assert_equal(@euid, Process.uid)
-         #assert_equal(@uid, Process.euid)
-      end
+    assert_nothing_raised{ Process::UID.switch }
+  end
 
-      #def test_switch_with_block
-      #end
-   end
-
-   def teardown
-      @uid  = nil
-      @euid = nil
-   end
+  def teardown
+    @uid  = nil
+    @euid = nil
+    @user = nil
+  end
 end
