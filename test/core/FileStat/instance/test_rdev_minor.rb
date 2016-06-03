33753faafa1ef8e099dfecc3601e39c7e1f22970
Daniel Berger <dberger@redhat.com>
2016-06-02 21:56:29 -0600

    Updated and refactored.

--- test/core/FileStat/instance/test_rdev_minor.rb
+++ test/core/FileStat/instance/test_rdev_minor.rb
@@ -1,48 +1,40 @@
 ######################################################################
-# tc_rdev_minor.rb
+# test_rdev_minor.rb
 #
 # Test case for the FileStat#rdev_minor instance method.
 ######################################################################
 require 'test/helper'
 require 'test/unit'
 
-class TC_FileStat_RdevMinor_Instance < Test::Unit::TestCase
-   include Test::Helper
+class TC_FileStat_RdevMinor_InstanceMethod < Test::Unit::TestCase
+  include Test::Helper
    
-   def setup
-      @stat = File::Stat.new(__FILE__)
-      @rdev = 0
-   end
+  def setup
+    @stat = File::Stat.new(__FILE__)
+    @rdev = 0
+  end
 
-   def test_rdev_minor_basic
-      assert_respond_to(@stat, :rdev_minor)
-      
-      if WINDOWS
-         assert_nil(@stat.rdev_minor)
-      else
-         assert_kind_of(Fixnum, @stat.rdev_minor)
-      end
-   end
+  test "rdev_minor_basic functionality" do
+    assert_respond_to(@stat, :rdev_minor)
+  end
 
-   # I'm not sure if there's a more predictable way for OS X.
-   def test_rdev_minor
-      unless WINDOWS
-         assert_equal(0, @stat.rdev_minor)
-         if OSX
-            @rdev = File::Stat.new('/dev/stdin').rdev_minor
-            assert_equal(true, [1,2,3].include?(@rdev))
-         else
-            assert_equal(@rdev, File::Stat.new('/dev/stdin').rdev_minor)
-         end
-      end
-   end
+  test "rdev_minor returns expected type of value" do
+    expected_type = WINDOWS ? NilClass : Fixnum
+    assert_kind_of(expected_type, @stat.rdev_minor)
+  end
 
-   def test_rdev_minor_expected_errors
-      assert_raises(ArgumentError){ @stat.rdev_minor(1) }
-   end
+  test "rdev_minor returns the expected value" do
+    omit_if_windows('rdev_minor')
+    assert_equal(0, @stat.rdev_minor)
+    assert_true(File::Stat.new('/dev/stdin').rdev_minor >= 0)
+  end
 
-   def teardown
-      @stat = nil
-      @rdev = nil
-   end
+  test "rdev_minor does not accept any arguments" do
+    assert_raises(ArgumentError){ @stat.rdev_minor(1) }
+  end
+
+  def teardown
+    @stat = nil
+    @rdev = nil
+  end
 end
