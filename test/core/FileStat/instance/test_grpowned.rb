1a2d3158d0f11ee0c71c4e54d359f1245f8f15a4
Daniel Berger <dberger@redhat.com>
2016-06-02 21:39:16 -0600

    Removed unnecessary and incorrect code.

--- test/core/FileStat/instance/test_grpowned.rb
+++ test/core/FileStat/instance/test_grpowned.rb
@@ -16,32 +16,29 @@ class TC_FileStat_GrpOwned_InstanceMethod < Test::Unit::TestCase
     @stat = File::Stat.new(@file)
 
     if WINDOWS
-      @user = nil
       @bool = nil
     else
-      @user = Etc.getpwnam(Etc.getlogin)
-      @bool = Etc.getgrgid(@user.gid).name == @user.name
-      @bool = true if ROOT || OSX
+      @bool = ROOT || OSX ? true : false
     end
   end
 
-  test "grpowned basic functionality" do
+  test "grpowned? basic functionality" do
     assert_respond_to(@stat, :grpowned?)
     assert_boolean(@stat.grpowned?)
   end
 
-  test "grpowned always returns false on Windows" do
-    omit_unless(WINDOWS, 'grpowned test skipped except on Windows')
+  test "grpowned? always returns false on Windows" do
+    omit_unless_windows('grpowned?')
     assert_false(@stat.grpowned?)
   end
 
-  test "grpowned returns the expected results on Unix" do
-    omit_if(WINDOWS, 'grpowned test skipped on Windows')
+  test "grpowned? returns the expected results on Unix" do
+    omit_if_windows('grpowned?')
     assert_true(@stat.grpowned?)
     assert_equal(@bool, File::Stat.new('/').grpowned?)
   end
 
-  test "grpowned does not take any arguments" do
+  test "grpowned? does not take any arguments" do
     assert_raises(ArgumentError){ @stat.grpowned?(1) }
   end
 
@@ -50,7 +47,6 @@ class TC_FileStat_GrpOwned_InstanceMethod < Test::Unit::TestCase
 
     @stat = nil
     @bool = nil
-    @user = nil
     @file = nil
   end
 end
