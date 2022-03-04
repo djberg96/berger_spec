9298cf6b975d3b82b5aef8553323d8e0e3e1e7d7
Daniel Berger <djberg96@gmail.com>
2022-03-04 12:41:40 -0500

    Update range hash test.

--- test/core/Range/instance/test_hash.rb
+++ test/core/Range/instance/test_hash.rb
@@ -7,34 +7,35 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Range_Hash_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @range1 = Range.new(1, 100)
-      @range2 = Range.new('a', 'z', true)
-   end
+  def setup
+    @range1 = Range.new(1, 100)
+    @range2 = Range.new('a', 'z', true)
+  end
 
-   def test_hash_basic
-      assert_respond_to(@range1, :hash)
-      assert_nothing_raised{ @range1.hash }
-      assert_kind_of(Fixnum, @range1.hash)
-   end
+  test "hash basic functionality" do
+    assert_respond_to(@range1, :hash)
+    assert_nothing_raised{ @range1.hash }
+    assert_kind_of(Numeric, @range1.hash)
+  end
 
-   def test_hash_identical
-      assert_equal(true, @range1.hash == Range.new(1, 100).hash)
-      assert_equal(false, @range1.hash == Range.new(1, 99).hash)
-      assert_equal(false, @range1.hash == Range.new(1.0, 100).hash)
-   end
+  test "range equality works as expected" do
+    assert_true(@range1.hash == Range.new(1, 100).hash)
+    assert_false(@range1.hash == Range.new(1, 99).hash)
+    assert_false(@range1.hash == Range.new(1.0, 100).hash)
+  end
 
-   def test_hash_identical_match_exclusive
-      assert_equal(true, @range2.hash == Range.new('a', 'z', true).hash)
-      assert_equal(false, @range2.hash == Range.new('a', 'z').hash)
-   end
+  test "a subset of a range is not considered equal" do
+    assert_true(@range2.hash == Range.new('a', 'z', true).hash)
+    assert_false(@range2.hash == Range.new('a', 'z').hash)
+  end
 
-   def test_hash_expected_errors
-      assert_raise(ArgumentError){ @range1.hash(1) }
-   end
+  test "hash does not accept any arguments" do
+    assert_raise(ArgumentError){ @range1.hash(1) }
+    assert_raise(ArgumentError){ @range1.send(:hash, 1) }
+  end
 
-   def teardown
-      @range1 = nil
-      @range2 = nil
-   end
+  def teardown
+    @range1 = nil
+    @range2 = nil
+  end
 end
