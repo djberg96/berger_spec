16289f4f46555f50928775fca18ca289f5eda177
Daniel Berger <djberg96@gmail.com>
2022-03-09 09:45:52 -0500

    Fix deprecation warnings for Object#object_id tests.

--- test/core/Object/instance/test_object_id.rb
+++ test/core/Object/instance/test_object_id.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_object_id.rb
+# test_object_id.rb
 #
 # Test case for the Object#object_id instance method and the
 # Object#__id__ alias.
@@ -8,23 +8,24 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Object_ObjectId_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @object = Object.new
-   end
+  def setup
+    @object = Object.new
+  end
 
-   def test_object_id_basic
-      assert_respond_to(@object, :object_id)
-      assert_nothing_raised{ @object.object_id }
-      assert_kind_of(Fixnum, @object.object_id)
-   end
+  test "object_id basic functionality" do
+    assert_respond_to(@object, :object_id)
+    assert_nothing_raised{ @object.object_id }
+    assert_kind_of(Integer, @object.object_id)
+  end
 
-   def test_id_alias_basic
-      assert_respond_to(@object, :__id__)
-      assert_nothing_raised{ @object.__id__ }
-      assert_kind_of(Fixnum, @object.__id__)
-   end
+  test "id is an synonym for object_id" do
+    assert_respond_to(@object, :__id__)
+    assert_nothing_raised{ @object.__id__ }
+    assert_kind_of(Integer, @object.__id__)
+    assert_equal(@object.__id__, @object.object_id)
+  end
 
-   def teardown
-      @object = nil
-   end
+  def teardown
+    @object = nil
+  end
 end
