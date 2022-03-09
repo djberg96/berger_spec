c89ee7dfbd6699f0f65042b20ed8a53e3a5015a8
Daniel Berger <djberg96@gmail.com>
2022-03-09 09:42:50 -0500

    Update clone tests.

--- test/core/Object/instance/test_clone.rb
+++ test/core/Object/instance/test_clone.rb
@@ -1,5 +1,5 @@
 #######################################################################
-# tc_clone.rb
+# test_clone.rb
 #
 # Test case for the Object#clone instance method.
 #######################################################################
@@ -7,56 +7,59 @@ require 'test/helper'
 require 'test/unit'
 
 class ObjectClone
-   attr_accessor :str
+  attr_accessor :str
 end
 
 class TC_Object_Clone_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @object = Object.new
-      @clone  = nil
-      @string = 'Hello'
+  def setup
+    @object = Object.new
+    @clone  = nil
+    @string = 'Hello'
 
-      @custom = ObjectClone.new
-      @custom.str = @string
-   end
+    @custom = ObjectClone.new
+    @custom.str = @string
+  end
 
-   def test_clone_basic
-      assert_respond_to(@object, :clone)
-      assert_nothing_raised{ @object.clone }
-   end
+  test "clone basic functionality" do
+    assert_respond_to(@object, :clone)
+    assert_nothing_raised{ @object.clone }
+  end
 
-   def test_clone
-      assert_nothing_raised{ @clone = @object.clone }
-      assert_equal(true, @clone.object_id == @clone.object_id)
-   end
+  test "clone creates a separate object" do
+    assert_nothing_raised{ @clone = @object.clone }
+    assert_false(@clone.object_id == @object.object_id)
+  end
 
-   def test_clone_instance_variables
-      assert_nothing_raised{ @clone = @custom.clone }
-      assert_equal('Hello', @clone.str)
-      assert_nothing_raised{ @custom.str[1,4] = 'i' }
-      assert_equal('Hi', @custom.str)
-      assert_equal('Hi', @clone.str)
-   end
+  test "cloned instance variables are retained" do
+    assert_nothing_raised{ @clone = @custom.clone }
+    assert_equal('Hello', @clone.str)
+    assert_nothing_raised{ @custom.str[1,4] = 'i' }
+    assert_equal('Hi', @custom.str)
+    assert_equal('Hi', @clone.str)
+  end
 
-   def test_clone_keeps_frozen_state
-      assert_nothing_raised{ @custom.freeze }
-      assert_nothing_raised{ @clone = @custom.clone }
-      assert_equal(true, @custom.frozen?)
-      assert_equal(true, @clone.frozen?)
-   end
+  test "a cloned object retains the original object's frozen state" do
+    assert_nothing_raised{ @custom.freeze }
+    assert_nothing_raised{ @clone = @custom.clone }
+    assert_true(@custom.frozen?)
+    assert_true(@clone.frozen?)
+  end
 
-   def test_clone_expected_errors
-      assert_raise(ArgumentError){ @object.clone(true) }
-      assert_raise(TypeError){ 7.clone }
-      assert_raise(TypeError){ nil.clone }
-      assert_raise(TypeError){ true.clone }
-      assert_raise(TypeError){ false.clone }
-      assert_raise(TypeError){ 'hello'.to_sym.clone }
-   end
+  test "clone does not accept any arguments" do
+    assert_raise(ArgumentError){ @object.clone(true) }
+  end
 
-   def teardown
-      @object = nil
-      @clone  = nil
-      @custom = nil
-   end
+  test "cloning primitive objects is permitted" do
+    assert_nothing_raised{ 7.clone }
+    assert_nothing_raised{ nil.clone }
+    assert_nothing_raised{ true.clone }
+    assert_nothing_raised{ false.clone }
+    assert_nothing_raised{ 'hello'.to_sym.clone }
+  end
+
+  def teardown
+    @object = nil
+    @clone  = nil
+    @custom = nil
+  end
 end
