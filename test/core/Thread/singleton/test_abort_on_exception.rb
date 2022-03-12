38531dca0ca5669daaff57dcab7e4cc9b65989e5
Daniel Berger <djberg96@gmail.com>
2022-03-12 09:41:47 -0500

    Update the abort_on_exception singleton thread tests.

--- test/core/Thread/singleton/test_abort_on_exception.rb
+++ test/core/Thread/singleton/test_abort_on_exception.rb
@@ -1,46 +1,58 @@
 ###############################################################################
-# tc_abort_on_exception.rb
+# test_abort_on_exception.rb
 #
 # Test case for the Thread.abort_on_exception and Thread.abort_on_exception=
-# class methods
+# singleton methods
 ###############################################################################
 require 'test/helper'
 require 'test/unit'
 
-class TC_Thread_AbortOnException_ClassMethod < Test::Unit::TestCase
-   include Test::Helper
+class TC_Thread_AbortOnException_SingletonMethod < Test::Unit::TestCase
+  include Test::Helper
 
-   def setup
-      @thread = nil
-      Thread.abort_on_exception = false
-   end
+  def setup
+    @orig_stderr = $stderr.dup
+    @thread = nil
+    Thread.abort_on_exception = false
+  end
 
-   def test_abort_on_exception_basic
-      assert_respond_to(Thread, :abort_on_exception)
-      assert_respond_to(Thread, :abort_on_exception=)
-      assert_boolean(Thread.abort_on_exception)
-   end
+  test "abort_on_exception basic functionality" do
+    assert_respond_to(Thread, :abort_on_exception)
+    assert_boolean(Thread.abort_on_exception)
+  end
 
-   def test_abort_on_exception
-      assert_equal(false, Thread.abort_on_exception)
-      assert_equal(true, Thread.abort_on_exception = true)
-      assert_equal(true, Thread.abort_on_exception)
-   end
+  test "abort_on_exception= basic functionality" do
+    assert_respond_to(Thread, :abort_on_exception=)
+    assert_true(Thread.abort_on_exception = true)
+  end
 
-   # TODO: How do I test Thread.abort_on_exception = true without breaking
-   # the test suite?
-   #
-   def test_abort_on_exception_behavior
-      assert_nothing_raised{ @thread = Thread.new{ raise 'AOE TEST' } }
-      assert_raise(RuntimeError){ @thread.join }
-   end
+  test "abort_on_exception returns expected value" do
+    assert_false(Thread.abort_on_exception)
+    assert_true(Thread.abort_on_exception = true)
+    assert_true(Thread.abort_on_exception)
+  end
 
-   def test_abort_on_exception_expected_errors
-      assert_raise(ArgumentError){ Thread.abort_on_exception(true) }
-   end
+  # NOTE: These are not very good tests.
+  test "abort_on_exception behavior when true works as expected" do
+    $stderr.reopen(IO::NULL)
+    Thread.abort_on_exception = true
+    assert_nothing_raised{ @thread = Thread.new{ raise 'ABORT_ON_EXCEPTION TEST' } }
+    assert_raise(RuntimeError){ @thread.join }
+  end
 
-   def teardown
-      @thread = nil
-      Thread.abort_on_exception = false
-   end
+  test "abort_on_exception behavior when false works as expected" do
+    $stderr.reopen(IO::NULL)
+    assert_nothing_raised{ @thread = Thread.new{ raise 'ABORT_ON_EXCEPTION TEST' } }
+    assert_raise(RuntimeError){ @thread.join }
+  end
+
+  test "abort_on_exception does not accept any arguments" do
+    assert_raise(ArgumentError){ Thread.abort_on_exception(true) }
+  end
+
+  def teardown
+    $stderr = @orig_stderr
+    @thread = nil
+    Thread.abort_on_exception = false
+  end
 end
