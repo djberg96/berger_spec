593ef32262953ec22328c9d262b9992fc5ff7f35
Daniel Berger <djberg96@gmail.com>
2022-03-12 21:11:30 -0500

    Update float floor tests.

--- test/core/Float/instance/test_floor.rb
+++ test/core/Float/instance/test_floor.rb
@@ -1,5 +1,5 @@
 ######################################################################
-# tc_floor.rb
+# test_floor.rb
 #
 # Test case for the Float#floor instance method.
 ######################################################################
@@ -7,30 +7,43 @@ require 'test/helper'
 require 'test/unit'
 
 class Test_Float_Floor_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @float_pos = 1.07
-      @float_neg = -0.93
-   end
+  def setup
+    @float_pos = 1.07
+    @float_neg = -0.93
+  end
 
-   def test_floor_basic
-      assert_respond_to(@float_pos, :floor)
-      assert_nothing_raised{ @float_pos.floor }
-      assert_kind_of(Integer, @float_pos.floor)
-   end
+  test "floor basic functionality" do
+    assert_respond_to(@float_pos, :floor)
+    assert_nothing_raised{ @float_pos.floor }
+    assert_kind_of(Integer, @float_pos.floor)
+  end
 
-   def test_floor
-      assert_equal(1, @float_pos.floor)
-      assert_equal(-1, @float_neg.floor)
-      assert_equal(1, (1.0).floor)
-      assert_equal(0, (0.0).floor)
-   end
+  test "floor with no arguments returns expected value for postive floats" do
+    assert_equal(1, @float_pos.floor)
+    assert_equal(1, (1.0).floor)
+    assert_equal(0, (0.0).floor)
+  end
 
-   def test_floor_expected_errors
-      assert_raises(ArgumentError){ @float_pos.floor(1) }
-   end
+  test "floor with no arguments returns expected value for negative floats" do
+    assert_equal(-1, @float_neg.floor)
+  end
 
-   def teardown
-      @float_pos = nil
-      @float_neg = nil
-   end
+  test "floor with arguments returns the expected value" do
+    assert_equal(0.5, 0.555.floor(1))
+    assert_equal(0.55, 0.555.floor(2))
+    assert_equal(0.555, 0.555.floor(3))
+    assert_equal(0.555, 0.555.floor(99))
+  end
+
+  test "arguments to floor must be numeric" do
+    assert_raise(TypeError){ 0.555.floor("test") }
+    assert_raise(TypeError){ 0.555.floor(true) }
+    assert_raise(TypeError){ 0.555.floor(false) }
+    assert_raise(TypeError){ 0.555.floor(nil) }
+  end
+
+  def teardown
+    @float_pos = nil
+    @float_neg = nil
+  end
 end
