36ae344ce4d1264f97f547b57d90f2df45af689e
Daniel Berger <djberg96@gmail.com>
2022-03-12 21:26:12 -0500

    Update float constant tests.

--- test/core/Float/singleton/test_constants.rb
+++ test/core/Float/singleton/test_constants.rb
@@ -1,5 +1,5 @@
 ######################################################################
-# tc_constants.rb
+# test_constants.rb
 #
 # Test case for the constants associated with the Float class.
 ######################################################################
@@ -7,21 +7,18 @@ require 'test/helper'
 require 'test/unit'
 
 class Test_Float_Constants < Test::Unit::TestCase
-   def test_float_constants
-      assert_not_nil(Float::DIG)
-      assert_not_nil(Float::EPSILON)
-      assert_not_nil(Float::MANT_DIG)
-      assert_not_nil(Float::MAX)
-      assert_not_nil(Float::MAX_10_EXP)
-      assert_not_nil(Float::MAX_EXP)
-      assert_not_nil(Float::MIN)
-      assert_not_nil(Float::MIN_10_EXP)
-      assert_not_nil(Float::MIN_EXP)
-      assert_not_nil(Float::RADIX)
-   end
-
-   def test_float_rounds
-      assert_not_nil(Float::ROUNDS)
-      assert_equal(true, [-1,0,1,2,3].include?(Float::ROUNDS))
-   end
+  test "expected float constants are defined" do
+    assert_not_nil(Float::DIG)
+    assert_not_nil(Float::EPSILON)
+    assert_not_nil(Float::INFINITY)
+    assert_not_nil(Float::MANT_DIG)
+    assert_not_nil(Float::MAX)
+    assert_not_nil(Float::MAX_10_EXP)
+    assert_not_nil(Float::MAX_EXP)
+    assert_not_nil(Float::MIN)
+    assert_not_nil(Float::MIN_10_EXP)
+    assert_not_nil(Float::MIN_EXP)
+    assert_not_nil(Float::NAN)
+    assert_not_nil(Float::RADIX)
+  end
 end
