e344162de945e6b30d78751871b3691a039e188e
Daniel Berger <djberg96@gmail.com>
2022-11-08 04:59:57 -0500

    Refactor the Thread#raise tests.

--- test/core/Thread/instance/test_raise.rb
+++ test/core/Thread/instance/test_raise.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# tc_raise.rb
+# test_raise.rb
 #
 # Test case for the Thread#raise instance method.
 ########################################################################
@@ -7,83 +7,69 @@ require 'test/helper'
 require 'test/unit'
 
 class TC_Thread_Raise_InstanceMethod < Test::Unit::TestCase
-   def setup
-      @thread = Thread.new{ sleep }
-   end
+  def setup
+    @thread = Thread.new{ sleep }
+    @stderr = $stderr.dup
+    Thread.pass until @thread.status == 'sleep'
+  end
 
-   def test_raise_basic
-      assert_respond_to(@thread, :raise)
-   end
+  test "raise basic functionality" do
+    assert_respond_to(@thread, :raise)
+  end
 
-   def test_raise
-      Thread.pass until @thread.status == 'sleep'
-      assert_nothing_raised{ @thread.raise }
-      assert_raise(RuntimeError){ @thread.join }
-   end
+  test "raises a RuntimeError if a raised thread is joined" do
+    Thread.pass until @thread.status == 'sleep'
+    assert_nothing_raised{ @thread.raise }
+    assert_raise(RuntimeError){ @thread.join }
+  end
 
-   def test_raise_with_message
-      Thread.pass until @thread.status == 'sleep'
+  test "raises the specified message if a raised thread is joined" do
+    assert_nothing_raised{ @thread.raise('hello') }
 
-      assert_nothing_raised{ @thread.raise('hello') }
-      assert_raise(RuntimeError){ @thread.join }
-      assert_raise(RuntimeError){ @thread.join } # Duplicate intentional
+    begin
+      @thread.join
+    rescue RuntimeError => msg
+      assert_equal('hello', msg.to_s)
+    end
+  end
 
-      begin
-         @thread.join
-      rescue RuntimeError => msg
-         assert_equal('hello', msg.to_s)
-      end
-   end
+  test "raises the specified error class if provided as an argument" do
+    assert_nothing_raised{ @thread.raise(StandardError) }
 
-   def test_raise_with_exception
-      Thread.pass until @thread.status == 'sleep'
+    begin
+      @thread.join
+    rescue StandardError => msg
+      assert_equal('StandardError', msg.to_s)
+    end
+  end
 
-      assert_nothing_raised{ @thread.raise(StandardError) }
-      assert_raise(StandardError){ @thread.join }
-      assert_raise(StandardError){ @thread.join } # Duplicate intentional
+  test "raises the specified error class with the given message if provided" do
+    assert_nothing_raised{ @thread.raise(StandardError, 'hello') }
 
-      begin
-         @thread.join
-      rescue StandardError => msg
-         assert_equal('StandardError', msg.to_s)
-      end
-   end
+    begin
+      @thread.join
+    rescue StandardError => msg
+      assert_equal('hello', msg.to_s)
+    end
+  end
 
-   def test_raise_with_exception_and_message
-      Thread.pass until @thread.status == 'sleep'
+  test "raises the specified error class with the given message and backtrace if provided" do
+    assert_nothing_raised{ @thread.raise(NameError, 'hello', %/foo bar/) }
 
-      assert_nothing_raised{ @thread.raise(StandardError, 'hello') }
-      assert_raise(StandardError){ @thread.join }
-      assert_raise(StandardError){ @thread.join } # Duplicate intentional
+    begin
+      @thread.join
+    rescue NameError => msg
+      assert_equal('hello', msg.to_s)
+      assert_equal('foo bar', msg.backtrace[0])
+    end
+  end
 
-      begin
-         @thread.join
-      rescue StandardError => msg
-         assert_equal('hello', msg.to_s)
-      end
-   end
+  test "raise accepts a maximum of three arguments" do
+    assert_raise(ArgumentError){ @thread.raise(1,2,3,4) }
+  end
 
-   def test_raise_with_exception_and_message_and_array
-      Thread.pass until @thread.status == 'sleep'
-
-      assert_nothing_raised{ @thread.raise(NameError, 'hello', %/foo bar/) }
-      assert_raise(NameError){ @thread.join }
-      assert_raise(NameError){ @thread.join } # Duplicate intentional
-
-      begin
-         @thread.join
-      rescue NameError => msg
-         assert_equal('hello', msg.to_s)
-         assert_equal('foo bar', msg.backtrace[0])
-      end
-   end
-
-   def test_raise_expected_errors
-      assert_raise(ArgumentError){ @thread.raise(1,2,3,4) }
-   end
-
-   def teardown
-      @thread.exit
-      @thread = nil
-   end
+  def teardown
+    @thread.exit if @thread
+    @thread = nil
+  end
 end
